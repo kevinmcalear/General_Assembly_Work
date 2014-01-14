3e93277b1c19be77bcd4de7e71a170a58734cf6f
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-01-14 18:06:18 -0500

    Create Phase III of Calculator

--- w01/d02/Michael/calculator.rb
+++ w01/d02/Michael/calculator.rb
@@ -24,8 +24,42 @@ def sqrt (first_num)
 	return Math.sqrt(first_num)
 end
 
-def exponent (base, power)
+def exponent (first_num, second_num)
 	print "The exponent of Base:#{base} and Power:#{power} is #{base ** power}"
 	return base ** power
 
 end
+
+
+puts "Welcome to the Ruby Calculator.  What would you like to do?", 
+	"(A)ddition",
+	"(S)ubtraction",
+	"(M)ultiplication",
+	"(D)ivision"
+	"Square (R)oot", 
+	"(E)xponent", 
+	"(Q)uit"
+ans = gets.chomp.downcase
+
+puts "What is the first number?"
+first_num = gets.chomp.to_i
+puts "What is the second number?"
+second_num = gets.chomp.to_i
+
+CASE ans
+	when "a"	
+		add(first_num, second_num)
+	when "s"
+		subtract(first_num, second_num)
+	when "m"
+		multiplication(first_num, second_num)
+	when "d"
+		division(first_num, second_num)
+	when "r"
+		sqrt(first_num, second_num)
+	when "e"
+		exponent(first_num, second_num)
+	else puts "Thank you for using Ruby Calculator"
+end
+
+	
