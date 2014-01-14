dada7c85c91e2f55be878b7ff3d9fa2c4aee7df4
Pierre S. Gergis <pierre.s.gergis@gmail.com>
2014-01-14 17:06:15 -0500

    Added fizzbuzz method

--- w01/d02/Pierre/methods.rb
+++ w01/d02/Pierre/methods.rb
@@ -36,3 +36,17 @@ puts "2 is prime? #{is_prime?(2)}"
 puts "4 is prime? #{is_prime?(4)}"
 puts "20 is prime? #{is_prime?(20)}"
 puts "29 is prime? #{is_prime?(29)}"
+
+#Twisted fizzbuzz (specs in separate file: twisted_fizzbuzz.md)
+def twisted_fizzbuzz(n)
+  for i in 0..n
+    if i%3==0 && i%5!=0
+      puts "#{i} is divisible by 3 and not by 5"
+    elsif i%5==0 && i%3 != 0
+      puts "#{i} is divisible by 5 and not by 3"
+    end
+  end
+end
+
+#test fizzbuzz
+twisted_fizzbuzz(45)
