ed329c9edf579f2958010e8660ef9cec4f4884e8
Zack Stayman <z.stayman@gmail.com>
2014-01-14 17:35:49 -0500

    Methods defined

---
+++ w01/d02/Zack_Stayman/calculator.rb
@@ -0,0 +1,18 @@
+puts "CALC"
+
+def addition(x, y)
+  return x + y
+end
+
+def subtraction(x, y)
+  return x - y
+end
+
+def multiplication(x, y)
+  return x * y
+end
+
+def division(x, y)
+  return x / y
+end
+
