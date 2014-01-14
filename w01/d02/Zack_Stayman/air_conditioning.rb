1ee94946c5fa3e4462e605ef0eab0f8f44791332
Zack Stayman <z.stayman@gmail.com>
2014-01-14 11:53:00 -0500

    fixed air conditioning

--- w01/d02/Zack_Stayman/air_conditioning.rb
+++ w01/d02/Zack_Stayman/air_conditioning.rb
@@ -1,5 +1,14 @@
 puts "is the A/C functional?"
-functional = gets.chomp
+functional = gets.chomp.downcase
+
+if functional == "yes"
+  functional == true
+elsif functional == "no"
+  functional == false
+else
+  puts "please enter yes or no"
+  functional = gets.chomp
+end
 
 puts "what is the current temperature?"
 temp = gets.chomp.to_i
@@ -7,17 +16,13 @@ temp = gets.chomp.to_i
 puts "what temperature do you want it to be?"
 comfort = gets.chomp.to_i
 
-if functional == "yes"
-  functional = true
-else
-  functional = false
-end
+
 
 if functional && (temp > comfort)
   puts "Turn the ac on please"
-elsif (functional == false) && (temp > comfort)
+elsif (!functional) && (temp > comfort)
   puts "Fix the AC now"
-elsif (functional == false) && (temp < comfort)
+elsif (!functional) && (temp < comfort)
   puts "Fix the AC when you can"
 else
   puts "You're doing fine"
