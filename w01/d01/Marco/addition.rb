1ca8b51173cd75256ac8a29b70130d1d39d63f12
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-14 17:10:14 -0500

    merge classwork - no hw

---
+++ w01/d01/Marco/addition.rb
@@ -0,0 +1,11 @@
+puts "What is the first number you would like to add?"
+
+first_number = gets.chomp.to_i
+
+puts "What is the second number you would like to add?"
+
+second_number = gets.chomp.to_i
+
+answer = first_number + second_number
+
+puts "#{first_number} + #{second_number} = #{answer}"
