04d332af261b5ff0d777dca72ef1c1fcc691601b
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-01-14 17:34:30 -0500

    Create Phase II of Calculator

--- w01/d02/Michael/calculator.rb
+++ w01/d02/Michael/calculator.rb
@@ -6,15 +6,26 @@ end
 
 def subtract (first_num, second_num)
 	print "The difference is #{first_num - second_num}"
-	return first_num-second_num
+	return first_num - second_num
 end
 
 def multiplication (first_num, second_num)
 	print "The product is #{first_num * second_num}"
-	return first_num*second_num
+	return first_num * second_num
 end
 
 def division (first_num, second_num)
 	print "The dividend is #{first_num / second_num}"
-	return first_num/second_num
+	return first_num / second_num
+end
+
+def sqrt (first_num)
+	print "The Square Root of #{first_num} is #{Math.sqrt(first_num)}"
+	return Math.sqrt(first_num)
+end
+
+def exponent (base, power)
+	print "The exponent of Base:#{base} and Power:#{power} is #{base ** power}"
+	return base ** power
+
 end
