9318dc2a0fbf3a576bc20ac17e201afbee412688
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-01-14 17:21:16 -0500

    Create Phase I of Calculator

---
+++ w01/d02/Michael/calculator.rb
@@ -0,0 +1,20 @@
+
+def add (first_num, second_num)
+	print "The sum is #{first_num + second_num}"
+	return first_num+second_num
+end
+
+def subtract (first_num, second_num)
+	print "The difference is #{first_num - second_num}"
+	return first_num-second_num
+end
+
+def multiplication (first_num, second_num)
+	print "The product is #{first_num * second_num}"
+	return first_num*second_num
+end
+
+def division (first_num, second_num)
+	print "The dividend is #{first_num / second_num}"
+	return first_num/second_num
+end
