a606b08443bd863358db38378676f3a60848e78b
Zack Stayman <z.stayman@gmail.com>
2014-01-14 17:56:12 -0500

    menu works

--- w01/d02/Zack_Stayman/calculator.rb
+++ w01/d02/Zack_Stayman/calculator.rb
@@ -1,18 +1,63 @@
 puts "CALC"
 
+action = nil
+
 def addition(x, y)
-  return x + y
+  a = x + y
+  return a
 end
 
 def subtraction(x, y)
-  return x - y
+  a = x - y
+  return a
 end
 
 def multiplication(x, y)
-  return x * y
+  a = x * y
+  return a
 end
 
 def division(x, y)
-  return x / y
+  a = x/y
+  return a
 end
 
+while action != "q"
+puts "do you want to (a)dd, (s)ubtract, (m)ultiply, (d)ivide, or (q)uit?"
+action = gets.chomp
+
+  if action == "a"
+    puts "what is the first number you wish to add?"
+    x = gets.chomp.to_f
+    puts "what is the second number you wish to add?"
+    y = gets.chomp.to_f
+    ans = addition(x, y)
+    puts ans
+  elsif action == "s"
+    puts "what is the number you wish to subtract from?"
+    x = gets.chomp.to_f
+    puts "what is the number you wish to subtract?"
+    y = gets.chomp.to_f
+    ans = subtraction(x, y)
+    puts ans
+  elsif action == "m"
+    puts "what is the first number you wish to multiply?"
+    x = gets.chomp.to_f
+    puts "what is the second number you wish to multiply?"
+    y = gets.chomp.to_f
+    ans = multiplication(x, y)
+    puts ans
+  elsif action == "d"
+    puts "what is the number you wish to divide from?"
+    x = gets.chomp.to_f
+    puts "what is the number you wish to divide by?"
+    y = gets.chomp.to_f
+    ans = division(x, y)
+    puts ans
+  elsif action == "q"
+    puts "goodbye"
+  else
+    puts "ERROR: Invalid entry please make another"
+    action=gets.chomp
+  end
+end
