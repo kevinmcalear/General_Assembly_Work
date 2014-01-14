b490bac4a287b8b32828f7056c21998c9d7a18d7
Peter Lai <peter.yd.lai@gmail.com>
2014-01-14 09:53:23 -0500

    morning

---
+++ w01/d02/INSTRUCTOR/addition.rb
@@ -0,0 +1,5 @@
+puts "What is the first number you want to add?"
+x = gets.chomp.to_i
+puts "Please enter the second number to add"
+x2 = gets.chomp.to_i
+puts "The sum of these numbers is #{x + x2}"
---
+++ w01/d02/INSTRUCTOR/name_my_pet.rb
@@ -0,0 +1,9 @@
+puts "Please enter an animal type"
+animal = gets.chomp
+puts "Enter an age"
+age = gets.chomp
+puts "Name"
+name = gets.chomp
+# puts "AWESOME!!! #{name} the #{animal} is #{age} !!!!"
+
+puts "AWESOME!!!" + name + "the" +animal + "is" + age + "!!!"
