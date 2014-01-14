8fcf123056dbbf18c1bf6bfeb983aa96e020a645
Granger <GrangerAbuhoff@gmail.com>
2014-01-14 17:30:10 -0500

    add exponent and sqrt

--- w01/d02/Granger/calculator.rb
+++ w01/d02/Granger/calculator.rb
@@ -1,3 +1,39 @@
-def +(first)
-  puts "goal"
+off = false
+
+
+# while !off
+#   print "Enter your first number: "
+#   print "Enter your operation (+,-,/,*,^,sqr,  "
+#   print "Enter your second number: "
+
+#   puts "Turn off (N)? "
+#    if gets.chomp == "N" 
+#     || gets.chomp == "(N)"
+#     || gets.chomp == "n" 
+#     || gets.chomp.downcase == "no"
+#     off = true
+# end
+
+def add(first, second) 
+  first + second
+end
+
+def subtract(first, second)
+  first - second
+end
+
+def multiply(first, second)
+  first*second
+end
+
+def divide(first, second)
+  first/second
+end
+
+def exponent(first, second)
+  first^second
+end
+
+def square root(first)
+  Math.sqrt(first)
 end
