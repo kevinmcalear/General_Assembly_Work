e2720f5a137ca071524a8767197b88ab002dd280
My Name <myemail@example.com>
2014-01-15 18:34:16 -0500

    part 2 hw day 3

--- w01/d03/Ilias/mta.rb
+++ w01/d03/Ilias/mta.rb
@@ -1,14 +1,51 @@
+def stop_count(line1, line2, start, finish)
+  if line1 == line2
+    x = line1.index(start)
+    y = line1.index(finish)
+    dist = (x - y).abs
+    return dist
+  else
+    x = (line1.index(start) - line1.index("us")).abs
+    y = (line2.index("us") - line2.index(finish)).abs
+    dist = (x + y)
+    return dist
+  end
+end
+
 n = ['ts', '34th', '28th-n', '23rd-n', 'us']
+l = ['8th', '6th', 'us', '3rd', '1st']
 
-puts "Where are you? ('ts', '34th', '28th-n', '23rd-n', 'us')"
-start = gets.chomp.downcase
+puts "What line do you want to ride on? n or l?"
+first_line = gets.chomp.downcase
+case first_line
+when "n"
+  first_line = n
+  puts "What are you now? #{n}"
+  first_stop = gets.chomp.downcase
+when "l"
+  first_line = l
+  puts "What are you now? #{l}'"
+  first_stop = gets.chomp.downcase
+else
+  puts "Sorry didn't get that..."
+  first_stop = gets.chomp.downcase
+end
 
-puts "Where are you going? ('ts', '34th', '28th-n', '23rd-n', 'us')"
-finish = gets.chomp.downcase
+puts "What line do you want to get off on? n or l?"
+second_line = gets.chomp.downcase
+case second_line
+when "n"
+  second_line = n
+  puts "What stop you do want to arrive at? #{n}"
+  last_stop = gets.chomp.downcase
+when "l"
+  second_line = l
+  puts "What stop you do want to arrive at? #{l}"
+  last_stop = gets.chomp.downcase
+end
 
-x = n.index(start)
-y = n.index(finish)
+stops = stop_count(first_line, second_line, first_stop, last_stop)
 
-stops = (x - y).abs
+puts "You are traveling #{stops} stops."
 
-puts "You are traveling #{stops}."
+
