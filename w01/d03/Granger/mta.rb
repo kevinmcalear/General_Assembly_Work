3a51eff041c1562ccb607d361f3714a4390a34c4
Granger <GrangerAbuhoff@gmail.com>
2014-01-15 17:43:23 -0500

    2nd commit: two lines

--- w01/d03/Granger/mta.rb
+++ w01/d03/Granger/mta.rb
@@ -7,14 +7,46 @@ mta[:l] = l
 mta[:s] = s
 
 
+puts "What line do you want to start on? n or l"
+start_line = gets.chomp.downcase
+
+"What stop do you want to get on at:"
+
+case start_line
+when "n"
+    puts n
+when "l" 
+  puts l
+end
 
 
-puts "What stop do you want to get on at: #{n}"
-start = gets.chomp.downcase
-puts  "What stop do you want to get off at: #{n}"
+    start = gets.chomp.downcase
+
+puts "What line do you want to stop on? n or l"
+stop_line = gets.chomp.downcase
+
+puts  "What stop do you want to get off at:"
+case stop_line
+when "n"
+    puts n
+when "l" 
+  puts l
+end
+
 stop = gets.chomp.downcase
 
-start_index = n.index(start)
-stop_index = n.index(stop)
+start_index = mta[start_line.to_sym].index(start)
+stop_index = mta[stop_line.to_sym].index(stop)
 
-p total_stops = (start_index - stop_index).abs
+total_stops = 0
+
+if start_line != stop_line
+  total_stops = (start_index -  mta[start_line.to_sym].index("us")).abs 
+  total_stops += (stop_index -  mta[stop_line.to_sym].index("us")).abs
+else
+  total_stops = (start_index - stop_index).abs
+end
+
+p total_stops
+
+
