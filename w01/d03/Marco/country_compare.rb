3e66356eaef85432f917629fcd5e47e9afa648e4
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-15 14:38:44 -0500

    random save

---
+++ w01/d03/Marco/country_compare.rb
@@ -0,0 +1,24 @@
+# USA : NYC, LA | burgers, hot dogs
+# France : Paris, Nice | baguettes, croissants
+# Italy : Rome, Pisa | pizza, pasta, wine
+
+countries = {
+
+  :usa => {    
+    :cities => ["NYC", "LA"],
+    :food => ["burgers", "hot dogs"]
+    },
+
+  :france => {
+    :cities => ["Paris", "Nice"],
+    :food => ["baguettes", "croissants"]
+    },
+
+  :italy => {
+    :cities => ["Rome", "Pisa"], 
+    :food => ["pizza", "pasta", "wine"]
+    }
+
+}
+
+puts countries
