ec55412b7abea93ccdab862f82b537f3fd61e24f
tunneylax20 <bryantunney@gmail.com>
2014-01-15 18:02:47 -0500

    first commit

---
+++ w01/d03/Drew/mta.rb
@@ -0,0 +1,29 @@
+mta = {
+  :n => ["ts", "34th", "28th-n", "23rd-n", "us", "8th"],
+  :l => ["8th", "6th", "us", "3rd", "1st"],
+  :s => ["gc", "33rd", "28th-s", "23rd-s", "us", "as"]
+}
+
+def print_stops(mta, line)
+  print "For line #{line}, the stops are : "
+  mta[line].each do |stop|
+    print "#{stop} "
+  end
+end
+
+puts "What stop would you like to get on?"
+print_stops(mta, :n)
+puts
+begin_trip = gets.chomp
+
+puts "What stop would you like to get off at?"
+print_stops(mta, :n)
+puts
+end_trip = gets.chomp
+
+begin_index = mta[:n].index(begin_trip)
+end_index = mta[:n].index(end_trip)
+
+total_stops = (begin_index - end_index).abs
+
+puts "The length of that trip is #{total_stops}."
