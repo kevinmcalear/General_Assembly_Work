7f9770332a3f52d64b300710154112fc09ff4708
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-01-14 23:07:27 -0500

    Commit 2 - Calc

--- w01/d02/Nicholas/calc.rb
+++ w01/d02/Nicholas/calc.rb
@@ -16,3 +16,14 @@ def divide(num1, num2)
   return num1/num2
 end
 
+def exp(num1, num2 = 2)
+  return num1 ** num2
+end
+
+def sqrt(number)
+  x = 0 
+  while x * x != number
+    x += 1
+  end
+  return x
+end
