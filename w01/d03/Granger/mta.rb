5a5e8a0dbbacac211e2a8c9cb8512ba0d8bcce5e
Granger <GrangerAbuhoff@gmail.com>
2014-01-15 17:00:45 -0500

    commit 1: one line

---
+++ w01/d03/Granger/mta.rb
@@ -0,0 +1,20 @@
+n = ['ts', '34th', '28th-n', '23rd-n', 'us']
+l = ['8th', '6th', 'us', '3rd', '1st']
+s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
+mta = {}
+mta[:n] = n
+mta[:l] = l
+mta[:s] = s
+
+
+
+
+puts "What stop do you want to get on at: #{n}"
+start = gets.chomp.downcase
+puts  "What stop do you want to get off at: #{n}"
+stop = gets.chomp.downcase
+
+start_index = n.index(start)
+stop_index = n.index(stop)
+
+p total_stops = (start_index - stop_index).abs
