981335004c737cb8366c26263ddf8d5c00b90a11
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-01-14 20:04:10 -0500

    Initial Commit Calc

---
+++ w01/d02/Nicholas/calc.rb
@@ -0,0 +1,18 @@
+# Calculator Program by Nicholas Bundy
+
+def add(num1, num2)
+  return num1 + num2
+end 
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1/num2
+end
+
