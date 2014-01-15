12ed136212cbff39717e08bb3ff4cc7c0ed57126
My Name <myemail@example.com>
2014-01-14 21:23:13 -0500

    commiting air conditiner and finished calculator with bonuses

--- w01/d02/Dima/calculator.rb
+++ w01/d02/Dima/calculator.rb
@@ -18,7 +18,40 @@
       puts "Result: #{result}"
     end
 
-    
+    def root(a)
+      result = Math.sqrt(a)
+      puts "Result: #{result}"
+    end
+
+    def sin(a)
+      result = Math.sin(a)
+      puts "Result: #{result}"
+    end
+
+    def cos(a)
+      result = Math.cos(a)
+      puts "Result: #{result}"
+    end
+
+    def tan(a)
+      result = Math.tan(a)
+      puts "Result: #{result}"
+    end
+
+    def exponent(base, exp)
+      result = base ** exp
+      puts "Result: #{result}"
+    end
+
+    def factorial(num)
+      fact = 1
+      while num > 0
+       fact = fact * num
+       num = num -1
+      end
+      puts "Result: #{fact}"
+   end
+
     def quit
       puts "Good bye"
       return "q"
@@ -31,7 +64,7 @@
       when "r"
         puts "(a)ddition, (s)ubstraction, (m)multiplication, (d)ivision, (q)uit"
       when "v"
-        puts "sq(u)are root, e(x)ponents, factoria(l), s(i)n, c(o)s, (t)an (q)uit"
+        puts "sq(u)are root, e(x)ponents, factoria(l), s(i)n, c(o)s, (t)an, (q)uit"
       when "q"
         return quit
       end
@@ -62,6 +95,32 @@
         puts "Please enter the second number:"
         b = gets.chomp.to_i
         division(a, b)
+      when "u"
+        puts "Please enter the number"
+        a = gets.chomp.to_i
+        root(a)
+      when "i"
+        puts "Please enter the number"
+        a = gets.chomp.to_i
+        sin(a)
+      when "o"
+        puts "Please enter the number"
+        a = gets.chomp.to_i
+        cos(a)
+      when "t"
+        puts "Please enter the number"
+        a = gets.chomp.to_i
+        tan(a)
+      when "x"
+        puts "Please enter the base number"
+        base = gets.chomp.to_i
+        puts "Exponent number"
+        exp = gets.chomp.to_i
+        exponent(base, exp)
+      when "l"
+        puts "Please enter the number"
+        num = gets.chomp.to_i
+        factorial(num)
       when "q"
         quit
 
