05acf2fcb72cad43f6e25863b1433bc640e958fb
Kevin McAlear <kevin.mcalear@gmail.com>
2014-01-14 19:08:50 -0500

    Completed Part 3git add .! Wooot

--- w01/d02/Kevin/calculator.rb
+++ w01/d02/Kevin/calculator.rb
@@ -1,74 +1,83 @@
+def menu
   puts "********WELCOME TO MY CALCULATOR********"
   puts 
   puts "please tell me what you want to do:"
   puts "(b)asic calculation (a)dvanced calculation or (q)uit"
   input = gets.chomp
-
-if input == "b"
-  puts "Please tell me if you want to:"
-  puts "(a)dd (s)ubtract (m)ultiply (d)ivide or anything else to go back"
-  basic_input = gets.chomp
-elsif input == "a"
-  puts "Please tell me if you want to:"
-  puts "(e)xponents (s)quare roots or anything else to go back"
-  advanced_input = gets.chomp
-else 
-  puts "Thanks for using me!"
+  return input
 end
 
-case basic_input
+input = menu
+while input != "q"
 
-when "a"
-  puts "Tell me the first number you want to add."
-  num_one = gets.chomp.to_i
-  puts "Now the number you want to add please."
-  num_two = gets.chomp.to_i
-  added_num = num_one + num_two
-  puts "Your Answer Was: #{added_num}"
-when "s"
-  puts "Tell me the first number you want."
-  num_one = gets.chomp.to_i
-  puts "Now the number you want to subtract by please."
-  num_two = gets.chomp.to_i
-  subtracted_num = num_one - num_two
-  puts "Your Answer Was: #{subtracted_num}"
+  case input
+  
+  when "b"
+    puts "Please tell me if you want to:"
+    puts "(a)dd (s)ubtract (m)ultiply (d)ivide or anything else to go back"
+    basic_input = gets.chomp
 
-when "m"
-puts "Tell me the first number you want."
-  num_one = gets.chomp.to_i
-  puts "Now the number you want to multiply by please."
-  num_two = gets.chomp.to_i
-  subtracted_num = num_one * num_two
-  puts "Your Answer Was: #{subtracted_num}"
+  when "a"
+    puts "Please tell me if you want to:"
+    puts "(e)xponents (s)quare roots or anything else to go back"
+    advanced_input = gets.chomp
+  end
 
-when "d"
-  puts "Tell me the first number you want."
-  num_one = gets.chomp.to_i
-  puts "Now the number you want to divide by please."
-  num_two = gets.chomp.to_i
-  subtracted_num = num_one / num_two
-  puts "Your Answer Was: #{subtracted_num}"
+  case basic_input
 
-else
+  when "a"
+    puts "Tell me the first number you want to add."
+    num_one = gets.chomp.to_i
+    puts "Now the number you want to add please."
+    num_two = gets.chomp.to_i
+    added_num = num_one + num_two
+    puts "Your Answer Was: #{added_num}"
 
-end
+  when "s"
+    puts "Tell me the first number you want."
+    num_one = gets.chomp.to_i
+    puts "Now the number you want to subtract by please."
+    num_two = gets.chomp.to_i
+    subtracted_num = num_one - num_two
+    puts "Your Answer Was: #{subtracted_num}"
 
-case advanced_input
+  when "m"
+    puts "Tell me the first number you want."
+    num_one = gets.chomp.to_i
+    puts "Now the number you want to multiply by please."
+    num_two = gets.chomp.to_i
+    subtracted_num = num_one * num_two
+    puts "Your Answer Was: #{subtracted_num}"
 
-when "e"
-  puts "Tell me the base number you want."
-  num_one = gets.chomp.to_i
-  puts "Now the exponent you want to use please."
-  num_two = gets.chomp.to_i
-  exponent_num = num_one ** num_two
-  puts "Your Answer Was: #{exponent_num}"
+  when "d"
+    puts "Tell me the first number you want."
+    num_one = gets.chomp.to_i
+    puts "Now the number you want to divide by please."
+    num_two = gets.chomp.to_i
+    subtracted_num = num_one / num_two
+    puts "Your Answer Was: #{subtracted_num}"
+  end
 
-when "s"
-  puts "Tell me number you want to square."
-  num = gets.chomp.to_i
-  squarert_num = Math.sqrt(num) 
-  puts "Your Answer Was: #{squarert_num.to_i}"
+  case advanced_input
 
-else
+  when "e"
+    puts "Tell me the base number you want."
+    num_one = gets.chomp.to_i
+    puts "Now the exponent you want to use please."
+    num_two = gets.chomp.to_i
+    exponent_num = num_one ** num_two
+    puts "Your Answer Was: #{exponent_num}"
 
+  when "s"
+    puts "Tell me number you want to square."
+    num = gets.chomp.to_i
+    squarert_num = Math.sqrt(num) 
+    puts "Your Answer Was: #{squarert_num.to_i}"
+  end
+
+  if input == "q"
+    puts "Thanks for using me!"
+  end
+
+  input = menu
 end
