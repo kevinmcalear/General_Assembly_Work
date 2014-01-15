9a79298d639322eb7d6d61ae6f9c1e853ab30922
tunneylax20 <bryantunney@gmail.com>
2014-01-14 19:31:07 -0500

    commit 2

--- w01/d02/Drew/calculator.rb
+++ w01/d02/Drew/calculator.rb
@@ -1,29 +1,62 @@
-puts "Select 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, q to quit"
-option = gets.chomp
- 
-while option != "q"
+def add(x, y)
+  return x + y
+end
 
-puts "Enter first value"
-x = gets.chomp.to_i
-puts "Enter second value"
-y = gets.chomp.to_i
+def sub(x, y)
+  return x - y
+end
 
-case option
+def mul(x, y)
+  return x * y
+end
+
+def div(x, y)
+  return x / y
+end
+
+def exp(x, y)
+  return x ** y
+end
+
+def sq(x)
+return Math.sqrt(x)
+end
+
+program = 1
+
+while program == 1
+
+puts "Select 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for exponents, 6 for square root, q to quit"
+  type = gets.chomp
+
+if type == "q"
+  program = 0
+elsif program == 1
+  puts "Enter first value"
+  x = gets.chomp.to_i
+  puts "Enter second value"
+  y = gets.chomp.to_i
+
+case type
   when "1"
-    results = x + y
+    result = add(x, y)
   when "2"
-    results = x - y
+    result = sub(x, y)
   when "3"
-    results = x * y
-  else
-    results = x / y
+    result = mul(x, y)
+  when "4"
+    result = div(x, y)
+  when "5"
+    result = exp(x, y)
+  when "6"
+    result = sq(x)
 end
 
-puts results
+puts result
 
-exit
+
 
 end
 
-
+end
 
