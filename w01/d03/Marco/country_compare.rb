4b7e5d2b2cffeafed617e42546093eff048d53e6
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-15 17:14:09 -0500

    all classwork before homework

--- w01/d03/Marco/country_compare.rb
+++ w01/d03/Marco/country_compare.rb
@@ -21,4 +21,40 @@ countries = {
 
 }
 
-puts countries
+#puts countries
+
+# to call a particular country, type:
+# puts countries[:italy][:cities] 
+
+# to print a random city from France, type:
+#puts countries[:france][:cities].sample
+
+puts "What countries have you been to?"
+country = gets.chomp.downcase.to_sym
+
+def discuss_trip(countries, country)
+
+city = countries[country][:cities].sample
+food = countries[country][:food].sample
+
+if country == :france
+  puts "What are you talking about?! You've never been to #{country.capitalize}!"
+else  
+puts "I just went on holiday to #{city}, #{country.capitalize}, and I really enjoyed the #{food}."
+  end
+
+end
+
+puts discuss_trip(countries, country)
+
+
+
+
+
+
+
+
+
+
+
+
