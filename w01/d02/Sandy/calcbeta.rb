4132f19151059babc2cae158d0d25d90e8938aa6
My Name <myemail@example.com>
2014-01-14 20:25:50 -0500

    all files done for today jan 14

---
+++ w01/d02/Sandy/calcbeta.rb
@@ -0,0 +1,16 @@
+puts "What would you like to do? 
+        Enter A for adding
+        Enter S for subtracting
+        Enter M for multiply
+        Enter D for divide 
+        Enter SQRT for square square square root
+        Enter E for exponent" 
+ input = gets.chomp
+
+while
+    if input == "A"
+      def add (a,b)
+        return a + b
+      end
+    end
+end
