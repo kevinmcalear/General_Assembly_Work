aff40fca527784274529a6fada528093d5f814d3
Kevin McAlear <kevin.mcalear@gmail.com>
2014-01-15 17:10:06 -0500

    n-line working

---
+++ w01/d03/Kevin/mta.rb
@@ -0,0 +1,11 @@
+n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n']
+puts "Where are you?"
+start = gets.chomp
+puts "Where are you going?"
+finish = gets.chomp
+
+start_position = n.index(start)
+finish_position = n.index(finish)
+dist = (start_position - finish_position).abs
+
+puts "You need to go #{dist} stops."
