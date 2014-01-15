97405aeff558ac2166b69db1493b31059f654a03
My Name <myemail@example.com>
2014-01-15 18:57:33 -0500

    part 2.1 hw d3

--- w01/d03/Ilias/mta.rb
+++ w01/d03/Ilias/mta.rb
@@ -1,30 +1,30 @@
-def stop_count(line1, line2, start, finish)
+mta = {
+  :n => ['ts', '34th', '28th-n', '23rd-n', 'us'],
+  :l => ['8th', '6th', 'us', '3rd', '1st']
+}
+
+def stop_counter(line1, line2, start, finish, mta)
   if line1 == line2
-    x = line1.index(start)
-    y = line1.index(finish)
+    x = mta[line1].index(start)
+    y = mta[line1].index(finish)
     dist = (x - y).abs
     return dist
   else
-    x = (line1.index(start) - line1.index("us")).abs
-    y = (line2.index("us") - line2.index(finish)).abs
+    x = (mta[line1].index(start) - mta[line1].index("us")).abs
+    y = (mta[line2].index("us") - mta[line2].index(finish)).abs
     dist = (x + y)
     return dist
   end
 end
 
-n = ['ts', '34th', '28th-n', '23rd-n', 'us']
-l = ['8th', '6th', 'us', '3rd', '1st']
-
 puts "What line do you want to ride on? n or l?"
-first_line = gets.chomp.downcase
+first_line = gets.chomp.downcase.to_sym
 case first_line
-when "n"
-  first_line = n
-  puts "What are you now? #{n}"
+when :n
+  puts "What are you now? #{mta[:n]}"
   first_stop = gets.chomp.downcase
-when "l"
-  first_line = l
-  puts "What are you now? #{l}'"
+when :l
+  puts "What are you now? #{mta[:l]}'"
   first_stop = gets.chomp.downcase
 else
   puts "Sorry didn't get that..."
@@ -32,19 +32,17 @@ else
 end
 
 puts "What line do you want to get off on? n or l?"
-second_line = gets.chomp.downcase
+second_line = gets.chomp.downcase.to_sym
 case second_line
-when "n"
-  second_line = n
-  puts "What stop you do want to arrive at? #{n}"
+when :n
+  puts "What stop you do want to arrive at? #{mta[:n]}"
   last_stop = gets.chomp.downcase
-when "l"
-  second_line = l
-  puts "What stop you do want to arrive at? #{l}"
+when :l
+  puts "What stop you do want to arrive at? #{mta[:l]}"
   last_stop = gets.chomp.downcase
 end
 
-stops = stop_count(first_line, second_line, first_stop, last_stop)
+stops = stop_counter(first_line, second_line, first_stop, last_stop, mta)
 
 puts "You are traveling #{stops} stops."
 
