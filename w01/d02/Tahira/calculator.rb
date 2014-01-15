cfeb23cd339c310980f7d52a9e6e06806f676f57
My Name <myemail@example.com>
2014-01-14 20:44:05 -0500

    Modified commit 3#

--- w01/d02/Tahira/calculator.rb
+++ w01/d02/Tahira/calculator.rb
@@ -22,12 +22,14 @@ operation_type = "A"
 #Main menu loop
 while (operation_type.upcase != "Q")
   puts "What type of operation would you like to perform?"
-  puts "Basic (B), Advanced (A), Trigonometric (T), or Quit (Q)"
-
+  puts "Basic (B), Advanced (A), Trigonometric (T), Factorial (F) or Quit (Q):"
   operation_type = gets.chomp.upcase
+  while !(operation_type == "B" || operation_type == "A" || operation_type == "T" || operation_type == "F" || operation_type == "Q")
+    puts "Please enter a valid operation type. Basic (B), Advanced (A), Trigonometric (T), Factorial (F) or Quit (Q):"
+    operation_type = gets.chomp.upcase
+  end
 
   case operation_type
-
 #Basic calculator functions
   when "B"
     puts "What operation would you like to do? Add (A), Subtract (S), Multiply (M), or Divide (D):"
@@ -42,7 +44,6 @@ while (operation_type.upcase != "Q")
       n2 = gets.chomp.to_f
       puts "The answer is:"
       puts Calculator(basic_type, n1, n2)
-
 #Advanced calculator functions
   when "A"
     puts "What operation would you like to do? Exponent (EX), Root (RT):"
@@ -64,7 +65,7 @@ while (operation_type.upcase != "Q")
       puts "The answer is:"
       puts Calculator(advanced_type, n1, n2)
   end
-
+#Trig functions
 
 end
 
