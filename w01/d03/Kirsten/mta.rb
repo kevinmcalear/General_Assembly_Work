b41bdbb8ae64b161f2a8cd95ad18c4c5421a620e
kdoyne <kdoyne930@gmail.com>
2014-01-15 18:37:01 -0500

    Two lines working.

--- w01/d03/Kirsten/mta.rb
+++ w01/d03/Kirsten/mta.rb
@@ -1,16 +1,31 @@
 
+mta = {
+:n => ["times square", "34th", "28th", "23rd", "union square", "8th"],
+:l => ["8th ave", "6th ave", "union square", "3rd ave", "1st ave"],
+:s => ["grand central", "33rd", "28th", "23rd", "union square", "astor place"]
+}
 
-n_line = ["times square", "34th", "28th", "23rd", "union square", "8th"]
-# l_line = ["8th ave", "6th ave", "union square", "3rd ave", "1st ave"]
-# 6_line = ["grand central", "33rd", "28th", "23rd", "union square", "astor place"]
+puts "What line would you like to get on (l or n)?"
+first_line = gets.chomp.downcase
 
-puts "The N line stops at Times Square, 34th, 28th, 23rd, Union Square, and 8th"
-puts "What stop do you want to get on at?"
-f_stop = gets.chomp.downcase
+puts "The #{first_line} line stops at #{mta[first_line.to_sym].join(", ")}"
+puts "Where would you like to get on?"
+first_stop = gets.chomp.downcase
 
-puts "What stop do you want to get off at?"
-l_stop = gets.chomp.downcase
+puts "What line do you want to get off of (l or n)?"
+last_line = gets.chomp.downcase
 
-l_stop_number = n_line.index(l_stop) +1
-f_stop_number = n_line.index(f_stop) +1
-puts "You have gone #{(l_stop_number-f_stop_number).abs} stops."
+puts "The #{last_line} line stops at #{mta[last_line.to_sym].join(", ")}"
+puts "Where would you like to get off?"
+last_stop = gets.chomp.downcase
+
+if firsts_line = last_line
+stops = mta[first_line.to_sym].index(first_stop) - mta[last_line.to_sym].index(last_stop)
+puts "You have travelled #{stops.abs} stops."
+else
+  stops = (mta[first_line.to_sym].index(first_stop) - mta[first_line.to_sym].index("union square")).abs + (mta[last_line.to_sym].index("union square") - mta[last_line.to_sym].index(last_stop).abs)
+puts "You have travelled #{stops.abs} stops."
+end
+
+
+
