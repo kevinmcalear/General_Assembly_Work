a4add20300c2e9e269d74962d7b857efdaa70c74
My Name <myemail@example.com>
2014-01-14 20:07:57 -0500

    commit 3 basic and advanced functions complete

--- w01/d02/Sandy/calculator.rb
+++ w01/d02/Sandy/calculator.rb
@@ -14,5 +14,87 @@ def divide (a,b)
   return a/b
 end
 
+def square_root (a)
+  return Math.sqrt(a)
+end 
+
+def exponent (a,b)
+  return a**b
+end
+
+
+puts "What would you like to calculate? 
+        Enter A for adding
+        Enter S for subtracting
+        Enter M for multiply
+        Enter D for divide 
+        Enter SQRT for square root
+        Enter E for exponent
+        To quit, enter Q" 
+ input = gets.chomp!
+
+
+while input != "Q"
+    if input == "A"
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts "Enter another number"
+      second_number = gets.chomp.to_i
+      puts add(first_number, second_number)
+end
+
+    if input == "S"
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts "Enter another number"
+      second_number = gets.chomp.to_i
+      puts subtract(first_number, second_number)
+    end
+
+    if input == "M"
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts "Enter another number"
+      second_number = gets.chomp.to_i
+      puts multiply(first_number, second_number)
+    end
+
+    if input == "D"
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts "Enter another number"
+      second_number = gets.chomp.to_i
+      puts divide(first_number, second_number)
+    end
+
+    if input == "SQRT"
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts square_root(first_number)
+    end
+
+    if input =="E" 
+      puts "Enter a number"
+      first_number = gets.chomp.to_i
+      puts "Enter another number"
+      second_number = gets.chomp.to_i
+      puts exponent(first_number, second_number)
+    end
+
+    puts "What would you like to calculate? 
+        Enter A for adding
+        Enter S for subtracting
+        Enter M for multiply
+        Enter D for divide 
+        Enter SQRT for square root
+        Enter E for exponent
+        To quit, enter Q" 
+    input = gets.chomp!
+end
+
+
+
+
+
 
 
