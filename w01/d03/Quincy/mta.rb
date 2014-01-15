86a93015f6bdae21b9fbd4394b0b67f8165aa113
My Name <myemail@example.com>
2014-01-15 18:11:49 -0500

    third commit

--- w01/d03/Quincy/mta.rb
+++ w01/d03/Quincy/mta.rb
@@ -1,36 +1,41 @@
-n = ['ts', '34th', '28th-n', '23rd-n', 'us']
+n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th']
 l = ['8th', '6th', 'us', '3rd', '1st']
-s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
+s = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
 mta = {}
 mta[:n] = n
 mta[:l] = l
 mta[:s] = s
 
-
-puts "What line are you taking? The N or the L?"
-start_line = gets.chomp.downcase
-
-"What stop do you want to get on at:"
-
-case start_line
-when "n"
-puts n
-when "l"
-puts l
+def stops(line, mta)
+case line
+  when "n"
+    return mta[:n]
+  when "l"
+    return mta[:l]
+  when "s"
+    return mta[:s]
+end
 end
 
+puts "What line are you taking? The N, L or 6?"
+start_line = gets.chomp.downcase
+
+start_line = "s" if start_line =="6"
+
+puts "What stop do you want to get on at?"
+
+puts stops(start_line, mta)
 
 start = gets.chomp.downcase
 
-puts "What line do you want to stop on? The N or L?"
+puts "What line do you want to stop on? The N, L or 6?"
 stop_line = gets.chomp.downcase
 
-case stop_line
-when "n"
-puts n
-when "l"
-puts l
-end
+stop_line = "s" if stop_line =="6"
+
+puts "What stop do you want to get off at?"
+
+puts stops(stop_line, mta)
 
 stop = gets.chomp.downcase
 
