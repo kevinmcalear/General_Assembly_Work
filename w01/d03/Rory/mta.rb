6e7f5c142bff08a1e85d3e58263bd3bb2a178cf0
Rory Budnick <rorybudnick@gmail.com>
2014-01-15 20:47:16 -0500

    better third

--- w01/d03/Rory/mta.rb
+++ w01/d03/Rory/mta.rb
@@ -16,7 +16,7 @@ puts "What stop you want to get on at?"
 on_stop = gets.chomp
 puts "What line do you want to get off?"
 line2 = gets.chomp.downcase.to_sym
-if line 2 == line 1
+if line2 == line1
 	puts "The stops on the #{line1.upcase} line are:"
 	mta[line1].each do |stop|
 		puts stop
@@ -26,12 +26,12 @@ if line 2 == line 1
 	total = (mta[line1].index(off_stop) - mta[line1].index(on_stop)).abs
 	puts "The number of stops for your trip is #{total}."
 else
-	puts "The stops on the #{line 2} line are:"
+	puts "The stops on the #{line2.upcase} line are:"
 	mta[line2].each do |stop|
 		puts stop
 	end
 	puts "What stop do you want to get off at?"
 	off_stop2 = gets.chomp
-	total2 = (mta[line1].index('us') - mta[line1].index(on_stop)).abs + (mta[line1].index(off_stop2) - mta[line1].index('us')).abs
-	puts "The number of stops for your trip is #{total2}"
+	total2 = ((mta[line1].index('us') - mta[line1].index(on_stop)).abs) + ((mta[line2].index(off_stop2) - mta[line2].index('us')).abs)
+	puts "The number of stops for your trip is #{total2}."
 end
