399130a45dc6dabbe6d17871832e3629098cb4e4
Kevin McAlear <kevin.mcalear@gmail.com>
2014-01-16 11:52:32 -0500

    more midday updates

--- w01/d04/Kevin/midday.rb
+++ w01/d04/Kevin/midday.rb
@@ -1,3 +1,16 @@
 albums = ["Who's Next", "Tommy", "Quadrophenia", "The Who Sell Out", "My Generation", "A Quick One"]
 
-albums.each { |album| puts "I <3 #{album}."}
+def i_love_each_of( albums )
+
+  i = 0
+  until i == albums.count
+    
+    puts "I <3 #{albums[ i ]}!"
+   
+    i += 1
+
+  end
+
+end
+
+i_love_each_of( albums )
