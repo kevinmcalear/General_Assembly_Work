3283b317f95dcd757420bb112cf30391542d164a
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-15 23:36:34 -0500

    all but final bonus finished

--- w01/d03/Marco/mta_app.rb
+++ w01/d03/Marco/mta_app.rb
@@ -1,17 +1,19 @@
 
 
 puts "Please choose the subway line you will be taking on your trip."
-puts "(L), (N), or (S)ix"
+puts "(L), (N), (S)ix or (O)ne"
 
 subway_line1 = gets.chomp.downcase.to_sym
 
 n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n']
 l = ['8th', '6th', 'us', '3rd', '1st']
 s = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'astor']
+o = ['59th', '50th', 'TS', '34th', '28th']
 mta = {}
 mta[:n] = n
 mta[:l] = l
 mta[:s] = s
+mta[:o] = o
 
 puts "Please enter your starting station on the #{subway_line1.upcase} Line."
 puts mta[subway_line1].join(", ").upcase
@@ -20,7 +22,7 @@ subway_start = gets.chomp.to_s
 $origin = mta[subway_line1].index(subway_start)
 
 puts "Please choose the subway line for your destination."
-puts "(L), (N), or (S)ix"
+puts "(L), (N), (S)ix or (O)ne"
 
 subway_line2 = gets.chomp.downcase.to_sym
 
@@ -29,16 +31,31 @@ puts mta[subway_line2].join(", ").upcase
 
 subway_end = gets.chomp.to_s
 
+$destination = mta[subway_line2].index(subway_end)
+
 case 
 when subway_line1 == subway_line2
 
-  destination = mta[subway_line1].index(subway_end)
+  puts "You have #{($origin - $destination).abs} stops before your destination station."
 
-  puts "You have #{($origin - destination).abs} stops before your destination station."
+when subway_line1 == :o || subway_line2 == :o
 
-else subway_line2 == :n || subway_line2 == :l
+  pre_switch = ($origin - mta[subway_line1].index("us")).abs
+  post_switch = ($destination - mta[subway_line2].index("us")).abs
 
-  $destination = mta[subway_line2].index(subway_end)
+  travel = (pre_switch + post_switch).abs
+
+  if subway_line1 == :n || subway_line2 == :n
+
+  puts "You have #{travel} stops before your destination station."
+
+  else 
+
+  puts "You have #{travel + 4} stops before your destination station."
+
+  end
+
+when subway_line2 == :n || subway_line2 == :l || subway_line2 == :s
 
   pre_switch = ($origin - mta[subway_line1].index("us")).abs
   post_switch = ($destination - mta[subway_line2].index("us")).abs
