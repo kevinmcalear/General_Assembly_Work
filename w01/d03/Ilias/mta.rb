6206f6658368f724ff5c217a51961088d0ee4129
My Name <myemail@example.com>
2014-01-15 19:36:35 -0500

    HW W01 D03

--- w01/d03/Ilias/mta.rb
+++ w01/d03/Ilias/mta.rb
@@ -1,15 +1,19 @@
+
+##DATA: MTA stops for each line##
 mta = {
-  :n => ['ts', '34th', '28th-n', '23rd-n', 'us'],
-  :l => ['8th', '6th', 'us', '3rd', '1st']
+  :n => ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],  # N line
+  :l => ['8th-l', '6th', 'us', '3rd', '1st'],                        # L line
+  :s => ['gc', '33rd', '28th-s', '23rd-s', 'us', 'astor']    # 6 line
 }
 
+##Method##
 def stop_counter(line1, line2, start, finish, mta)
-  if line1 == line2
+  if line1 == line2                                                   # Change of train
     x = mta[line1].index(start)
     y = mta[line1].index(finish)
     dist = (x - y).abs
     return dist
-  else
+  else                                                                      # No change of train
     x = (mta[line1].index(start) - mta[line1].index("us")).abs
     y = (mta[line2].index("us") - mta[line2].index(finish)).abs
     dist = (x + y)
@@ -17,33 +21,31 @@ def stop_counter(line1, line2, start, finish, mta)
   end
 end
 
-puts "What line do you want to ride on? n or l?"
+puts "What line do you want to ride on? N L or S?"    #Q1
 first_line = gets.chomp.downcase.to_sym
-case first_line
-when :n
-  puts "What are you now? #{mta[:n]}"
-  first_stop = gets.chomp.downcase
-when :l
-  puts "What are you now? #{mta[:l]}'"
+if mta.include?(first_line)
+  puts "What stop are you at now? #{mta[first_line]}"  #Q2
   first_stop = gets.chomp.downcase
 else
-  puts "Sorry didn't get that..."
+  puts "Sorry didn't get that..."                                   # Error case
   first_stop = gets.chomp.downcase
 end
 
-puts "What line do you want to get off on? n or l?"
+puts "What line do you want to get off on? N L or S?" # Q3
 second_line = gets.chomp.downcase.to_sym
-case second_line
-when :n
-  puts "What stop you do want to arrive at? #{mta[:n]}"
+if mta.include?(second_line)
+  puts "What stop you do want to arrive at? #{mta[second_line]}" #Q4
   last_stop = gets.chomp.downcase
-when :l
-  puts "What stop you do want to arrive at? #{mta[:l]}"
-  last_stop = gets.chomp.downcase
+else
+  puts "Sorry didn't get that..."                                   # Error case
+  second_line = gets.chomp.downcase.to_sym
 end
 
+## Assignment of Method to Var ##
 stops = stop_counter(first_line, second_line, first_stop, last_stop, mta)
 
+##Method call, aka Output##
 puts "You are traveling #{stops} stops."
+
 
 
