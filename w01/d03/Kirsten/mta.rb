b9340023d7d1c9e50b392300628c94557872df37
kdoyne <kdoyne930@gmail.com>
2014-01-15 21:56:37 -0500

    working and cleaned up

--- w01/d03/Kirsten/mta.rb
+++ w01/d03/Kirsten/mta.rb
@@ -1,7 +1,7 @@
 mta = {
 :n => ["times square", "34th", "28th", "23rd", "union square", "8th"],
 :l => ["8th ave", "6th ave", "union square", "3rd ave", "1st ave"],
-:six => ["grand central", "33rd", "28th", "23rd", "union square", "astor place"]
+:six => ["grand central", "33rd", "28th", "23rd", "union square", "astor place"],
 }
 
 puts "What line would you like to get on (l, n, or 6)?"
@@ -30,10 +30,10 @@ puts "The #{last_line} line stops at #{mta[last_line.to_sym].join(", ")}"
 puts "Where would you like to get off?"
 last_stop = gets.chomp.downcase
 
-if firsts_line = last_line
-stops = mta[first_line.to_sym].index(first_stop) - mta[last_line.to_sym].index(last_stop)
-puts "You have travelled #{stops.abs} stops."
+if first_line == last_line
+  stops = mta[first_line.to_sym].index(first_stop) - mta[last_line.to_sym].index(last_stop)
+  puts "You have traveled #{stops.abs} stops."
 else
-  stops = (mta[first_line.to_sym].index(first_stop) - mta[first_line.to_sym].index("union square")).abs + (mta[last_line.to_sym].index("union square") - mta[last_line.to_sym].index(last_stop).abs)
-puts "You have travelled #{stops.abs} stops."
+  stops = ((mta[first_line.to_sym].index(first_stop) - mta[first_line.to_sym].index("union square")).abs) + (mta[last_line.to_sym].index("union square") - mta[last_line.to_sym].index(last_stop).abs)
+puts "You have traveled #{stops.abs} stops."
 end
