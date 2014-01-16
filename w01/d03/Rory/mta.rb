c607d46a247ae208a42f0a4854256779f73c3735
Rory Budnick <rorybudnick@gmail.com>
2014-01-15 20:38:32 -0500

    second

--- w01/d03/Rory/mta.rb
+++ w01/d03/Rory/mta.rb
@@ -1,19 +1,37 @@
 #one line functionality
-$N = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
-$L = ["8th Ave", "6th Ave", "Union Square", "3rd Ave", "1st Ave"]
-puts "The stops on the N line are: "
+mta = {
+:n => ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
+:l => ['8th', '6th', 'us', '3rd', '1st'],
+}
 
-$n.each do |stop|
+puts "What line do you want to get on?"
+line1 = gets.chomp.downcase.to_sym
+
+puts "The stops on the #{line1.upcase} line are:"
+mta[line1].each do |stop|
 	puts stop
 end
 
-def num_of_stops
-	puts "What stop you want to get on at?"
-	on_stop = gets.chomp
+puts "What stop you want to get on at?"
+on_stop = gets.chomp
+puts "What line do you want to get off?"
+line2 = gets.chomp.downcase.to_sym
+if line 2 == line 1
+	puts "The stops on the #{line1.upcase} line are:"
+	mta[line1].each do |stop|
+		puts stop
+	end
 	puts "What stop do you want to get off at?"
 	off_stop = gets.chomp
-	total = ($N.index(off_stop) - $N.index(on_stop)).abs
+	total = (mta[line1].index(off_stop) - mta[line1].index(on_stop)).abs
 	puts "The number of stops for your trip is #{total}."
+else
+	puts "The stops on the #{line 2} line are:"
+	mta[line2].each do |stop|
+		puts stop
+	end
+	puts "What stop do you want to get off at?"
+	off_stop2 = gets.chomp
+	total2 = (mta[line1].index('us') - mta[line1].index(on_stop)).abs + (mta[line1].index(off_stop2) - mta[line1].index('us')).abs
+	puts "The number of stops for your trip is #{total2}"
 end
-
-num_of_stops
