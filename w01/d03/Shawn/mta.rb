fcd29b57c6b4211b1c709294df320495ff7039f2
My Name <myemail@example.com>
2014-01-15 19:44:23 -0500

    mostly complete & functional except for the bonus portions

--- w01/d03/Shawn/mta.rb
+++ w01/d03/Shawn/mta.rb
@@ -1,16 +1,30 @@
-n_line_stops = ["TS", "34th", "28th", "23rd", "US", "8th"]
+mta = {
+  :n => ["TS", "34th", "28th", "23rd", "US", "8th"],
+  :l => ["8th", "6th", "US", "3rd", "1st"],
+  :six => ["GC", "33rd", "28th", "23rd", "US", "Astor Pl"],
+  :one => ["59th", "50th", "TS", "34th", "28th"]
+}
 
-puts 'The N-line has the following stops:'
-n_line_stops.each do |stop|
-  print " #{stop} \n"
-  
+def lets_get_started(mta)
+    puts "What line would you like to take?" 
+    mta.each do |key, value|
+    puts key
 end
+    which_line = gets.chomp
+    puts "The #{which_line.to_sym} has the following stops:"
+    puts mta[which_line.to_sym]
 
-puts "What stop would you like to get on at?"
-entry_point = gets.chomp
-puts "... and where will you be getting off the train?"
-exit_point = gets.chomp
+    puts "What stop would you like to get on?"
+    entry_point = gets.chomp
+    puts "...and where will you be getting off the train?..."
+    exit_point = gets.chomp
 
-no_of_stops = (n_line_stops.index(exit_point) - n_line_stops.index(entry_point)).abs
+ entry_index = mta[which_line.to_sym].index(entry_point)
+ exit_index = mta[which_line.to_sym].index(exit_point)
 
-puts "It will take you #{no_of_stops} stops..."
+ puts "Okay... it will take you #{(exit_index - entry_index).abs} stops..."
+ puts "Bon voyage..."
+end    
+
+lets_get_started(mta)
+
