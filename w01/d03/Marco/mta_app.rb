c2d698f91ecad00b638356bddef26746130c34b0
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-15 19:00:08 -0500

    code is working before finishing commit 2

--- w01/d03/Marco/mta_app.rb
+++ w01/d03/Marco/mta_app.rb
@@ -1,34 +1,28 @@
+
+
+puts "Please choose the subway line you will be taking on your trip."
+puts "(L), (N), or (S)ix"
+
+subway_line = gets.chomp.downcase.to_sym
+
 n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n']
 l = ['8th', '6th', 'us', '3rd', '1st']
-s = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'a']
+s = ['gc', '33rd', '28th-s', '23rd-s', 'us', 'astor']
 mta = {}
 mta[:n] = n
 mta[:l] = l
 mta[:s] = s
 
-puts "Please choose the subway line you will be taking on your trip."
-
-subway_line = gets.chomp.downcase
-
-puts "Please enter your starting station."
-#puts "The stations on the #{subway_line.upcase} are: " 
-#puts subway_line.each do |line, station| 
-# puts station
-# end
-puts "Stations: 'ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'"
+puts "Please enter your starting station on the #{subway_line.upcase} Line."
+puts mta[subway_line].join(", ").upcase
 
 subway_start = gets.chomp.to_s
+origin = mta[subway_line].index(subway_start)
 
 puts "Please enter your destination station."
 
 subway_end = gets.chomp.to_s
-
-origin = n.index(subway_start)
-
-
-
-
-destination = n.index(subway_end)
+destination = mta[subway_line].index(subway_end)
 
 
 
