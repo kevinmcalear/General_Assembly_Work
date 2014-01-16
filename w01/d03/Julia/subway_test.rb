4d3f03c7bf60c948b6c76158b237954621675289
Julia Lovett <julia.m.lovett@gmail.com>
2014-01-15 20:11:33 -0500

    Complete HW 01-03

---
+++ w01/d03/Julia/subway_test.rb
@@ -0,0 +1,17 @@
+subway_lines = {
+	:N => ["Times Square", "34th-N", "28th-N", "23rd-N", "Union Square", "8th-N"], 
+	:L => ["8th-L", "6th", "Union Square", "3rd", "1st"], 
+	:SIX => ["Grand Central", "33rd", "28th-6", "23rd-6", "Union Square", "Astor Place"]
+}
+first_train = :N
+starting_station = "Times Square"
+
+final_train = :N
+final_station = "23rd-N"
+
+
+location_of_first_station = subway_lines[first_train].index(starting_station)
+location_of_final_station = subway_lines[final_train].index(final_station)
+
+puts location_of_final_station
+puts location_of_first_station
