b02176a6a78c332d0770ec8bfe92698fd56d3d12
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-15 20:07:14 -0500

    commit 2

--- w01/d03/Marco/mta_app.rb
+++ w01/d03/Marco/mta_app.rb
@@ -3,7 +3,7 @@
 puts "Please choose the subway line you will be taking on your trip."
 puts "(L), (N), or (S)ix"
 
-subway_line = gets.chomp.downcase.to_sym
+subway_line1 = gets.chomp.downcase.to_sym
 
 n = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n']
 l = ['8th', '6th', 'us', '3rd', '1st']
@@ -13,22 +13,43 @@ mta[:n] = n
 mta[:l] = l
 mta[:s] = s
 
-puts "Please enter your starting station on the #{subway_line.upcase} Line."
-puts mta[subway_line].join(", ").upcase
+puts "Please enter your starting station on the #{subway_line1.upcase} Line."
+puts mta[subway_line1].join(", ").upcase
 
 subway_start = gets.chomp.to_s
-origin = mta[subway_line].index(subway_start)
+$origin = mta[subway_line1].index(subway_start)
 
-puts "Please enter your destination station."
+puts "Please choose the subway line for your destination."
+puts "(L), (N), or (S)ix"
+
+subway_line2 = gets.chomp.downcase.to_sym
+
+puts "Please enter your destination station on the #{subway_line2.upcase} Line."
+  puts mta[subway_line2].join(", ").upcase
 
 subway_end = gets.chomp.to_s
-destination = mta[subway_line].index(subway_end)
 
+case 
+  when subway_line1 == subway_line2
 
+  destination = mta[subway_line1].index(subway_end)
 
-puts "You have #{(origin - destination).abs} stops before your destination station."
+  puts "You have #{(origin - destination).abs} stops before your destination station."
 
+  else subway_line2 == :n || subway_line2 == :l
 
+  $destination = mta[subway_line2].index(subway_end)
 
+  # puts mta[subway_line2].index(subway_end)
+  # puts mta[subway_line2].index("us")
+
+  pre_switch = ($origin - mta[subway_line1].index("us")).abs
+  post_switch = ($destination - mta[subway_line2].index("us")).abs
+
+  travel = (pre_switch + post_switch).abs - 1
+
+  puts "You have #{travel} stops before your destination station."
+
+end
 
 
