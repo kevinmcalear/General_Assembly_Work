2930bbed4c53ec401dc508a3bf7125db7c98ef9d
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-01-16 22:41:50 -0500

    Commit One Word Names

--- w01/d04/Nicholas/potter.rb
+++ w01/d04/Nicholas/potter.rb
@@ -3,37 +3,42 @@ f = File.new("potter.csv", "r")
 potter = []
 
 csv_info = f.read
+# Return an array with just the names of all characters
 names = csv_info.split("\n")
+
+# Separate the info of each person into an array
 separate_info = []
 names.each {|line| separate_info << line.split(",")}
 
-#separate out the mentions, names, schools
+# Return an array with all character info for characters mentioned 
+# more than 500 times.
+greater_than_500 = separate_info.select do |contact|
+   contact[0].to_i > 500
+end
+
+# Separate out the mentions, names, schools into hashes
 separate_info.each do | line|
   a = {:mentions => line[0], :name => line[1], :school => line[2]}
   potter.push(a)
 end
 
-#storing just the names
+# Storing just the names
 def store_names(potter)
   store = []
   potter.each {|spot| store << spot[:name]}
   return store
 end
-
-#People that are mentioned more than 500 times
- greater_than_500 = separate_info.select do |contact|
-   contact[0].to_i > 500
- end
  
+# Storing just the school names and deleting duplicates and nil's
 def school_names(potter)
   houses = []
   potter.each {|spot| houses << spot[:school]}
   return houses.uniq!.compact!
 end
 
-# potter_houses = school_names(potter).uniq!
-# potter_houses.compact!
-puts school_names(potter)
+# Return an array with characters that have one word names
+one_word = separate_info.delete_if {|x| x[1].include?(" ")}
+
 
 f.close
 
