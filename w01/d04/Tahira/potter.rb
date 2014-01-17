90dc351ef62b388a60b0b193e45a49b21f340e3a
My Name <myemail@example.com>
2014-01-16 22:57:29 -0500

    HW 01 04

--- w01/d04/Tahira/potter.rb
+++ w01/d04/Tahira/potter.rb
@@ -3,6 +3,7 @@ f = File.new("potter.csv", "r")
   characters = Array.new
   f.each_line do |line|
     attributes = line.split(',')
+    attributes = attributes.map {|a| a.tr("\n", "")}
     a = Hash.new
     a[:mentions] = attributes[0]
     a[:name] = attributes[1]
@@ -11,49 +12,103 @@ f = File.new("potter.csv", "r")
   end
 f.close
 
-#Method that returns names of all the characters
+#Method that returns array with the names of all the characters
 def names(characters)
   names_list = characters.map {|h| h[:name]}
   return names_list
 end
 
+#Method that returns all information (array of hashes) about characters mentioned more than 500 times
 def multiple_mentions(characters)
-  multiple_mentions_list = characters.select { |h| h[:mentions].to_i > 500} 
+  multiple_mentions_list = characters.select { |h| h[:mentions].to_i > 500 } 
   return multiple_mentions_list
 end
 
-def houses(characters) {
-  houses_list =
-}
+#Method that returns array with the names of all houses
+def houses(characters) 
+  house_list = Array.new
+  characters.each do |h|
+    if !house_list.include?(h[:house]) && h[:house] != nil
+      house_list.push(h[:house])
+    end
+  end
+  return house_list
+end
 
+#number of characters in given residence 
+def num_in_house(residence, characters)
+  in_a_house = characters.select { |h| h[:house] != nil }
+  in_residence = in_a_house.select { |h| h[:house] == residence}
+  return in_residence.count
+end
+
+#characters with one word names
+def one_word_names(characters)
+  characters_w_one_word_names = characters.select{ |h| !h[:name].include?(" ")}
+  return characters_w_one_word_names
+end 
+
+#Gives weasleys "Badger" as middle name
+def weasleys_badger(characters)
+  weasleys = characters.select{ |h| h[:name].include?("Weasley")}
+  weasleys_list = weasleys.map {|h| h[:name].gsub(" "," Badger ")}
+end 
+
+#returns characters with first names that end in y
+def first_name_y(characters)
+  first_names = Array.new
+  characters.each do |h|
+    name = h[:name].split(" ")
+    first_name_length = name[0].length
+    first_name = name[0]
+    if first_name[first_name_length-1] == "y"
+      first_names.push(first_name)
+    end 
+  end
+  return first_names
+end
+
+#returns array of character names, reverses names of those from Slytherin
+def reverse_slytherin(characters)
+  characters.each do |h|
+    if h[:house] == "Slytherin"
+      h[:name].reverse!
+    end
+  end
+  return characters.map{ |h| h[:name]}
+end 
+
+def unique_last(residence, characters)
+    last_name_list = Array.new
+    characters.each do |h|
+      if(h[:house] == residence)
+        name = h[:name].split(" ")
+        if !last_name_list.include?(name[name.length-1])
+          last_name_list.push(name[name.length-1])
+        end
+      end
+    end
+    return last_name_list
+end
+  
+  
 ## Test that code is working
 puts characters
 puts characters.count
 puts names(characters)
 puts multiple_mentions(characters)
+puts houses(characters)
+puts one_word_names(characters)
+puts num_in_house("Hufflepuff", characters)
+puts weasleys_badger(characters)
+puts first_name_y(characters)
+puts reverse_slytherin(characters)
+puts ""
+puts unique_last("Gryffindor", characters)
 
 
-
-# # Book 8: Harry Potter and the Comma Separated Value
-
-# Included is a csv file with a bunch of Harry Potter data. Each line contains the number of times a certain character was mentioned throughout the Harry Potter series, their name, and in some cases, the house they are associated with. Your job will be to slice and dice the data in a variety of ways using File I/O, Arrays & Hashes, Enumeration, and String Manipulation.
-
-# ### To Do
-
-# * Write a Ruby program to open the `potter.csv` file.
-# * Figure out the data structure in which you want to store the information, and read in the data.
-
-# Write methods for each of the following:
-
-# 1. Return an array with just the names of all the characters
-# * Return an array with all the characters with more than 500 mentions. Each entry in the array should contain all information about that character. (ie `{:mentions=>18956, :name=>"Harry Potter", :house=>"Gryffindor"}`)
-# * Return an array with the names of all the houses. `["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]`
-# * Return an array with all the characters with one word names. Each entry in the array should contain all information about that character. (ie `{:mentions=>1797, :name=>"Voldemort", :house=>"Slytherin"}`)
-# * Return the number of characters from Hufflepuff (Hint: It should be 3)
-# * Return an array with all of the character names, but for the characters from Slytherin, reverse their names (ie "Draco Malfoy" becomes "yoflaM ocarD")
 # * Return an array with the unique last names of all the characters from Gryffindor (ie, print Weasley only 1 one time)
-# * Return an array with the names of all the Weasleys, but give each of them a middle name of "Badger"
-# * Return an array with the first names of characters whose first names end in "y"
+
 
 # ### Bonus
 
