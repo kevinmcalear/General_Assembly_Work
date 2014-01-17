66bd63a4cceea69f5dcaddbe07245896ef0f60c1
My Name <myemail@example.com>
2014-01-16 20:30:56 -0500

    slytherin reversed names. potter

--- w01/d04/Dima/potter.rb
+++ w01/d04/Dima/potter.rb
@@ -9,12 +9,49 @@ new_array = data_array.map {|line| line.split(",")}
 def names_method(array_of_hashes)
   puts array_of_hashes.map {|position| position[:name]} #just names
 end
+
 def array_500(array_of_hashes) #all the cahrs with more than 500 mentions
-  array_500 = array_of_hashes.select {|position| position[:mentions].to_i > 500 && position[:house] && position[:name]}
+  array_500 = array_of_hashes.select {|position| position[:mentions].to_i > 500} #&& position[:house] && position[:name]}
   puts array_500
 end
+
+def array_of_houses(array_of_hashes)
+  puts array_of_hashes.map {|position| position[:house]}
+end
+
+def one_word_name(array_of_hashes)
+  puts array_of_hashes.select {|position| !position[:name].include? " "}
+end
+
+def hufflepuff(array_of_hashes)
+  hufflepuff_arr = array_of_hashes.select {|position| position[:house] == "Hufflepuff"}
+  puts "The number of Hufflepuff houses is: #{hufflepuff_arr.length}"
+end
+
+def slytherin_reverse(array_of_hashes)
+  slytherin_houses = array_of_hashes.select {|position| position[:house] == "Slytherin"}
+  reversed_names = slytherin_houses.map {|position| position[:name].reverse}
+  puts reversed_names
+end
+
+def unique_last_names
+end
+
+puts "ARRAY OF HOUSES"
+array_of_houses(array_of_hashes)
+puts ""
+puts "NAMES"
 names_method(array_of_hashes)
+puts ""
+puts "500 MENTIONS"
 array_500(array_of_hashes)
-
+puts ""
+puts "ONE WORD NAME"
+one_word_name(array_of_hashes)
+puts ""
+puts "Hufflepuff HOUSES AMOUNT"
+hufflepuff(array_of_hashes)
+puts ""
+puts "NAMES FROM SLYTHERIN REVERSED"
+slytherin_reverse(array_of_hashes)
 f.close
-
