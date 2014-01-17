a6cf16ebb1e4ebf8cc3ef9ea72fca17ea3dcf785
My Name <myemail@example.com>
2014-01-16 19:09:41 -0500

    array of hashes for potter.rb

--- w01/d04/Dima/potter.rb
+++ w01/d04/Dima/potter.rb
@@ -1,7 +1,14 @@
 f = File.open("potter.csv", "r")
 data = f.read
 data_array = data.split("\n")
-new_array = data_array.map {|line| line.split(",")}
-names = new_array.map{|position| position[1]}
-puts mention_array
+new_array = data_array.map {|line| line.split(",")} #big array of arrays
+
+names = new_array.map{|position| position[1]} #just names
+
+array_of_hashes = new_array.map {|the_character| {:mentions => the_character[0],
+  :name => the_character[1],
+  :house => the_character[2]} }
+  puts array_of_hashes #array of hashes
+
 f.close
+
