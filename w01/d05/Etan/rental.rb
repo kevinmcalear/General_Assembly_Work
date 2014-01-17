0ed4c97ceae298fc7a4c6099f926850b0dfc9989
My Name <myemail@example.com>
2014-01-17 16:56:35 -0500

    Classes made

---
+++ w01/d05/Etan/rental.rb
@@ -0,0 +1,128 @@
+class Person
+
+	def name=(name)
+		@name = name
+	end
+
+	def name()
+		return @name
+	end
+
+
+	def age=(age)
+		@age=age
+	end
+
+	def age()
+		return @age
+	end
+
+
+	def gender=(gender)
+		@gender=gender
+	end
+
+	def gender()
+		return @gender
+	end
+
+
+  	def play
+    return "#{self.name()} by #{self.age()} #{self.gender()}"
+  	end
+
+end
+
+
+class Apartment
+
+	def name=(name)
+		@name = name
+	end
+
+	def name()
+		return @name
+	end
+
+
+	def price=(price)
+		@price=price
+	end
+
+	def price()
+		return @price
+	end
+
+
+	def sqft=(sqft)
+		@sqft=sqft
+	end
+
+	def sqft()
+		return @sqft
+	end
+
+	def num_beds=(num_beds)
+		@num_beds = num_beds
+	end
+
+	def num_beds()
+		return @num_beds
+	end
+
+
+	def num_baths=(num_baths)
+		@num_baths=num_baths
+	end
+
+	def num_baths()
+		return @num_baths
+	end
+
+
+	def renter=(renter)
+		@renter=renter
+	end
+
+	def renter()
+		return @renter
+	end
+end
+
+
+class Building
+
+	def name=(name)
+		@name = name
+	end
+
+	def name()
+		return @name
+	end
+
+
+	def address=(address)
+		@address=address
+	end
+
+	def address()
+		return @address
+	end
+
+
+	def num_floors=(num_floors)
+		@num_floors=num_floors
+	end
+
+	def num_floors()
+		return @num_floors
+	end
+
+	def apartments=(apartments)
+		@apartments = apartments
+	end
+
+	def aprtments()
+		return @apartments
+	end
+end
