387bd9a54faf59519b89294c54d1e08115c3d4ae
Granger <GrangerAbuhoff@gmail.com>
2014-01-17 16:06:36 -0500

    initial commit on rental

---
+++ w01/d05/Granger/rental.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_accessor :name, :age, :gender
+
+end
+
+class Apartment
+  attr_accessor :name, :price, :sqft, :num_beds, :num_baths, :renter
+
+end
+
+class Building
+  attr_accessor :name, :address, :num_floors, :apartments
+
+end
