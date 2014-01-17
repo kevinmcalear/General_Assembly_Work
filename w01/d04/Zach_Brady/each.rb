83c561c4ca89c18f03f8fbbfef9a55741ec61f91
Zach Brady <zacbra@gmail.com>
2014-01-17 08:56:46 -0500

    Yesterday's work & foundation of HW

--- w01/d04/Zach_Brady/each.rb
+++ w01/d04/Zach_Brady/each.rb
@@ -1,13 +1,17 @@
-# WRITE A METHOD THAT CYCLES THROUGH AN ARRAY OF ALBUM NAMES PRINTING THE MESSAGE: "I <3 ALBUM_NAME! BUT WITHOUT USING .EACH, INSTEAD USING A LOOP"
-
 albums = ["Who's Next", "Tommy", "Quadrophenia", "The Who Sell Out", "My Generation", "A Quick One"]
+loved_albums = []
 
 def I_love_each_of(albums)
     i = 0
     while i < albums.count
-        puts "I <3 #{albums[i]}!"
+        loved_albums.push("I <3 #{albums[i]}!")
         i += 1
     end
+    puts loved_albums
 end
 
-I_love_each_of(albums)
+
+albums.map { |albums| loved_albums.push("I <3 #{albums}!") }
+
+puts albums.select { |album | album.include?("Who")}
+
---
+++ w01/d04/Zach_Brady/receipt_generator_pt2.rb
@@ -0,0 +1,7 @@
+f = File.open("receipt.md", "r")
+
+string_version_of_receipt = f.read
+
+puts string_version_of_receipt
+
+f.close
