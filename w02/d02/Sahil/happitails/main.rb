00cbcdb381bd307d4957318681370abdd1002741
My Name <myemail@example.com>
2014-01-21 16:53:27 -0500

    commit 4

--- w02/d02/Sahil/happitails/main.rb
+++ w02/d02/Sahil/happitails/main.rb
@@ -4,13 +4,17 @@ require_relative 'models/shelter'
 
 s = Shelter.new
 choice = nil
-while choice != "4"
+while choice != "8"
 puts ""
 puts "MAIN MENUE (Pick a number)"
 puts "   1. Create an Animal"
 puts "   2. Create a client"
 puts "   3. Create a shelter"
-puts "   4. Quit"
+puts "   4. Display all animals"
+puts "   5. Display all clients"
+puts "   6. Facilitate adoption"
+puts "   7. Facilitate return"
+puts "   8. Quit"
 choice = gets.chomp
 	case choice
 	when "1"
@@ -21,6 +25,14 @@ choice = gets.chomp
 		old_shelter = s
 		s = Shelter.new
 	when "4"
+		s.display_animals
+	when "5"
+		s.display_clients
+	when "6"
+		s.adopt()
+	when "7"
+		s.return
+	when "8"
 		puts "Goodbye!"
 	else
 		"That's not a choice"
