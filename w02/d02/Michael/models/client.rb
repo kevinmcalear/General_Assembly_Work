73e1b4bfa1d1e3b7fe74fbab3be654b682848627
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-01-21 13:44:19 -0500

    Create Phase I Models-- animal client shelter

---
+++ w02/d02/Michael/models/client.rb
@@ -0,0 +1,27 @@
+class Client
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @animals = []
+  end 
+  def name
+    return @name
+  end
+  def age
+    return @age
+  end 
+  def animals
+    @animals
+  end 
+  def create_animals_array(array)
+    self.animals.push(array)
+  end 
+end 
+
+
+##TEST###
+tom = Client.new("Tom", 10)
+tom.create_animals_array(["Rover"])
+puts tom.name
+puts tom.age
+puts tom.animals
