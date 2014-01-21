03aa4a0d9e4a87e0743000e4ac094ac978d95153
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-21 15:26:55 -0500

    work through commit 3 and completed adding animals and clients to shelter arrays

--- w02/d02/Marco/main.rb
+++ w02/d02/Marco/main.rb
@@ -75,16 +75,69 @@ def initialize(name)
       species = gets.chomp.downcase
 
     animal = Animal.new(name, species)
+    
     self.shelter_animals().push(animal)
+
+    puts "#{name.capitalize} the #{species} has been added!"
   end
 
-  def add_client_to_shelter(client)
+  def add_client_to_shelter
+    puts "What is the new client's name?"
+      name = gets.chomp.downcase
+
+    puts "What is the new client's age?"
+      age = gets.chomp.to_i
+
+    client = Client.new(name, age)
+
     self.shelter_clients().push(client)
-    return shelter_clients(client)
+    
+    puts "#{name.capitalize}, aged #{age} years has been added!."
   end
 
 
 end
+
+new_shelter = Shelter.new("HappiTails")
+
+def menu
+  puts "-----------------------------"
+  puts "Animal Shelter Management App"
+  puts "1. Create a new (A)nimal"
+  puts "2. Create a new (C)lient"
+  puts "3. Create a new (S)helter"
+end
+
+menu
+
+response = gets.chomp.downcase
+
+until response == "q"
+
+  case response
+  when "a"
+    new_shelter.add_animal_to_shelter
+    menu 
+
+    response = gets.chomp.downcase
+
+  when "c"
+    new_shelter.add_client_to_shelter
+    menu
+    response = gets.chomp.downcase
+
+
+  when "s"
+    puts "s"
+    response = gets.chomp.downcase
+  else 
+    puts "That is not a valid option! Please try again."
+    response = gets.chomp.downcase
+  end
+
+end
+
+
 
 # test_shelter = Shelter.new("Lost Pets 4 Eww")
 # test_animal = Animal.new("Jack", "dog")
@@ -92,4 +145,4 @@ end
 
 # test_shelter.shelter_animals.push(test_animal)
 
-puts @shelter_animals
+
