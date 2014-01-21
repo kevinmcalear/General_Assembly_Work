f8225fb53749ee0e026bc3f788807437fb76967f
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-01-21 18:00:55 -0500

    commit 4 completed - facilitatte adoptions

--- w02/d02/Marco/main.rb
+++ w02/d02/Marco/main.rb
@@ -1,4 +1,6 @@
 
+require 'pry'
+
 class Animal
   def initialize(name, species)
     @name = name
@@ -25,10 +27,10 @@ end
 
 class Client
   def initialize(name, age)
-  @name = name
-  @age = age
+    @name = name
+    @age = age
 
-  client_pets = []
+    @client_pets = []
   end
 
   def name 
@@ -39,20 +41,23 @@ class Client
     return @age
   end
 
-  def client_pets(pets)
-    client_pets.push(pets)
+  def add_pet(pets)
+    @client_pets.push(pets)
+  end
+
+  def list_pets
+    @client_pets
   end
 
 
 end
 
 class Shelter
-def initialize(name)
-  @name = name
+  def initialize(name)
+    @name = name
 
-  @shelter_animals = []
-  @shelter_clients = []
-
+    @shelter_animals = []
+    @shelter_clients = []
   end
 
   def name 
@@ -95,6 +100,54 @@ def initialize(name)
     puts "#{name.capitalize}, aged #{age} years has been added!."
   end
 
+  def adopt_animal
+    puts "Please enter name of animal to be adopted."
+    animal_name = gets.chomp.downcase
+
+      # binding.pry
+
+    puts "Please enter the species of animal to be adopted."
+    animal_species = gets.chomp.downcase
+
+    pet_finder = self.shelter_animals().find do |pet|
+      pet.name == animal_name
+      pet.species == animal_species
+    end
+
+    animal_index = @shelter_animals.index(pet_finder)
+
+    adopted_animal = pet_finder
+
+    self.shelter_animals().delete_at(animal_index)
+
+      # puts @shelter_animals
+
+    puts "Please enter name of client"
+    client_name = gets.chomp.downcase
+
+    puts "Please enter age of client"
+    client_age = gets.chomp.to_i
+
+    client_finder = self.shelter_clients().find do |adopter|
+      adopter.name == client_name
+      adopter.age == client_age
+    end
+
+    found_client = client_finder
+
+    #binding.pry
+
+    found_client.add_pet(adopted_animal)
+
+      # client_index = @shelter_clients.index(client_finder)
+
+      # self.shelter_clients().push(pet_finder)
+  end
+
+  def surrender_animal
+
+  end
+
 
 end
 
@@ -108,6 +161,8 @@ def menu
   puts "(3). New Shelter"
   puts "(4). Display all Animals"
   puts "(5). Display all Clients"
+  puts "(6). Client/Animal Adoption"
+  puts "(7). Client/Animal Surrender"
   puts "(Q). to Quit"
 end
 
@@ -159,6 +214,14 @@ until response == "q"
 
     menu
     response = gets.chomp.downcase
+
+  when "6"
+    new_shelter.adopt_animal
+
+    menu
+    response = gets.chomp.downcase
+
+  when "7"
 
   else 
     puts "That is not a valid option! Please try again."
