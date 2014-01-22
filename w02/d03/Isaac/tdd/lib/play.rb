9ec08be36598ceee721ef98cf3fec5deb68828ba
My Name <myemail@example.com>
2014-01-22 16:44:05 -0500

    commit

--- w02/d03/Isaac/tdd/lib/play.rb
+++ w02/d03/Isaac/tdd/lib/play.rb
@@ -1,5 +1,5 @@
-def add(num1, num2)
-  num1 + num2
+def add(a, b)
+  return a + b
 end
 
 def odds(numbers)
@@ -9,12 +9,24 @@ def odds(numbers)
 end
 
 def palindrome?(word)
-  uniform = word.downcase.chomp.strip
+  uniform = word.downcase.strip
   return uniform == uniform.reverse && uniform.size > 1
 end
 
 def rps(player_one, player_two)
-  beats = { scissors: "rock", paper: "scissors", rock: "paper" }
-  
+  beats = {
+    "scissors" => "rock",
+    "paper" => "scissors",
+    "rock" => "paper"
+  }
 
+  if beats[player_one[:play]] == player_two[:play]
+    return "#{player_two[:name]} wins!"
+  else
+    return "#{player_one[:name]} wins!"
+  end
 end
+
+
+
+
