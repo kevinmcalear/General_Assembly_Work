7517147da9142eeb351ac91e6e4ea6620352e293
My Name <myemail@example.com>
2014-01-23 00:16:42 -0500

    Homework

---
+++ w02/d03/Tahira/mlk_lab/lib/dream.rb
@@ -0,0 +1,90 @@
+class Nation
+  def initialize()
+    @states = Array.new
+    @states = [1]
+  end
+
+  def judge_by(attribute)
+      if attribute == :color
+        return false
+      else 
+        return true
+      end 
+  end
+
+  def join_hands(white, black)
+    return @states
+  end
+
+  def creed
+    return "all men are created equal"
+  end
+
+  def rise_up?
+    return true
+  end
+
+end
+
+
+class State
+  def initialize(name)
+    @name = name
+    @white_children = {:ancestors => "slaves", :age => 18, :color => "white"}
+    @black_children = {:ancestors => "slave-owners", :age => 18, :color => "black"}
+    @people = [@white_children, @black_children]
+    @together = Array.new
+    @status = "a state sweltering with the heat of injustice and oppression"
+    ring = false
+    @ringing = ring
+  end
+
+  def name
+    return @name
+  end
+
+  def people
+    return @people
+  end
+
+  def state
+    return
+  end
+
+  def table_of_brotherhood
+    return @together
+  end
+
+  def sit_at_table(white)
+    @together.push(@white_children)
+    @together.push(@black_children)
+  end
+
+  def status
+    return @status
+  end
+
+  def ringing?()
+   return @ringing
+  end
+
+
+  def emancipate
+    @status = "an oasis of freedom and justice"
+  end
+
+end
+
+
+
+class Freedom
+  def ring(state)
+    @states = Array.new
+    @states.push(state)
+    return @states
+  end
+
+  def state
+    state.ringing? = true
+  end
+end
