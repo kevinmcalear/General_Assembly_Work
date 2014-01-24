601242de5d885d2e22a320fbcd6c631c2723d14f
Rory Budnick <rorybudnick@gmail.com>
2014-01-24 08:09:09 -0500

    second commit

--- w02/d04/Rory/gladiator/lib/gladiator.rb
+++ w02/d04/Rory/gladiator/lib/gladiator.rb
@@ -17,30 +17,41 @@ end
 class Arena
   def initialize(arena_name)
     @arena_name = arena_name
-    @gladiator_names = []
+    @gladiators_in_arena = []
   end
 
-  def gladiator_names
-    @gladiator_names  
+  def gladiators_in_arena
+    @gladiators_in_arena
   end
 
   def arena_name
     @arena_name
   end
 
-  def add_gladiator(name)
-    if @gladiator_names.count <2
-      @gladiator_names.push(name)
+  def add_gladiator1(name)
+    if @gladiators_in_arena.count <2
+      @gladiators_in_arena.push(name)
+    else
+      puts "You cannot have more than 2 gladiators"
+    end
+  end
+
+
+  def add_gladiator2(name)
+    if @gladiators_in_arena.count <2
+      @gladiators_in_arena.push(name)
     else
       puts "You cannot have more than 2 gladiators"
     end
   end
 
   def remove_gladiator(name)
-    @gladiator_names.delete(name)
+    @gladiators_in_arena.delete(name)
   end
 
-  def gladiators_fight
+  def gladiators_fight(glad1, glad2)
+    if glad1 == "trident" && glad2 == "spear"
+      puts "#{glad1} 1 wins"
   end
 
   def winner
