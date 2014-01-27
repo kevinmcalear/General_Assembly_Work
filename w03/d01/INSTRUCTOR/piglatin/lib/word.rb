57d811bd05c0b991f3c82a4d77edaaa39d05675a
Peter Lai <peter.yd.lai@gmail.com>
2014-01-27 11:06:18 -0500

    pig latin

--- w03/d01/INSTRUCTOR/piglatin/lib/word.rb
+++ w03/d01/INSTRUCTOR/piglatin/lib/word.rb
@@ -6,4 +6,19 @@ class Word
   def original_word
     return @original_word.downcase
   end
+
+  def piglatinize
+    case original_word[0]
+    when "a", "e", "i", "o", "u"
+      return original_word + "way"
+    else
+      # return original_word.delete(original_word[0]) + original_word[0] + "ay"
+      vowels = ["a", "e", "i", "o", "u"]
+      counter = 0
+      while !vowels.include?(original_word[counter])
+        counter += 1
+      end
+      return original_word[counter..-1] + original_word[0...counter] + "ay"
+    end
+  end
 end
