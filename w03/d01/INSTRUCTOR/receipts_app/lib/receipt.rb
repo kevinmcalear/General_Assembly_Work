e81c5bf481fa069240073c7d98ecc3ae4c1cacf8
h4w5 <philip@h4w5.com>
2014-01-27 16:06:52 -0500

    updates to receipts app

--- w03/d01/INSTRUCTOR/receipts_app/lib/receipt.rb
+++ w03/d01/INSTRUCTOR/receipts_app/lib/receipt.rb
@@ -1,4 +1,6 @@
 class Receipt
+  
+  @@receipts = []
 
   def initialize(store, item, quantity, price, date)
     @store = store
@@ -6,6 +8,8 @@ class Receipt
     @quantity = quantity
     @price = price
     @date = date
+
+    @@receipts << self
   end
 
   def store
@@ -30,6 +34,35 @@ class Receipt
 
   def to_s
     return "Receipt: #{@store}, #{@item}, #{@quantity}, #{@price}, #{@date}"
+  end
+
+  def self.all
+    return @@receipts
+  end
+
+  def self.clear
+    @@receipts = []
+  end
+
+  def self.save_all(path)
+    f = File.new(path,"w+")
+
+    @@receipts.each do |receipt|
+      f.puts "#{receipt.store}\t#{receipt.item}\t#{receipt.quantity}\t#{receipt.price}\t#{receipt.date}"
+    end
+
+    f.close
+  end
+
+  def self.read_all(path)
+    f = File.new(path,"a+")
+
+    f.each do |line|
+      line_array = line.split("\t")
+      Receipt.new(line_array[0],line_array[1],line_array[2],line_array[3],line_array[4])
+    end
+
+    f.close
   end
 
 end
