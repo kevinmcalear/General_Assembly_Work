d4902363b5c546f5337a3cd5be109c89f6b29f91
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-01-28 00:06:39 -0500

    Create Grammys

---
+++ w03/d01/Michael/grammys/lib/main.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require_relative "grammy.rb"
+FILE_PATH = "lib/grammys.csv"
+
+system "clear"
+
+puts "=========================="
+puts "Welcome to the Grammys App"
+puts "=========================="
+
+begin
+
+
+puts "[1] Add a Grammy"
+puts "[2] List all Grammys"
+puts "[3] Delete a Grammy (This option will list all the Grammys with number indices, and you choose which one to delete by entering the index.)"
+puts "[4] Quit" 
+
+  choice = gets.chomp.downcase
+
+  if choice == "1"
+    print "What is the Year of the Award? "
+    year = gets.chomp
+    print "What is the Category? "
+    category = gets.chomp
+    print "Who was the winner? "
+    winner = gets.chomp
+
+    Grammy.new(year,category,winner)
+    Grammy.save_all(FILE_PATH)
+  
+  elsif choice == "2"
+    Grammy.view_all(FILE_PATH)
+
+  end 
+
+end while choice != "4" 
