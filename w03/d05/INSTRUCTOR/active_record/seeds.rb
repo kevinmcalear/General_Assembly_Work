bbdcb509dfe63ec1ca5faf30d0010537f58df764
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2014-02-02 20:59:48 -0500

    active record

---
+++ w03/d05/INSTRUCTOR/active_record/seeds.rb
@@ -0,0 +1,11 @@
+require_relative './play'
+
+Musical.create(
+  [
+    { title: "The Sound of Music", composer: "Richard Rodgers", lyricist: "Oscar Hammerstein", year: 1959 },
+    { title: "Babes in Arms", composer: "Richard Rodgers", lyricist: "Lorenz Hart", year: 1937 },
+    { title: "Sunday in the Park with George", composer: "Stephen Sondheim", lyricist: "Stephen Sondheim", year: 1984 },
+    { title: "West Side Story", composer: "Leonard Bernstein", lyricist: "Stephen Sondheim", year: 1957 },
+    { title: "The Last 5 Years", composer: "Jason Robert Brown", lyricist: "Jason Robert Brown", year: 2001 }
+  ]
+)
