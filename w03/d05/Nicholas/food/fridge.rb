4c4e73363f0754e1de266bf10c62778ddf6783d4
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-02-02 22:43:22 -0500

    Complete HW 03-05

--- w03/d05/Nicholas/food/fridge.rb
+++ w03/d05/Nicholas/food/fridge.rb
@@ -21,6 +21,8 @@ end
 class Drink < ActiveRecord::Base
   belongs_to :fridge
 end
+
+
 puts `clear`
 choice = 0 
 while choice != 10
@@ -36,16 +38,20 @@ while choice != 10
   puts " (6) View all the drinks in a fridge"
   puts " (7) Add drinks to a fridge"
   puts " (8) Consume part of a drink"
-  puts " (9) Consume all fo a drink"
+  puts " (9) Consume all of a drink"
   puts " (10) Quit"
   puts ""
   puts "------------------------------------"
 
   print "Enter a menu option: "
   choice = gets.chomp.to_i
+  puts `clear`
 
   if choice == 1
-    Fridge.all.each {|fridge| puts fridge.location}
+    puts "---------------------------"
+    puts "      List of Fridges"
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "---------------------------"
 
   elsif choice == 2
 
@@ -58,21 +64,26 @@ while choice != 10
 
     creation = Fridge.create(location: location, brand: brand, cubic_feet: cubic_feet)
 
-
+    puts "Fridge Created!"
 
   elsif choice == 3
-    Fridge.all.each {|fridge| puts fridge.location}
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
 
-    puts "Choose a fridge to look inside: "
-    response = gets.chomp
+    puts "Choose the number of a fridge to look inside: "
+    response = gets.chomp.to_i
+    fridge = Fridge.find_by_id(response)
 
-    response.food.all.each {|food| puts food.name }
+    selection = Food.where(fridge_id: response)
+    puts "-----------------------------"
+    puts "  List of Food in #{fridge.location} Fridge"
+    selection.each {|food| puts food.name }
+    puts "-----------------------------"
 
 
   elsif choice == 4
-    Fridge.all.each {|fridge| puts fridge.location}
-    puts "Enter the fridge to add food to: "
-    fridge_choice = gets.chomp
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "Enter the number of the fridge to add food to: "
+    fridge_choice = gets.chomp.to_i
 
     puts "Enter the name of the food to add: "
     name = gets.chomp
@@ -80,13 +91,106 @@ while choice != 10
     weight = gets.chomp.to_i
     puts "Is the food vegan? (y or n): "
     vegan = gets.chomp
-    if vegan = y
+    if vegan == "y"
      veg = true
-   else
+    else
     veg = false
-  end
-  fridge_num = Fridge.find_by(fridge_choice)
-  creation = Food.create(name: name, weight: weight, vegan: veg, fridge_id: fridges(fridge_num))
+    end
+
+  creation = Food.create(name: name, weight: weight, vegan: veg, fridge_id: fridge_choice)
+  puts "Food has been added!"
+
+  elsif choice == 5
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "Enter the fridge you'd like to eat from: "
+    fridge_choice = gets.chomp.to_i
+
+    selection = Food.where(fridge_id: fridge_choice)
+    selection.each {|food| puts food.name}
+
+    puts "What would you like to eat? "
+    eat = gets.chomp
+    consume = Food.find_by(name: eat)
+    consume.destroy
+
+    puts "Food has been eaten."
+
+  elsif choice == 6
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+
+    puts "Choose the number of a fridge to look inside: "
+    response = gets.chomp.to_i
+
+    fridge = Fridge.find_by_id(response)
+
+    selection = Drink.where(fridge_id: response)
+    puts "-----------------------------"
+    puts "  List of Drinks in #{fridge.location} Fridge"
+    selection.each {|drink| puts drink.name }
+    puts "-----------------------------"
+
+  elsif choice == 7
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "Enter the number of the fridge to add a drink to: "
+    fridge_choice = gets.chomp.to_i
+
+    puts "Enter the name of the drink to add: "
+    name = gets.chomp
+    puts "Enter the size of the drink: "
+    size = gets.chomp.to_i
+    puts "Is this an alcoholic drink? (y or n): "
+    alchy = gets.chomp
+    if alchy == "y"
+      booze = true
+    else
+      booze = false
+    end
+
+    creation = Drink.create(name: name, size: size, alcoholic: booze, fridge_id: fridge_choice)
+    puts "Drink has been added!"
+
+  elsif choice == 8
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "Enter the fridge you'd like to drink from: "
+    response = gets.chomp.to_i
+    fridge = Fridge.find_by_id(response)
+
+    selection = Drink.where(fridge_id: response)
+    puts "-----------------------------"
+    puts "  List of Drinks in #{fridge.location} Fridge"
+    selection.each {|drink| puts drink.name }
+    puts "-----------------------------"
+
+    puts "What would you like to drink? "
+    beverage = gets.chomp
+    consume = Drink.find_by(name: beverage)
+    puts consume.size
+    puts "Enter an amount to drink: "
+    amount = gets.chomp.to_i
+    subtract_liquid = (consume.size - amount)
+    if subtract_liquid <= 0
+      consume.destroy
+      puts "You drank all of it!"
+    else
+    consume.update(size: subtract_liquid)
+    puts "You drank #{amount} from #{consume.name}"
+    end
+
+
+  elsif choice == 9
+    Fridge.all.each {|fridge| puts "#{fridge.id}. #{fridge.location}"}
+    puts "Enter the fridge you'd like to drink from: "
+    fridge_choice = gets.chomp.to_i
+
+    selection = Drink.where(fridge_id: fridge_choice)
+    selection.each {|drink| puts drink.name}
+
+    puts "What would you like to drink? "
+    beverage = gets.chomp
+    consume = Drink.find_by(name: beverage)
+    consume.destroy
+
+    puts "Drink is gone!"
 
 end
 
