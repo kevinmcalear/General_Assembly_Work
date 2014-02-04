1fd29b3aeb7eee309ea2aac1ca4598b1d9ade441
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-04 10:23:49 -0500

    Create Chopped App

--- w04/d01/Michael/chopped/lib/chopped/judge.rb
+++ w04/d01/Michael/chopped/lib/chopped/judge.rb
@@ -1,3 +1,17 @@
+
+
 class Judge < ActiveRecord::Base
-  self.validates(:name, {presence: true})
+  has_many :dish_ratings
+  self.validates(:name, { presence: true })
+
+  def gives_score(dish, score)
+    dish_id = Dish.find_by(:name => dish.name)[:id]
+    DishRating.create(
+      :score => score, 
+      :dish_id => dish_id
+      # :judge_id =>judge.id
+    )
+
+  end
+
 end
--- w04/d01/Michael/chopped/spec/judge_spec.rb
+++ w04/d01/Michael/chopped/spec/judge_spec.rb
@@ -1,7 +1,11 @@
+require 'pry'
 require 'spec_helper'
 
 describe Judge do
-  subject(:judge) { Judge.new }
+  subject(:judge) { Judge.create(:name => 'Reinhold')}
+
+  it { should validate_presence_of(:name) }
+  it { should have_many(:dish_ratings)}
 
   # it "is invalid without a name" do
   #   expect(judge).to_not be_valid
@@ -9,8 +13,26 @@ describe Judge do
   #   judge.name = "Judge Reinhold"
 
   #   expect(judge).to be_valid
-
   # end
 
-  it {should validate_presence_of(:name)}
+
+
+  describe "#gives_score" do 
+    before do 
+      @course = Course.create(:round => 1, :ingredient => 'sriracha')
+      @chef = Chef.create(:name =>"Jon")
+      @dish = Dish.create(:name => "Octopus Surprise", :course_id => Course.find_by(:round =>1)[:id], :chef_id => Chef.find_by(:name=>"Jon")[:id]
+        )
+
+    end 
+    it "gives a score to a dish" do
+      dish_ratings = judge.gives_score(@dish,10)
+      expect(dish_ratings).to be_a DishRating
+      expect(@dish.dish_ratings).to include(dish_ratings)
+    end 
+
+  end 
+
 end
+
+# binding.pry
