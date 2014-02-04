a57d5acc64be3b423732c1313bb9dfab62e0c1c6
My Name <myemail@example.com>
2014-02-04 18:46:36 -0500

    second commit

--- w04/d02/Quincy/sinatra_calc.rb
+++ w04/d02/Quincy/sinatra_calc.rb
@@ -2,6 +2,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
+calculations = { 1 => ""}
+
 get "/sinatra_calc" do
   "Welcome to your Sinatra Calculator!!"
+end
+
+get "/sinatra_calc/calculator" do
+  calculations.values.join(", ")
+end
+
+get "/sinatra_calc/calculator/:num" do
+  if calculations[:num] == nil
+    puts "Sorry, you haven't done this many calculations!!"
+  else
+    puts calculations[:num]
+end
+
+post "/sinatra_calc/calculator/:oprtn/:num_1/:num_2"
+if :oprtn == "add"
+
+elsif :oprtn == "subtract"
+
+elsif :oprtn == "multiply"
+
+elsif :oprtn == "divide"
+
 end
