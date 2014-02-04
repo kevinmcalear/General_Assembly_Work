ec4107b467244275e2471e8a2785358b0183af33
tunneylax20 <bryantunney@gmail.com>
2014-02-04 18:58:57 -0500

    commit 2- add, sub, mul, div

--- w04/d02/Drew/sinatra_calc.rb
+++ w04/d02/Drew/sinatra_calc.rb
@@ -8,9 +8,62 @@ get '/' do
 end
 
 get '/calculator' do
-
 calculations.values.join(" : ")
+end
+
+get '/calculator/:id' do 
+  id = params[:id].to_i
+  entry = calculations[id]
+  if entry
+    entry
+  else
+    "The ID wasn't found"
+  end
+end
+
+post "/calculator/add" do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  answer = num1.to_i + num2.to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = answer
+
+  "#{num1} + #{num2} = #{answer}"
 
 end
+
+post "/calculator/subtract" do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  answer = num1.to_i - num2.to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = answer
+
+  "#{num1} + #{num2} = #{answer}"
+
+end
+
+post "/calculator/multiply" do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  answer = num1.to_i * num2.to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = answer
+
+  "#{num1} + #{num2} = #{answer}"
+
+end
+
+post "/calculator/divide" do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  answer = num1.to_i / num2.to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = answer
+
+  "#{num1} + #{num2} = #{answer}"
+
+end
+
 
   
