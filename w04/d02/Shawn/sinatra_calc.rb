0cdca9a754d8a27ac1e41c87f83125a33eff1ad5
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-04 18:51:45 -0500

    Step 3: List a specific calculation

--- w04/d02/Shawn/sinatra_calc.rb
+++ w04/d02/Shawn/sinatra_calc.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-calculations = { 1 => "1 + 1 = 2", 2 => "2 * 3 = 6"}
+calculations = { 1 => "1 + 1 = 2", 2 => "2 * 3 = 6", 3 => "6 / 3 = 2"}
 
 get '/' do 
   "Welcome to ∞Calculate∞"
@@ -11,3 +11,15 @@ get '/calculator' do
   # a list of all calculations
   all_calculations = calculations.values.join(", ")
 end
+
+get '/calculator/:id' do 
+  id = params[:id].to_i
+  calculation = calculations[id]
+  if calculation
+    calculation
+  else
+    "SAY WHAT?!?"
+  end
+end
+
+
