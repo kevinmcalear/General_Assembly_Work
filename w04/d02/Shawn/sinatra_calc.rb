01d208570ca1a652c577ff3764313e077ad29d0e
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-04 18:46:10 -0500

    Step 2: Lists all Calculations

--- w04/d02/Shawn/sinatra_calc.rb
+++ w04/d02/Shawn/sinatra_calc.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+calculations = { 1 => "1 + 1 = 2", 2 => "2 * 3 = 6"}
+
 get '/' do 
   "Welcome to ∞Calculate∞"
+end
+
+get '/calculator' do 
+  # a list of all calculations
+  all_calculations = calculations.values.join(", ")
 end
