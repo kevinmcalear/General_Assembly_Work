a7643cbabeae7bd03653ea48bcd4ba8a601d4faf
Rory Budnick <rorybudnick@gmail.com>
2014-02-04 18:38:52 -0500

    wk4 d2 hw round 1

--- w04/d02/Rory/atbash_cipher/lib/cipher.rb
+++ w04/d02/Rory/atbash_cipher/lib/cipher.rb
@@ -1,2 +1,15 @@
 class Cipher
+## 25 - existing = new index strategy
+  def self.encode(plaintext)
+    plain_alphabet = ('a'..'z').to_a
+
+    plaintext_array = plaintext.split('')
+
+    ciphertext_array = plaintext_array.map do |char|
+      plain_index = plain_alphabet.index(char)
+      cipher_index = 25 - plain_index
+      plain_alphabet[cipher_index]
+    end
+      ciphertext_array.join
+end
 end
