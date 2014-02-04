ddf4b474b468cbe40ef149d32ad24c1aa99481e3
My Name <myemail@example.com>
2014-02-04 06:44:51 -0500

    commit changes

---
+++ w04/d01/Quincy/chopped/spec/judge_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Judge do
+  subject(:judge) { Judge.create(name: "Alfred", number: 3) }
+
+  describe "#give_score" do
+
+    it "should return a score for a dish" do
+      expect( judge.give_score("Alfred", 3)).to eq("Alfred", 3)
+
+      # Give a score for a certain dish
+
+    end
+
+  end
+
+  it { should validate_presence_of(:name, :number) }
+end
