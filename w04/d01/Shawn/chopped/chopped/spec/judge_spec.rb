540537b7124737b8f3c2cee2f207b3a4ae5106f9
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-04 08:47:34 -0500

    Complete HW 03-01

--- w04/d01/Shawn/chopped/chopped/spec/judge_spec.rb
+++ w04/d01/Shawn/chopped/chopped/spec/judge_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
 describe Judge do
-  # subject(:judge) { Judge.new }
+  let(:judge) {Judge.create}
+  let(:chef) {Chef.create({name:"Chris"})}
+  let(:course) {Course.create({round: 1, ingredient: "chicken"})}
+  let(:chopping_block) { ChoppingBlock.create }
 
-  # it "is invalid without a name" do
-  #   expect(judge).to_not be_valid
-
-  #   judge.name = "Judge Reinhold" 
-
-  #   expect(judge).to be_valid
-  # end
   it { should validate_presence_of(:name) }
+
+  describe "#rate_dish" do 
+    let(:dish) { Dish.create({name: "Chicken Wings"})}
+    it { should validate_presence_of(:name)}
+    it "rates a dish" do 
+      expect(dish.name).to eq("Chicken Wings")
+      rating = judge.rate_dish(6, judge.id, dish.id)
+      expect(rating).to be_kind_of(DishRating)
+      ratings = DishRating.all 
+      expect(ratings).to include(rating)
+      expect(rating.score).to eq(6)
+    end
+  end
+
+  describe "#chop" do 
+
+      it "removes a chef" do
+        chopped_chef = chef.chop(course, chef)
+        expect(ChoppingBlock.all).to include(chopped_chef)
+      end
+  end
 end
