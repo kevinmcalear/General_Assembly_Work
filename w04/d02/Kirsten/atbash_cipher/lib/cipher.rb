6f7b1727b00bae7ff9cb776689c46d72e61b2f23
kdoyne <kdoyne930@gmail.com>
2014-02-04 18:35:55 -0500

    completed hw

--- w04/d02/Kirsten/atbash_cipher/lib/cipher.rb
+++ w04/d02/Kirsten/atbash_cipher/lib/cipher.rb
@@ -1,2 +1,20 @@
 class Cipher
+  def self.encode (word)
+    alphabet = ("a".."z").to_a
+    letters = word.split(//)
+    index_new = letters.map do |a| 
+      alphabet.index(a)
+    end
+    new_letters = index_new.map do |a|
+      alphabet[-a-1]
+    end
+    new_letters.join
+  end
 end
+
+#   def self.encode(word)
+#     alphabet = ("a".."z").to_a
+#     letters = Hash[alphabet.zip(alphabet.reverse)]
+#     return word.gsub(/./) {|character| letters[character]}
+#   end
+# end
