2eb18d9594ac2e751968298ab66251a99040c9ad
John Hinrichs <jmhinric@gmail.com>
2014-02-03 21:33:54 -0500

    Homework complete

--- w04/d01/John/chopped/lib/chopped/chef.rb
+++ w04/d01/John/chopped/lib/chopped/chef.rb
@@ -1,6 +1,8 @@
 class Chef < ActiveRecord::Base
   has_many :dishes
-
+  has_many :chopping_blocks
+  has_many :courses, :through => :chopping_blocks
+  self.validates(:name, {:presence => true} )
 
 
   def make_dish(course, dish_name)
--- w04/d01/John/chopped/spec/chef_spec.rb
+++ w04/d01/John/chopped/spec/chef_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe Chef do
-  subject(:chef) { Chef.create }
+  subject(:chef) { Chef.create(:name => "Gordon") }
+
+  it { should have_many(:dishes) }
+  it { should validate_presence_of(:name) }
+  it { should have_many(:chopping_blocks) }
+  it { should have_many(:courses).through(:chopping_blocks) }
 
   describe "#make_dish" do
-let(:course) { Course.create }
+    let(:course) { Course.create(:round => 1, :ingredient => "octopus") }
 
     it "makes a dish for a particular course" do
       dish = chef.make_dish(course, "Octopus brain")
@@ -15,6 +20,6 @@ let(:course) { Course.create }
 
 
 
-  # it { should validate_presence_of(:name) }
+  
 
 end
