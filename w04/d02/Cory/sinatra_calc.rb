f6c9a8736232db8612b016a6860971f495f95c03
My Name <myemail@example.com>
2014-02-04 17:57:29 -0500

    the claculator page displays all values calculated

--- w04/d02/Cory/sinatra_calc.rb
+++ w04/d02/Cory/sinatra_calc.rb
@@ -2,6 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 require "pry"
 
+calculations = {1 => "5", 2 => "67", 3 => "1623"}
+
 get '/' do
   "Welcome to the Calculator Web App"
+end
+
+get '/calculator' do
+  calculations.values.join(", ")
 end
