94d7534423b742727c4a83e909d287e827746061
github_name <github_email>
2014-02-04 19:57:11 -0500

    HW 04 02 p3

--- w04/d02/Ilias/sinatra_calc.rb
+++ w04/d02/Ilias/sinatra_calc.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 def add(x,y)
   puts x+y
@@ -33,9 +34,35 @@ past_calculations = {
 }
 
 get '/' do
-  "Welcome to your Calculatrice!"
+  "Welcome to your Calculatrice!\n"
 end
 
 get '/calculator' do
   "#{past_calculations.values.join(" | ")}"
+end
+
+get '/calculator/:id' do
+  id = params[:id].to_i
+  #binding.pry
+  if past_calculations[id].nil?
+    "NIL"
+  else
+    "#{past_calculations[id]}"
+  end
+end
+
+get '/calculator/ops/:method/' do
+  #binding.pry
+
+  case params[:method]
+  when "add"
+    "add"
+  when "subtract"
+    "subtract"
+  when "multiply"
+    "multiply"
+  when "divide"
+    "divide"
+  end
+
 end
