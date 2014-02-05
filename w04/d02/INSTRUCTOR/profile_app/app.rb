cdc629768600db4a7c298c81627757c9157bd0a4
h4w5 <philip@h4w5.com>
2014-02-05 15:08:42 -0500

    profile app

--- w04/d02/INSTRUCTOR/profile_app/app.rb
+++ w04/d02/INSTRUCTOR/profile_app/app.rb
@@ -2,4 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-get('/'){"Hello world!"}
+get('/') do
+  #@name = ""
+
+  if params[:name].nil?
+    @name = "world"
+  else
+    @name = params[:name]
+  end
+
+  erb(:profile)
+end
+
+get("/name") do
+  erb(:name)
+end
