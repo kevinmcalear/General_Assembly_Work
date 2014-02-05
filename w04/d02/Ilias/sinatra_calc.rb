a0bf7d014d248f71e21247d232b89d1274cfd2f1
github_name <github_email>
2014-02-04 19:25:53 -0500

    HW 04 02 p2

--- w04/d02/Ilias/sinatra_calc.rb
+++ w04/d02/Ilias/sinatra_calc.rb
@@ -1,6 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def add(x,y)
+  puts x+y
+  return x+y
+end
+
+def sub(x,y)
+  puts x-y
+  return x-y
+end
+
+def mult(x,y)
+  puts x*y
+  return x*y
+end
+
+def div(x,y)
+  puts x/y
+  return x/y
+end
+
+def exp(x,y)
+  puts x ** y
+  return x ** y
+end
+
+past_calculations = {
+  1 => "10",
+  2 => "20",
+  3 => "30"
+}
+
 get '/' do
-  "Hello World!"
+  "Welcome to your Calculatrice!"
+end
+
+get '/calculator' do
+  "#{past_calculations.values.join(" | ")}"
 end
