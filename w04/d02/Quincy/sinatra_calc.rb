1e2c2a19fc1ae8f8f1b62420933ec09675de6cd8
My Name <myemail@example.com>
2014-02-04 22:17:15 -0500

    final commit

--- w04/d02/Quincy/sinatra_calc.rb
+++ w04/d02/Quincy/sinatra_calc.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-calculations = { 1 => ""}
+calculations = { 1 => "5 = 2+ 3" }
 
 get "/sinatra_calc" do
   "Welcome to your Sinatra Calculator!!"
@@ -14,18 +14,33 @@ end
 
 get "/sinatra_calc/calculator/:num" do
   if calculations[:num] == nil
-    puts "Sorry, you haven't done this many calculations!!"
+    "Sorry, you haven't done this many calculations!!"
   else
-    puts calculations[:num]
+    calculations[:num].to_i
+  end
 end
 
-post "/sinatra_calc/calculator/:oprtn/:num_1/:num_2"
-if :oprtn == "add"
-
-elsif :oprtn == "subtract"
-
-elsif :oprtn == "multiply"
-
-elsif :oprtn == "divide"
-
+post "/sinatra_calc/calculator/add" do
+  sum = params[:num_1].to_i + params[:num_2].to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = "#{sum} = #{params[:num_1]} + #{params[:num_2]}"
 end
+
+post "/sinatra_calc/calculator/subtract" do
+  differnce = params[:num_1].to_i - params[:num_2].to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = "#{diffence} = #{params[:num_1]} - #{params[:num_2]}"
+end
+
+post "/sinatra_calc/calculator/multiply" do
+  product = params[:num_1].to_i * params[:num_2].to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = "#{product} = #{params[:num_1]} x #{params[:num_2]}"
+end
+
+post "/sinatra_calc/calculator/divide" do
+  quotient = params[:num_1].to_i / params[:num_2].to_i
+  last_id = calculations.keys.max
+  calculations[last_id + 1] = "#{quotient} = #{params[:num_1]} + #{params[:num_2]}"
+end
+
