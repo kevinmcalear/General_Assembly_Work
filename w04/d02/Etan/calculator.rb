4ae38f0ec6c3ce7edb27fbc76875edad19dfa1e9
My Name <myemail@example.com>
2014-02-04 19:17:00 -0500

    specific calculator entry

--- w04/d02/Etan/calculator.rb
+++ w04/d02/Etan/calculator.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-calculations = { 1 => "4", 2 => "10", 3=> "100"}
+calculations = { -2 => "4", -1 => "10", 0 => "100"}
 
 get "/" do
   "WELCOME TO CALCULATOR"
@@ -10,4 +10,14 @@ end
 
 get "/calculator" do 
   calculations.values.join(", ")
+end
+
+get "/calculator/:id" do
+  id = params[:id].to_i
+  entry = calculations[id]
+  if entry
+    entry
+  else
+    "NONE FOUND YOU DINGUS!!!"
+  end 
 end
