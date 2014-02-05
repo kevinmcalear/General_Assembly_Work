074789d01447570208b9ae7944428230b8fb81f6
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-02-04 19:25:44 -0500

    calculations posting to /calculator

--- w04/d02/Marco/sinatra_calc.rb
+++ w04/d02/Marco/sinatra_calc.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 user_calculations = {}
 
-
 get '/' do
 
   "Welcome to the NEW Calculator -- Brought to you by Sinatra
@@ -16,7 +15,8 @@ get '/calculator' do
     if user_calculations.empty? == true
       "There are no calulations yet! Please add some NOW."
     else
-      user_calculations.values.join('\n')
+      # binding.pry
+      user_calculations.values.join("\n")
     end
 end
 
@@ -31,6 +31,17 @@ get '/calculator/:id' do
   end
 end
 
-get '/add/:num1/:num2' do
+get '/calculator/add/:num1/:num2' do
   "#{params[:num1]} + #{params[:num2]} = #{params[:num1].to_i + params[:num2].to_i}"
+end
+
+post '/calculator/add' do
+  last_id = user_calculations.keys.max
+  first_num = params[:num1].to_i
+  second_num  = params[:num2].to_i
+  if user_calculations.empty? == true
+    user_calculations[1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+  else
+    user_calculations[last_id + 1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+  end
 end
