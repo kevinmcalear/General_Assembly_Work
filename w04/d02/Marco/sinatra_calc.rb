5c7b51717f2ed292bc65eb16bb1abe6bef529043
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-02-04 21:06:41 -0500

    dried up code and everything is working great

--- w04/d02/Marco/sinatra_calc.rb
+++ w04/d02/Marco/sinatra_calc.rb
@@ -35,47 +35,72 @@ get '/calculator/add/:num1/:num2' do
   "#{params[:num1]} + #{params[:num2]} = #{params[:num1].to_i + params[:num2].to_i}"
 end
 
-post '/calculator/add' do
+post '/calculator/:operator' do
   last_id = user_calculations.keys.max
   first_num = params[:num1].to_i
   second_num  = params[:num2].to_i
+
+  case params[:operator]
+  when "add"
+    user_option = "+"
+  when "subtract"
+    user_option = "-"
+  when "multiply"
+    user_option = "*"
+  when "divide"
+    user_option = "/"
+  end
+
+  equation_to_eval = "#{first_num} #{user_option} #{second_num}"
+
   if user_calculations.empty? == true
-    user_calculations[1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+    user_calculations[1] = "#{equation_to_eval} = #{eval(equation_to_eval)}<br>"
   else
-    user_calculations[last_id + 1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+    user_calculations[last_id + 1] = "#{equation_to_eval} = #{eval(equation_to_eval)}<br>"
   end
 end
 
-post '/calculator/subtract' do
-  last_id = user_calculations.keys.max
-  first_num = params[:num1].to_i
-  second_num  = params[:num2].to_i
-  if user_calculations.empty? == true
-    user_calculations[1] = "#{first_num} - #{second_num} = #{first_num - second_num}<br>"
-  else
-    user_calculations[last_id + 1] = "#{first_num} - #{second_num} = #{first_num - second_num}<br>"
-  end
-end
+# post '/calculator/add' do
+#   last_id = user_calculations.keys.max
+#   first_num = params[:num1].to_i
+#   second_num  = params[:num2].to_i
+#   if user_calculations.empty? == true
+#     user_calculations[1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+#   else
+#     user_calculations[last_id + 1] = "#{first_num} + #{second_num} = #{first_num + second_num}<br>"
+#   end
+# end
 
-post '/calculator/multiply' do
-  last_id = user_calculations.keys.max
-  first_num = params[:num1].to_i
-  second_num  = params[:num2].to_i
-  if user_calculations.empty? == true
-    user_calculations[1] = "#{first_num} x #{second_num} = #{first_num * second_num}<br>"
-  else
-    user_calculations[last_id + 1] = "#{first_num} x #{second_num} = #{first_num * second_num}<br>"
-  end
-end
+# post '/calculator/subtract' do
+#   last_id = user_calculations.keys.max
+#   first_num = params[:num1].to_i
+#   second_num  = params[:num2].to_i
+#   if user_calculations.empty? == true
+#     user_calculations[1] = "#{first_num} - #{second_num} = #{first_num - second_num}<br>"
+#   else
+#     user_calculations[last_id + 1] = "#{first_num} - #{second_num} = #{first_num - second_num}<br>"
+#   end
+# end
 
-post '/calculator/divide' do
-  last_id = user_calculations.keys.max
-  first_num = params[:num1].to_i
-  second_num  = params[:num2].to_i
-  if user_calculations.empty? == true
-    user_calculations[1] = "#{first_num} / #{second_num} = #{first_num / second_num}<br>"
-  else
-    user_calculations[last_id + 1] = "#{first_num} / #{second_num} = #{first_num / second_num}<br>"
-  end
-end
+# post '/calculator/multiply' do
+#   last_id = user_calculations.keys.max
+#   first_num = params[:num1].to_i
+#   second_num  = params[:num2].to_i
+#   if user_calculations.empty? == true
+#     user_calculations[1] = "#{first_num} x #{second_num} = #{first_num * second_num}<br>"
+#   else
+#     user_calculations[last_id + 1] = "#{first_num} x #{second_num} = #{first_num * second_num}<br>"
+#   end
+# end
+
+# post '/calculator/divide' do
+#   last_id = user_calculations.keys.max
+#   first_num = params[:num1].to_i
+#   second_num  = params[:num2].to_i
+#   if user_calculations.empty? == true
+#     user_calculations[1] = "#{first_num} / #{second_num} = #{first_num / second_num}<br>"
+#   else
+#     user_calculations[last_id + 1] = "#{first_num} / #{second_num} = #{first_num / second_num}<br>"
+#   end
+# end
 
