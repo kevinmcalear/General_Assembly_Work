8b919857d168eae59275ab8f0d71f17a43221eca
Kevin McAlear <kevin.mcalear@gmail.com>
2014-02-04 20:38:25 -0500

    finished homework, it is super manly

--- w04/d02/Kevin/calulator.rb
+++ w04/d02/Kevin/calulator.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-past_calculations = { 1 => "one", 2 => "two", 3 => "three"}
+past_calculations = { 1 => "Your Calculation Was: <span style=\"color: purple;\">7 + 4 = 11</span><br><br>", 2 => "Your Calculation Was: <span style=\"color: purple;\">7 * 3 = 21</span><br><br>", 3 => "Your Calculation Was: <span style=\"color: purple;\">7 - 7 = 0</span><br><br>"}
 
 get '/'  do
   '<body style ="background: pink; text-align: center;">
   <div style="margin-top: 50px;">
-  <a style="background: purple; color: white; padding: 10px; border-radius: 9px; margin: 25px;"  href="http://localhost:4567/banana"> Sup</a>
-  <a style="background: purple; color: white; padding: 10px; border-radius: 9px; margin: 25px;" href="http://localhost:4567/banana"> Sup</a>
-  <a style="background: purple; color: white; padding: 10px; border-radius: 9px; margin: 25px;" href="http://localhost:4567/banana"> Sup</a></div>
+  <a style="background: purple; color: white; padding: 10px; border-radius: 9px; margin: 25px;"  href="http://localhost:4567/calculator">   See Your Recipts   </a>
+  </div>
   <h1 style="font-style: italic; margin-top: 55px; margin-bottom: 25px;">Welcome To The Calculator My Friend.</h1>
   <p style="color: gray; margin-bottom: 25px;">A calculator for real mean.</p>
   <img src="http://upload.wikimedia.org/wikipedia/en/0/0a/NERD.GIF">
@@ -17,7 +16,14 @@ get '/'  do
 end
 
 get "/calculator" do
-  past_calculations.values.join(", ")
+  "<body style =\"background: pink; text-align: center;\">
+  <div style=\"margin-top: 50px;\">
+  <a style=\"background: purple; color: white; padding: 10px; border-radius: 9px; margin: 25px;\"  href=\"http://localhost:4567/\">   Go Back Home   </a>
+  </div>
+  <h1 style=\"font-style: italic; margin-top: 55px; margin-bottom: 25px;\">Your Recipts My Friend.</h1>
+  #{past_calculations.values.join(" ")}
+
+  </body>"
 
 end
 
@@ -27,4 +33,28 @@ get "/calculator/:id" do
   else
     "Doesn't exist man. Sorry!"
   end
+end
+
+post "/calculator/add" do
+  result = params[:num1].to_f + params[:num2].to_f
+  last_id = past_calculations.keys.max 
+  past_calculations[ last_id + 1] = "Your Calculation Was: <span style=\"color: purple;\">#{params[:num1]} + #{params[:num2]} = #{result}</span><br><br>"
+end
+
+post "/calculator/subtract" do
+  result = params[:num1].to_f - params[:num2].to_f
+  last_id = past_calculations.keys.max 
+  past_calculations[ last_id + 1] = "Your Calculation Was: <span style=\"color: purple;\">#{params[:num1]} + #{params[:num2]} = #{result}</span><br><br>"
+end
+
+post "/calculator/multiply" do
+  result = params[:num1].to_f * params[:num2].to_f
+  last_id = past_calculations.keys.max 
+  past_calculations[ last_id + 1] = "Your Calculation Was: <span style=\"color: purple;\">#{params[:num1]} + #{params[:num2]} = #{result}</span><br><br>"
+end
+
+post "/calculator/divide" do
+  result = params[:num1].to_f / params[:num2].to_f
+  last_id = past_calculations.keys.max 
+  past_calculations[ last_id + 1] = "Your Calculation Was: <span style=\"color: purple;\">#{params[:num1]} + #{params[:num2]} = #{result}</span><br><br>"
 end
