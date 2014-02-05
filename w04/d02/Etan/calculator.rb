95a06bd4ccd0029e4e85f60e8b5ebbb7d8fe903e
My Name <myemail@example.com>
2014-02-04 19:11:15 -0500

    calculator prints all previous calculations

--- w04/d02/Etan/calculator.rb
+++ w04/d02/Etan/calculator.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
+calculations = { 1 => "4", 2 => "10", 3=> "100"}
+
 get "/" do
   "WELCOME TO CALCULATOR"
+end
+
+get "/calculator" do 
+  calculations.values.join(", ")
 end
