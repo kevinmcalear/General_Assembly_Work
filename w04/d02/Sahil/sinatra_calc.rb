ac94b4205896c66f05a6107e3d1177b116c7d0ab
My Name <myemail@example.com>
2014-02-04 22:54:03 -0500

    2nd and 3rd part + fix

--- w04/d02/Sahil/sinatra_calc.rb
+++ w04/d02/Sahil/sinatra_calc.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get "/calculator" do
+calculations = ["first","second","third"]
+
+get "/" do
   "Welcome to the Calculator"
+end
+
+get "/calculator" do
+  calculations
+  # calculations.each do |calc|
+  #  s = calc + ", "
+  #  end
+end
+
+get "/calculator/:id" do
+  id = params[:id].to_i - 1
+  calculations[id]
 end
