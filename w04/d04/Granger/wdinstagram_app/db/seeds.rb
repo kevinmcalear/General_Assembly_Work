de0879d44119e4655485a22a4ba502e9bfa52340
Granger <GrangerAbuhoff@gmail.com>
2014-02-06 18:10:46 -0500

    basic functions done

---
+++ w04/d04/Granger/wdinstagram_app/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Entry.create(author: 'Steve', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Jessica', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Darwin', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Doug', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Zeke', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Justin', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Jasmine', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
+Entry.create(author: 'Angela', photo_url: 'www.google.com', date_taken: DateTime.new(2012, 8, 29, 22, 35, 0))
