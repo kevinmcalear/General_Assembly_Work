2559e6d1931eb7d55d6f27d7d1afd873e03e082f
Kevin McAlear <kevin.mcalear@gmail.com>
2014-02-06 13:44:51 -0500

    clowns clowns clowns

--- w04/d03/Kevin/sin_active/app.rb
+++ w04/d03/Kevin/sin_active/app.rb
@@ -17,39 +17,58 @@ get("/") do
 end
 
 # Read all clowns
-# This will be the Index Action
+# This will be the "Index Action"
 get("/clowns") do
   @clowns = Clown.all
   erb(:'clowns/index')
 end
 
+# This is a "New Action"
+get("/clowns/new") do
+  erb(:'clowns/new')
+end
+
 # Create a clown
-# This is a Create Action
+# This is a "Create Action"
 post("/clowns") do
+  @clown = Clown.create(name: params[:name], talent: params[:talent], is_singer: params[:is_singer])
   erb(:'clowns/show')
 end
 
 # Read a particular clown
-# This is a Show Action
+# This is a "Show Action"
 get("/clowns/:id") do
   @clown = Clown.find_by_id(params[:id])
   erb(:'clowns/show')
 end
 
+# This is a "New Action"
+get("/clowns/:id/edit") do
+  @clown = Clown.find_by_id(params[:id])
+  erb(:'clowns/edit')
+end
+
 # Update a particular clown
-# This is a Update Action
+# This is a "Update Action"
 put("/clowns/:id") do
+  @clown = Clown.find_by_id(params[:id])
+  @clown.update(
+    name: params[:name], 
+    talent: params[:talent], 
+    is_singer: params[:is_singer])
   erb(:'clowns/show')
 end
 
 # Destory a particular clown
-# This is a Destroy Action
+# This is a "Destroy Action"
 delete("/clowns/:id") do
+  @clown = Clown.find_by_id(params[:id])
+  @clown.destroy
   redirect to("/clowns")
 end
 
 # Destory all clowns
-# This is a Destroy Action
+# This is a "Destroy Action"
 delete("/clowns/") do
   redirect to("http://clowns.com")
 end
