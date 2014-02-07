4c9f8505bbb4bb2df5d3afeafc90ba1b470badcf
Zach Brady <zacbra@gmail.com>
2014-02-07 07:48:51 -0500

    hw 4-4

--- w04/d03/Zach_Brady/sin_active/app.rb
+++ w04/d03/Zach_Brady/sin_active/app.rb
@@ -1,54 +1,81 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 require_relative './models/config'
 require_relative './models/clowns'
 
-Clown.create( {
-  name: "John Wayne Gacy", 
-  happiness_level: 0,
-  creepiness_level: 100,
-  talent: "Bringing joy to children with a tourniquet",
-  is_singer: false
-  } )
+# Clown.create({
+#   name: "Shmozo",
+#   happiness_level: 100,
+#   creepiness_level: 0,
+#   talent: "Bringing joy to children.",
+#   is_singer: true
+# })
 
 get("/") do
   erb(:index)
 end
 
 # Read all the clowns
-# Index action
+# Index Action
 get("/clowns") do
   @clowns = Clown.all
   erb(:'clowns/index')
 end
 
+# New Action
+get("/clowns/new") do
+  erb(:'clowns/new')
+end
+
 # Create a clown
 # Create Action
 post("/clowns") do
+  @clown = Clown.create({
+    name: params[:name], 
+    talent: params[:talent], 
+    is_singer: params[:is_singer]
+  })
+
   erb(:'clowns/show')
 end
 
 # Read a particular clown
-# Show action
+# Show Action
 get("/clowns/:id") do
-  @clown = Clown.find_by_id(params[:id])
+  @clown = Clown.find_by(id: params[:id])
   erb(:'clowns/show')
+end
+
+get("/clowns/:id/edit") do
+  @clown = Clown.find_by(id: params[:id])
+  erb(:'clowns/edit')
 end
 
 # Update a particular clown
 # Update Action
 put("/clowns/:id") do
-  erb("/clowns/show")
+  @clown = Clown.find_by(id: params[:id])
+  @clown.update({
+    name: params[:name],
+    talent: params[:talent],
+    is_singer: params[:is_singer]
+    })
+
+  erb(:'clowns/show')
 end
 
 # Delete that clown
-# Destroy action
+# Destroy Action
 delete("/clowns/:id") do
+  @clown = Clown.find_by(id: params[:id])
+  @clown.destroy
   redirect to("/clowns")
 end
 
-# Delete ALL the clowns
+# Delete ALL clowns
+# Destroy Action
 delete("/clowns") do
   redirect to("http://clowns.com")
 end
