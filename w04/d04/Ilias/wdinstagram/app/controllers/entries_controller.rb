5c755b56e6a52bf807706d042602c0f3d751e421
github_name <github_email>
2014-02-06 19:32:53 -0500

    Instagram, here we come :)

--- w04/d04/Ilias/wdinstagram/app/controllers/entries_controller.rb
+++ w04/d04/Ilias/wdinstagram/app/controllers/entries_controller.rb
@@ -5,4 +5,21 @@ class EntriesController < ApplicationController
     render(:index)
   end
 
+  def new
+    render(:new)
+  end
+
+  def create
+    Entry.create(
+      author: params[:author],
+      photo_url: params[:photo_url],
+      date: params[:date]
+    )
+    redirect_to("/entries")
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
 end
