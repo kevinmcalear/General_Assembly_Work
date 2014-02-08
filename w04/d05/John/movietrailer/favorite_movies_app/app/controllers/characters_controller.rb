fefd29c1cb55918d7ed1b067bd517ee4c3bad25f
John Hinrichs <jmhinric@gmail.com>
2014-02-08 17:10:36 -0500

    add new character feature

--- w04/d05/John/movietrailer/favorite_movies_app/app/controllers/characters_controller.rb
+++ w04/d05/John/movietrailer/favorite_movies_app/app/controllers/characters_controller.rb
@@ -1,19 +1,45 @@
 class CharactersController < ActionController::Base
   
-  self.before_action :load_movie, only: [:index, :show, :edit, :update, :destroy]
+  self.before_action :load_movie, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  self.before_action :load_character, only: [:show, :edit]
 
   def index
     @characters = @movie.characters.all
   end
 
   def show
-    @character = @movie.characters.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @character = Character.create(character_params)
+    redirect_to "/movies/#{params[:movie_id]}/characters/#{@character.id}"
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
 
     def load_movie
       @movie = Movie.find(params[:movie_id])
+    end
+
+    def load_character
+      @character = @movie.characters.find(params[:id])
+    end
+
+    def character_params
+      return {
+        name: params[:name],
+        photo_url: params[:photo_url],
+        movie_id: params[:movie_id]
+      }
     end
 
 end
