07afcd5f454d8f93f161d92cba54cb46c6f8b89c
John Hinrichs <jmhinric@gmail.com>
2014-02-08 16:41:36 -0500

    show character for a movie

--- w04/d05/John/movietrailer/favorite_movies_app/app/controllers/characters_controller.rb
+++ w04/d05/John/movietrailer/favorite_movies_app/app/controllers/characters_controller.rb
@@ -1,14 +1,19 @@
 class CharactersController < ActionController::Base
   
-
+  self.before_action :load_movie, only: [:index, :show, :edit, :update, :destroy]
 
   def index
-    @movie = Movie.find(params[:movie_id])
     @characters = @movie.characters.all
+  end
+
+  def show
+    @character = @movie.characters.find(params[:id])
   end
 
   private
 
-
+    def load_movie
+      @movie = Movie.find(params[:movie_id])
+    end
 
 end
