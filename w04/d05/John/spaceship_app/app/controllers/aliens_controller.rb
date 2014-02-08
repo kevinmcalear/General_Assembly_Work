b4cd2517dd051ff4509d76df103a7d23df1cec21
John Hinrichs <jmhinric@gmail.com>
2014-02-08 12:12:00 -0500

    add update alien feature

--- w04/d05/John/spaceship_app/app/controllers/aliens_controller.rb
+++ w04/d05/John/spaceship_app/app/controllers/aliens_controller.rb
@@ -1,27 +1,51 @@
 class AliensController < ActionController::Base
+  
+  self.before_action :load_spaceship, only: [:index, :new, :show, :edit, :update, :destroy]
+  self.before_action :load_alien, only: [:show, :edit, :update, :destroy]
+  self.before_action :load_all_aliens, only: [:index]
+
   def index
-    @spaceship = Spaceship.find(params[:spaceship_id])
-    @aliens = @spaceship.aliens.all
-    render(:index)
   end
 
   def create
-    @alien = Alien.create(
-      name: params[:name],
-      species: params[:species],
-      strength: params[:strength],
-      spaceship_id: params[:spaceship_id])
-
-    redirect_to "/spaceships/#{params[:spaceship_id]}/aliens/#{@alien.id}"
+    @alien = Alien.create(alien_params)
+    redirect_to "/spaceships/#{params[:spaceship_id]}/aliens/#{params[:id]}"
   end
 
   def new
-    @spaceship = Spaceship.find(params[:spaceship_id])
-    render(:new)
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @alien.update(alien_params)
+    redirect_to ("/spaceships/#{@spaceship.id}/aliens/#{params[:id]}")
+  end
+
+  private
+
+  def load_spaceship
     @spaceship = Spaceship.find(params[:spaceship_id])
+  end
+
+  def load_alien
     @alien = @spaceship.aliens.find(params[:id])
+  end
+
+  def load_all_aliens
+    @aliens = @spaceship.aliens.all
+  end
+
+  def alien_params
+    return {
+      name: params[:name],
+      species: params[:species],
+      strength: params[:strength],
+      spaceship_id: params[:spaceship_id]
+    }
   end
 end
