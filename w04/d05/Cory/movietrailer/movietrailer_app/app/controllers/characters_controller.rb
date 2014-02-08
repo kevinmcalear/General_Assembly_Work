08b73955e392f73e6a85b7b65113aef6a62717ca
My Name <myemail@example.com>
2014-02-07 20:39:12 -0500

    setting up characters views and controller

--- w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/characters_controller.rb
+++ w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/characters_controller.rb
@@ -1,2 +1,21 @@
 class CharactersController < ApplicationController
+
+  def index
+    @characters = Character.where(movie_id: params[:movie_id])
+    render(:index)
+  end
+
+  def new
+    render(:new)
+  end
+
+  def create
+    Character.create({name: params[:name], 
+      photo_url: params[:photo_url], 
+      movie_id: params[:movie_id],
+      })
+    redirect_to ("/movies")
+  end
+
+
 end
--- w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
+++ w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
@@ -1,25 +1,44 @@
 class MoviesController < ApplicationController
 
-def index
-  @movies = Movie.all
-  render(:index)
-end
+  def index
+    @movies = Movie.all
+    render(:index)
+  end
 
-def new
-  render(:new)
-end
+  def new
+    render(:new)
+  end
 
-def create
-  Movie.create({title: params[:title], 
-    year: params[:year], 
-    poster_url: params[:poster_url],
-  })
-  redirect_to ("/movies")
-end
+  def create
+    Movie.create({title: params[:title], 
+      year: params[:year], 
+      poster_url: params[:poster_url],
+      })
+    redirect_to ("/movies")
+  end
 
-def show
-  @movie = Movie.find(params[:id])
-  render(:show)
- end
+  def show
+    @movie = Movie.find(params[:id])
+    render(:show)
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update({title: params[:title], 
+      year: params[:year], 
+      poster_url: params[:poster_url]
+      })
+    redirect_to ("/movies/#{params[:movie_id]}")
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to("/movies")
+  end
 
 end
