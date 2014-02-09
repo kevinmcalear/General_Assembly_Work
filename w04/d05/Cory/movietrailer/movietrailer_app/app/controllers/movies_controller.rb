198fdb1e1d278b192f240c39434af64aab1f09ad
My Name <myemail@example.com>
2014-02-08 20:21:00 -0500

    refactored movies controller

--- w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
+++ w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-
+self.before_action(:load_movie, { only: [:show, :edit, :update, :destroy] })
+  
   def index
     @movies = Movie.all
     render(:index)
@@ -18,16 +19,13 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
     render(:show)
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:id])
     @movie.update({title: params[:title], 
       year: params[:year], 
       poster_url: params[:poster_url]
@@ -36,9 +34,12 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to("/movies")
+  end
+
+   def load_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
