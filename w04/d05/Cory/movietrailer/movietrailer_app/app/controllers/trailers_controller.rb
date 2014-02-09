4a926245629a8c524b35c822fa4a2f362a38f827
My Name <myemail@example.com>
2014-02-08 19:16:42 -0500

    can create new trailers using just video id

--- w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/trailers_controller.rb
+++ w04/d05/Cory/movietrailer/movietrailer_app/app/controllers/trailers_controller.rb
@@ -1,2 +1,25 @@
 class TrailersController < ApplicationController
+
+  def index
+    @trailers = Trailer.where(movie_id: params[:movie_id])
+    render(:index)
+  end
+
+  def new
+    render(:new)
+  end
+
+  def create
+      Trailer.create({title: params[:title], 
+      embed_url: params[:embed_url], 
+      movie_id: params[:movie_id],
+      })
+    redirect_to ("/movies/#{params[:movie_id]}/trailers")
+  end
+
+  def show
+    @trailers = Trailer.find(params[:id])
+    render(:show)
+  end
+
 end
