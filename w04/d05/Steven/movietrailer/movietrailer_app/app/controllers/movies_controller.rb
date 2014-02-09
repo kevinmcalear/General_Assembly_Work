f26fe40e5fbe0ffc884971884d9fe549e162d1da
My Name <myemail@example.com>
2014-02-09 13:11:56 -0500

    first movies commit

---
+++ w04/d05/Steven/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    render(:index)
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render(:show)
+  end
+
+
+end
