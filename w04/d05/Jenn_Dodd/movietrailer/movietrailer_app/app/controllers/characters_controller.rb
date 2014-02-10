29c92ed0d70961b371adbda286a5bf9facd967c8
My Name <dodd.jl@gmail.com>
2014-02-09 21:29:19 -0500

    add and view characters

--- w04/d05/Jenn_Dodd/movietrailer/movietrailer_app/app/controllers/characters_controller.rb
+++ w04/d05/Jenn_Dodd/movietrailer/movietrailer_app/app/controllers/characters_controller.rb
@@ -1,1 +1,47 @@
-characters_controller.rb
+class CharactersController < ApplicationController
+
+  def index
+    @movie = Movie.find(params[:movie_id])
+    @characters = @movie.characters.all
+    render(:index)
+  end
+
+  def show
+    @movie = Movie.find(params[:movie_id])
+    @character = @movie.characters.find(params[:id])
+    render(:show)
+  end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    render(:new)
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.characters.create(character_params())
+    redirect_to ("/movies/#{ @movie.id }/characters")
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+
+  def character_params
+    return {
+      name: params[:name],
+      photo_url: params[:photo_url]
+    }
+  end
+
+end
