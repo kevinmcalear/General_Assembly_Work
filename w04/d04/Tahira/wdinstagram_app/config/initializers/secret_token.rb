20e917f3780bf033afab22f24cb973cdbc336058
My Name <myemail@example.com>
2014-02-10 02:04:14 -0500

    HW 4-4

---
+++ w04/d04/Tahira/wdinstagram_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstagramApp::Application.config.secret_key_base = '4dbdb854603283d6320d5868ea7deafc647c839749a0242adbe43b6825da6d6adad90d9ed1d2c48f3a08da075cddcd33dccb0c1a132254a1216f92cdde9f0441'
