7de6980f56dba757d00b68c8a62887ad72c08237
My Name <dodd.jl@gmail.com>
2014-02-09 23:44:03 -0500

    changed movie redirect

--- w04/d05/Jenn_Dodd/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
+++ w04/d05/Jenn_Dodd/movietrailer/movietrailer_app/app/controllers/movies_controller.rb
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     @movie.update(movie_params())
-    redirect_to("/movies")
+    redirect_to("/movies/#{ @movie.id }")
   end
 
   def destroy
