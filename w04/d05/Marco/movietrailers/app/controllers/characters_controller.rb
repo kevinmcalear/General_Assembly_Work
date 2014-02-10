e9ff4f96ea9889de663d3c9c4f45db8af740e5f3
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-02-09 21:13:41 -0500

    can add/update/delete characters and movie trailers -- still having an error once data is submitted, but it does change in the database

--- w04/d05/Marco/movietrailers/app/controllers/characters_controller.rb
+++ w04/d05/Marco/movietrailers/app/controllers/characters_controller.rb
@@ -10,4 +10,41 @@ class CharactersController < ApplicationController
     @char = Character.find(params[:id])
     render(:show)
   end
+
+  def new
+    @movies = Movie.all
+    render(:new)
+  end
+
+  def create
+    char = Character.create(char_params)
+    redirect_to("/movies/#{params[:movie_id]}/characters/#{params[:id]}")
+  end
+
+  def edit
+    @char = Character.find(params[:id])
+    render(:edit)
+  end
+
+  def update
+    @char = Character.find(params[:id])
+    @char.update(char_params)
+    redirect_to("movies/#{params[:movie_id]}/characters/#{@char.id}")
+  end
+
+  def destroy
+    @char = Character.find(params[:id])
+    @char.destroy
+    redirect_to("movies/#{params[:movie_id]}/characters")
+  end
+
+  private
+
+  def char_params
+    return {
+      name: params[:name],
+      photo_url: params[:photo_url],
+      movie_id: params[:movie_id].to_i
+    }
+  end
 end
--- w04/d05/Marco/movietrailers/app/controllers/movies_controller.rb
+++ w04/d05/Marco/movietrailers/app/controllers/movies_controller.rb
@@ -9,4 +9,23 @@ class MoviesController < ApplicationController
     render(:show)
   end
 
+  def new
+    render(:new)    
+  end
+
+  def create
+    Movie.create(movie_params)
+    redirect_to("/movies")
+  end
+
+  private
+
+  def movie_params
+    return {
+      title: params[:title],
+      year: params[:year],
+      poster_url: params[:poster_url]
+    }
+  end
+
 end
