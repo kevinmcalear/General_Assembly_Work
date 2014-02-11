0e597f8ceaba2e37aba6aa844ecd9c8d2595aa44
Granger <GrangerAbuhoff@gmail.com>
2014-02-11 10:20:21 -0500

    refactor anagram

--- w05/d02/Granger/lib/anagram.rb
+++ w05/d02/Granger/lib/anagram.rb
@@ -2,11 +2,7 @@ def anagram(word, possible_matches)
   letters = word.split("")
   matches = []
 
-  possible_matches.each do |possible_match|
-    if letters.sort == possible_match.split("").sort
-      matches << possible_match
-    end
+  possible_matches.select do |possible_match|
+    letters.sort == possible_match.split("").sort
   end
-
-  return matches
 end
