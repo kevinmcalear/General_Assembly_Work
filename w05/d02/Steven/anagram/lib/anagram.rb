dbd59bedc3fdfa4997fd2c18c70c344a76ecf15c
My Name <myemail@example.com>
2014-02-11 22:01:33 -0500

    hw commit

---
+++ w05/d02/Steven/anagram/lib/anagram.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+
+class Anagram 
+
+
+end
+.split
+.sort
+
+
+
+def is_anagram?(word_1, word_2)
+
+end
+
+
+def Anagram(word, array)
+  word.length = n
+  same_length = array.select do |array_word|
+    array_word.length == n
+  end
+
+
+end
+#KEVIN CODE
+
+# def same_letters(word1, word2)
+#   word1.split('').sort == word2.split('').sort
+# end
+
+# def is_anagram(word1, *comparing_words)
+#   comparing_words.select {| word | same_letters(word1, word) }
+# end
