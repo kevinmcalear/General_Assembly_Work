bb1df3a3ffb48ee8622a63aad725c027c17fd642
Kevin McAlear <kevin.mcalear@gmail.com>
2014-02-14 10:20:32 -0500

    updating repos

--- w05/d03/Kevin/hashback.rb
+++ w05/d03/Kevin/hashback.rb
@@ -5,7 +5,13 @@ require 'pry'
 
 def instagram_tag(tag)
 iae = HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=8a67c3b89c51484ea8f6ac75de2bdc01")
-puts iae["data"][0]["images"]["standard_resolution"]["url"]
+return iae["data"][0]["images"]["standard_resolution"]["url"]
 end
 
-instagram_tag("test")
+
+puts "Enter your hashtag."
+input = gets.chomp
+
+result = instagram_tag(input)
+
+puts result 
