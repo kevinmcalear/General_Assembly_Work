7f5873d81f416b700d57be42b6db2feb3a6041e1
Tahira <tahira.adaya@gmail.com>
2014-03-04 15:27:01 -0500

    commit changes

---
+++ w02/d03/Tahira/rspec/lib/fortune.rb
@@ -0,0 +1,36 @@
+#fortune.rb
+class FortuneCookie
+  def initialize
+    @wrapped = true
+    @cracked = false
+  end
+
+  def read
+    if wrapped?
+      return "You need to unwrap this!"
+    elsif cracked?
+      return "You will write lots of good tests."
+    else
+      return "You need to crack this!"
+    end
+  end 
+
+  def wrapped?
+    return @wrapped
+  end
+
+  def unwrap!
+    @wrapped = false
+  end
+
+  def crack!
+    if !wrapped?
+      @cracked = true
+    end
+  end
+
+  def cracked?
+    return @cracked
+  end
+
+end
