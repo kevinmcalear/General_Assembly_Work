adcd9bafb3a63719bf1ec6179491738d9b722c83
Granger <GrangerAbuhoff@gmail.com>
2014-03-06 11:18:25 -0500

    Complete hw: forgot to add decrypt

--- w08/d03/Granger/cypher.js
+++ w08/d03/Granger/cypher.js
@@ -1,7 +1,4 @@
 function cypher(word, codeWord) {
-  this.word = word;
-  this.codeWord = codeWord;
-
   var repeatCodeWord = new Array();
 
   for(var i = 0; i < word.length; i++) {
@@ -9,12 +6,29 @@ function cypher(word, codeWord) {
   }
 
   for (var j = 0; j < repeatCodeWord.length;j++) {
-
     var val = (repeatCodeWord[j].charCodeAt() -96) + (word[j].charCodeAt()-96);
-        console.log(val);
     val = (val % 26) + 95;
     repeatCodeWord[j] = String.fromCharCode(val);
+  }
 
+  return repeatCodeWord.join("");
+}
+
+function decrypt(word, codeWord) {
+  var repeatCodeWord = new Array();
+
+  for(var i = 0; i < word.length; i++) {
+    repeatCodeWord[i] = codeWord[(i % codeWord.length)]
+  }
+
+  for (var j = 0; j < repeatCodeWord.length;j++) {
+    var val = ( (word[j].charCodeAt()-96) - (repeatCodeWord[j].charCodeAt() -96));
+
+    if ( val < 0 ) {
+      val = val + 26;
+    }
+
+    repeatCodeWord[j] = String.fromCharCode((val+97));
   }
 
   return repeatCodeWord.join("");
