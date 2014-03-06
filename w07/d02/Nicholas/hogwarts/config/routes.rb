cca78fb649187ee362a2945acc5485015b4a98fe
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-06 09:05:14 -0500

    finishing work

--- w07/d02/Nicholas/hogwarts/config/routes.rb
+++ w07/d02/Nicholas/hogwarts/config/routes.rb
@@ -1,7 +1,7 @@
 Hogwarts::Application.routes.draw do
  
-root "house#index"
-resource :house, only: [:index, :show]
+root "houses#index"
+resource :houses, only: [:index, :show]
 resources :students, only: [:index, :show]
 
 end
