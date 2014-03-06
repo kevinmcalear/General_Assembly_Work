423a527898eed419f470536dadfb064500f0c940
Zack Stayman <z.stayman@gmail.com>
2014-03-05 20:00:58 -0500

    iterator added

--- w08/d03/Zack_Stayman/vigenere.js
+++ w08/d03/Zack_Stayman/vigenere.js
@@ -1,32 +1,59 @@
 var vigenereEncode = function(message, keyword, method) {
   // set the keyword to the appropriate length
   var array = alphabet();
-
   // get the alphabet as an array
-  var fixedKeyword = keywordLength();
+  var fixedKeyword = keywordLength(message, keyword);
 
-
-  var encrypt = "";
-
-  for( var i = 0; i<message.length; i++){
-    var messageIndex = array.indexOf(message[i]);
-    var keywordIndex = array.indexOf(fixedKeyword[i]);
-    if(method === "encode"){
-      var letter = messageIndex + keywordIndex;  
-      if(letter > 25){
-        letter -= 26;
+  // this if checks to see if you are encoding or decoding
+  if(method === "encode"){
+    return message.split("").map(function(element, index){
+      // finds the index of the encoded letter
+      var letterIndex = array.indexOf(element) + array.indexOf(fixedKeyword[index]);
+      // ensures that the index is between 0 and 25
+      if(letterIndex > 25){
+        letterIndex -= 26;
       };
-    } else {
-      var letter = messageIndex - keywordIndex;
-      if(letter < 0){
-        letter += 26;
+      // returns the encoded letter
+      return array[letterIndex];
+      // joins the array of encoded letters back together
+    }).join("");
+  } else if(method === "decode"){
+    return message.split("").map(function(element, index){
+      var letterIndex = array.indexOf(element) - array.indexOf(fixedKeyword[index]);
+      if(letterIndex < 0){
+        letterIndex += 26;
       };
-    };
-    
-    encrypt += array[letter];
+      return array[letterIndex];
+    }).join("");
+  } else {
+    return "the final argument must be encode or decode";
   };
-  return encrypt;
+
+
 };
+
+//   for( var i = 0; i<message.length; i++){
+//     var messageIndex = array.indexOf(message[i]);
+//     var keywordIndex = array.indexOf(fixedKeyword[i]);
+
+//     if(method === "encode"){
+//       var letter = messageIndex + keywordIndex;  
+//       if(letter > 25){
+//         letter -= 26;
+//       };
+//     } else {
+//       var letter = messageIndex - keywordIndex;
+//       if(letter < 0){
+//         letter += 26;
+//       };
+//     };
+
+//     encrypt += array[letter];
+//   };
+//   return encrypt;
+
+
+// };
 
 var keywordLength = function(message, keyword) {
   var result = "";
