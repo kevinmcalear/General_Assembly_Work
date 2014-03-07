5824895676d7eb2f0451d985b88d7356dc2eccac
tunneylax20 <bryantunney@gmail.com>
2014-03-07 16:29:36 -0500

    8 5 class

--- w08/d04/Drew/dom/app.js
+++ w08/d04/Drew/dom/app.js
@@ -1,20 +1,7 @@
-// My code 
-
-var h1 = document.querySelector("h1")
-var ul = document.createElement("ul")
-var li = document.createElement("li")
-
-h1.insertAdjacentElement("afterend", ul);
-
-ul.insertAdjacentHTML("beforeend", '<li style ="color: red;">Red</li>');
-ul.insertAdjacentHTML("beforeend", '<li style ="color: green;">Green</li>');
-ul.insertAdjacentHTML("beforeend", '<li style ="color: blue;">Blue</li>');
-
-
-// Kevin's Code
-
 console.log("YO");
-var colors = ["red", "blue", "green"];
+var colors = ["indianred", "blue", "green", "salmon", "yellow", "orange", "aqua"];
+
+var rand = colors[Math.floor(Math.random() * colors.length)];
 
 var h1 = document.querySelector("h1");
 
@@ -30,3 +17,54 @@ for( i = 0; i < colors.length; i++) {
   li.style.color = colors[i];
   ul.appendChild(li);
 }
+
+var button = document.querySelector("button")
+
+var clickFunc = function() {
+  document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
+}
+
+var buttonCount = 0;
+
+var clickCount = function() {
+  buttonCount ++
+
+  if (buttonCount === 10) {
+    this.disabled = true;
+  }
+};
+
+
+var input = document.createElement("input")
+input.type = 'text';
+button.insertAdjacentElement('afterend', input)
+
+var div = document.createElement("div")
+button.insertAdjacentElement('afterend', div)
+
+button.addEventListener("click", clickFunc)
+button.addEventListener("click", clickCount)
+
+input.addEventListener("keyup", function(){
+  div.innerText = this.value
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
