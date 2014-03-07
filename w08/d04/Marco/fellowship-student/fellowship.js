76e7fda47bd3ce036604281ffbc4216d0167f3eb
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-06 19:02:41 -0500

    did first two problems in fellowship of the ring assignment

--- w08/d04/Marco/fellowship-student/fellowship.js
+++ w08/d04/Marco/fellowship-student/fellowship.js
@@ -18,10 +18,47 @@ var buddies = [
 var lands = ["The Shire", "Rivendell", "Mordor"];
 
 function makeMiddleEarth(lands) {
+  // select section tag with an id of middle-earth
+  var selectSection = document.querySelector("#middle-earth");
+
+  for(var i = 0; i < lands.length; i++) {
+    var article = document.createElement("article");
+    article.id = lands[i];
+
+    // inside each article tag include an h1 with the name of the land
+    var h1 = document.createElement("h1");
+    h1.innerText = lands[i];
+
+    // append middle-earth to your document body
+    article.insertAdjacentElement('afterBegin', h1);
+
+    // add each land as an article tag
+    selectSection.appendChild(article);
+  }
 }
 
+makeMiddleEarth(lands);
+
 function makeHobbits(hobbits) {
+  // display an unordered list of hobbits in the shire
+  var ul = document.createElement("ul");
+
+  for(var i = 0; i < hobbits.length; i++) {
+    var li = document.createElement("li");
+
+    // give each hobbit a class of hobbit
+    li.className = hobbits[i];
+    li.innerText = hobbits[i];
+    ul.appendChild(li);
+  }
+  
+  var shireID = document.getElementById("The Shire");
+  console.log(shireID);
+  shireID.appendChild(ul);
+  
 }
+
+makeHobbits(hobbits);
 
 function keepItSecretKeepItSafe() {
 }
