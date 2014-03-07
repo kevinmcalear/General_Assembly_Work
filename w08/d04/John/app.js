88679089efc91900b5220bd7b2c963b0877da175
John Hinrichs <jmhinric@gmail.com>
2014-03-07 12:03:34 -0500

    js events

--- w08/d04/John/app.js
+++ w08/d04/John/app.js
@@ -1,6 +1,6 @@
 console.log("YO");
 
-var colors = ["red", "blue", "green", "purple"];
+var colors = ["red", "blue", "green"];
 
 var colorList = document.createElement("ul");
 
@@ -15,3 +15,53 @@ for (var i = 0; i < colors.length; i++) {
 
 var h1 = document.querySelector("h1");
 h1.insertAdjacentElement("afterend", colorList);
+
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+
+// button.addEventListener("click", function(){
+  // console.log("I've been clicked.");
+// });
+
+// OR
+
+var clickFunc = function() {
+  console.log("I'VE BEEN CLICKED!");
+};
+var colorFunc = function() {
+  var randColor = Math.floor(Math.random() * colors.length);
+  body.style.background = (colors[randColor]);
+};
+
+var clicks = 0;
+var clickCount = function() {
+  clicks ++;
+  if (clicks === 10) {
+    this.disabled = true;
+  }
+};
+
+button.addEventListener("click", clickFunc);
+button.addEventListener("click", colorFunc);
+button.addEventListener("click", clickCount);
+
+
+var div = document.createElement("div");
+var p = document.createElement("p");
+div.appendChild(p);
+var input = document.querySelector("input");
+input.insertAdjacentElement("afterend", div);
+
+input.addEventListener("keyup", function(){
+  p.innerText = this.value;
+});
+
+
+
+
+
+
+
+
+
+
