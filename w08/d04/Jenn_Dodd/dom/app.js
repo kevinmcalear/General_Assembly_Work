9c4a82640f17eb014e4e58d1f72ddac6fc3e04a4
My Name <dodd.jl@gmail.com>
2014-03-07 12:02:50 -0500

    morning dom stuff

--- w08/d04/Jenn_Dodd/dom/app.js
+++ w08/d04/Jenn_Dodd/dom/app.js
@@ -1,5 +1,8 @@
 console.log("YO");
 
+var body = document.querySelector("body");
+
+
 var colors = ["red", "blue", "green", "purple", "grey", "lightcoral", "cornflowerblue"];
 
 var ul = document.createElement("ul");
@@ -15,3 +18,63 @@ for (var i = 0; i < colors.length; i++) {
 var h1 = document.querySelector("h1");
 
 h1.insertAdjacentElement('afterend', ul);
+
+var button = document.querySelector("button");
+
+var clickFunc = function() {
+  // setInterval(function(){
+  //   body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  // },200);
+
+  body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+
+};
+
+var buttonCount = 0;
+var clickCount = function() {
+  if (buttonCount > 10) {
+    this.disabled = true;
+  } else {
+    buttonCount += 1;
+  };
+};
+
+var input = document.createElement("input");
+input.type = "text";
+
+var div = document.createElement("div");
+
+button.insertAdjacentElement("afterend", input);
+input.insertAdjacentElement("afterend", div );
+
+var typeFunc = function() {
+  var letters = input.value;
+  textbox.innerHTML= letters;
+};
+
+
+input.addEventListener("keyup", function(){
+  div.innerText = this.value;
+});
+button.addEventListener("click", clickFunc);
+button.addEventListener("click", clickCount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
