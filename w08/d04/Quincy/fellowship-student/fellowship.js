42d6d7deaae931d765a6b592b7afeedc5a3c78f2
Quincy Iheme <qiheme@gmail.com>
2014-03-07 09:09:01 -0500

    Complete HW 08-04

--- w08/d04/Quincy/fellowship-student/fellowship.js
+++ w08/d04/Quincy/fellowship-student/fellowship.js
@@ -18,19 +18,47 @@ var buddies = [
 var lands = ["The Shire", "Rivendell", "Mordor"];
 
 function makeMiddleEarth(lands) {
+  var section = document.querySelector("#middle-earth");
+  for (var i = 0; i < lands.length; i++ ) {
+    var article = document.createElement('article');
+    article.id = lands[i];
+    section.appendChild(article);
+
+    var h1 = document.createElement('h1');
+    h1.innerHTML = lands[i];
+    article.appendChild(h1);
+  }
 }
+
+makeMiddleEarth(lands);
 
 function makeHobbits(hobbits) {
+  var section = document.querySelector("#middle-earth");
+  var ul = document.createElement('ul');
+  for (var i = 0; i < hobbits.length; i++ )
+    var h1 = document.createElement('h1');
+    h1.innerHTML = hobbits[i];
+    ul.appendChild(h1);
+  }
 }
 
-function keepItSecretKeepItSafe() {
-}
+// function keepItSecretKeepItSafe() {
+//   var section = document.querySelector("section");
+//   document.section.createElement('div id=the-ring')
+//   // child of frodo??
+// }
 
-function makeBuddies(buddies) {
-}
+// function makeBuddies(buddies) {
+//   var section = document.querySelector("section");
+//    document.section.createElement('aside');
+//    for (var i = 0; i < hobbits.length; i++ )
+//     document.section.createElement('ul')
 
-function beautifulStranger() {
-}
 
-function forgeTheFellowShip() {
-}
+// }
+
+// function beautifulStranger() {
+// }
+
+// function forgeTheFellowShip() {
+// }
