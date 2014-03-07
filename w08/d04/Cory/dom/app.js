ed082c1cb958a1200b9346931d51d83ad4d97e65
My Name <myemail@example.com>
2014-03-07 12:02:19 -0500

    morning exercise

--- w08/d04/Cory/dom/app.js
+++ w08/d04/Cory/dom/app.js
@@ -1,6 +1,6 @@
 console.log("YO");
 
-var colors = ["red", "blue", "green", "yellow"];
+var colors = ["red", "blue", "green", "yellow", "tomato", "lightgreen"];
 
 // create the ul element
 var colorList = document.createElement("ul");
@@ -18,5 +18,43 @@ var colorList = document.createElement("ul");
 // select where to insert the element
  var h1 = document.querySelector("h1");
  h1.insertAdjacentElement("afterend", colorList);
+
+
+
+var body = document.querySelector("body");
+
+var button = document.querySelector("button");
+
+var colorFunc = function(){
+  body.style.background = colors[Math.floor(Math.random() * colors.length)];
+};
+
+var clickFunc = function(){
+  console.log("I have been clicked");
+};
+
+var buttonCount = 0;
+var clickCount = function(){
+  if(buttonCount == 9) {
+    this.disabled = true;
+  } else {
+    buttonCount += 1;
+  }
+
+};
+
+button.addEventListener("click", clickFunc);
+button.addEventListener("click", colorFunc);
+button.addEventListener("click", clickCount);
+
+
+var input = document.querySelector("input");
+var div = document.querySelector("div");
+
+var text = function(){
+  div.innerText = input.value;
+};
+
+input.addEventListener("keyup", text)
 
 
