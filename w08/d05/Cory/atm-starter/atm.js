703fabb79caa95b4ece35053585de68ea9e18485
My Name <myemail@example.com>
2014-03-08 17:29:20 -0500

    added zero class to zero balance

--- w08/d05/Cory/atm-starter/atm.js
+++ w08/d05/Cory/atm-starter/atm.js
@@ -3,12 +3,14 @@ var depositButton = document.getElementById("checking_deposit");
 var withdrawButton = document.getElementById("checking_withdraw");
 var checkingAmount = document.getElementById("checking_amount");
 var checkingBalance = document.getElementById("checking_balance");
+var checkingAccountDiv = document.getElementById("account1");
 
 // Find elements for savings
 var savingsDepositButton = document.getElementById("savings_deposit");
 var savingsWithdrawButton = document.getElementById("savings_withdraw");
 var savingsAmount = document.getElementById("savings_amount");
 var savingsBalance = document.getElementById("savings_balance");
+var savingsAccountDiv = document.getElementById("account2");
 
 
 // Build functions for checking
@@ -37,26 +39,28 @@ var subtractFromChecking = function() {
     checking_balance.innerText = "$" + "0";
 
   } else {
-    alert("You can't withdraw more than you have! Your savings are almost gone :(");
+    alert("You can't withdraw more than you have!");
   }
+    checkingAccountDiv.classList.add("zero");
 };
 
 // Build functions for savings
 var addToSavings = function() {
   savings_balance.innerText = "$" + (parseInt(savings_balance.innerText.replace("$", "")) +
-    parseInt(savings_amount.value.replace("$", "")));
+  parseInt(savings_amount.value.replace("$", "")));
 };
 
 var subtractFromSavings = function() {
   if((parseInt(savings_balance.innerText.replace("$", "")) -
     parseInt(savings_amount.value.replace("$", ""))) >= 0) {
 
-  savings_balance.innerText = "$" + (parseInt(savings_balance.innerText.replace("$", "")) -
+    savings_balance.innerText = "$" + (parseInt(savings_balance.innerText.replace("$", "")) -
     parseInt(savings_amount.value.replace("$", "")));
   
   } else {
     alert("You can't withdraw more than you have!");
   }
+  savingsAccountDiv.classList.add("zero");
 };
 
 
