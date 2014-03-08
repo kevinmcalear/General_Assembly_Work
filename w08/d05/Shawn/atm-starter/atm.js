8bd86cee100fab55fa202cf12fc7ea17ad631ffb
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-07 23:17:34 -0500

    withdraw/deposit for both accounts method & refactored code

--- w08/d05/Shawn/atm-starter/atm.js
+++ w08/d05/Shawn/atm-starter/atm.js
@@ -1,19 +1,41 @@
+var accounts = document.querySelectorAll("div.account");
+//checking info:
 var checkingBalance = document.querySelector("div.balance#checking_balance");
 var checkingDeposit = document.querySelector("input#checking_deposit");
 var checkingWithdraw = document.querySelector("input#checking_withdraw");
 var checkingInput = document.querySelector("input#checking_amount");
+//savings info:
+var savingsBalance = document.querySelector("div.balance#savings_balance");
+var savingsDeposit = document.querySelector("input#savings_deposit");
+var savingsWithdraw = document.querySelector("input#savings_withdraw");
+var savingsInput = document.querySelector("input#savings_amount");
 
-checkingDeposit.addEventListener("click", function() {
-  var value = checkingBalance.innerText.split("$");
-  var newTotal = parseInt(value[1]) + parseInt(checkingInput.value);
-  checkingBalance.innerText = "$" + newTotal;
+
+//add listener to checking account
+accounts[0].addEventListener("click", function(e){
+  console.log(e.target);
+  if (e.target.id === "checking_withdraw" ) { withdraw(checkingBalance, checkingInput); }
+  if (e.target.id === "checking_deposit" ) { deposit(checkingBalance, checkingInput); } 
 });
-
-checkingWithdraw.addEventListener("click", function() {
-  var value = checkingBalance.innerText.split("$");
-  var newTotal = parseInt(value[1]) - parseInt(checkingInput.value);
+//add listener to savings account
+accounts[1].addEventListener("click", function(e){
+  console.log(e.target);
+  if (e.target.id === "savings_withdraw" ) { withdraw(savingsBalance, savingsInput); }
+  if (e.target.id === "savings_deposit" ) { deposit(savingsBalance, savingsInput); } 
+});
+//account withdraw function  
+var withdraw = function(accountBalance, accountInput) {
+  var value = accountBalance.innerText.split("$");
+  var newTotal = parseInt(value[1]) - parseInt(accountInput.value);
   if(newTotal < 0 ){
-    console.log("this should take from savings");
+    alert("Invalid");
+  } else {
+    accountBalance.innerText = "$" + newTotal;
   }
-  checkingBalance.innerText = "$" + newTotal;
-});
+};
+//account deposit function
+var deposit = function(accountBalance, accountInput) {
+  var value = accountBalance.innerText.split("$");
+  var newTotal = parseInt(value[1]) + parseInt(accountInput.value);
+  accountBalance.innerText = "$" + newTotal;
+};
