c397e782ef768a8f67005d2e9340a2a942674ef6
h4w5 <philip@h4w5.com>
2014-03-09 11:23:24 -0400

    w08d05 refactor into single object

--- w08/d05/INSTRUCTOR/atm-starter/js/spec/atmSpec.js
+++ w08/d05/INSTRUCTOR/atm-starter/js/spec/atmSpec.js
@@ -12,28 +12,30 @@ describe("ATM", function(){
     it("has a savings property", function(){
       expect(account.savings).toBeDefined();
     });
+    it("has a withdraw function", function(){
+      expect(account.hasOwnProperty("withdraw")).toBe(true);
+      expect(account.withdraw).toEqual(jasmine.any(Function));
+    });
+    it("has a deposit function", function(){
+      expect(account.hasOwnProperty("deposit")).toBe(true);
+      expect(account.deposit).toEqual(jasmine.any(Function));
+    });
 
   });
 
-  describe("withdraw", function(){
+  describe("#withdraw", function(){
 
     it("removes money from an account", function(){
       var account = new Account();
       account.checking = 200;
-      account.checking = withdraw.call(account, "checking", 100);
-      expect(account.checking).toBe(100);
-    });
-    xit("can't remove more money than is in the account", function(){
-      var account = new Account();
-      account.checking = 100;
-      account.checking = withdraw.call(account, "checking", 200);
+      account.withdraw("checking", 100);
       expect(account.checking).toBe(100);
     });
     it("shifts money from other accounts in the Account object", function(){
       var account = new Account();
       account.checking = 100;
       account.savings = 100;
-      account.checking = withdraw.call(account, "checking", 150);
+      account.withdraw("checking", 150);
       expect(account.checking).toBe(0);
       expect(account.savings).toBe(50);
     });
@@ -41,17 +43,17 @@ describe("ATM", function(){
       var account = new Account();
       account.checking = 100;
       account.savings = 0;
-      account.checking = withdraw.call(account, "checking", 200);
+      account.withdraw("checking", 200);
       expect(account.checking).toBe(100);
     });
 
   });
 
-  describe("deposit", function(){
+  describe("#deposit", function(){
 
     it("adds money to an account", function(){
       var account = new Account();
-      account.savings = deposit.call(account, "savings", 100);
+      account.deposit("savings", 100);
       expect(account.savings).toBe(100);
     });
 
--- w08/d05/INSTRUCTOR/atm-starter/js/src/atm.js
+++ w08/d05/INSTRUCTOR/atm-starter/js/src/atm.js
@@ -1,25 +1,24 @@
 function Account(){
   this.checking = 0;
   this.savings = 0;
-}
 
-function withdraw(account, amount){
-  if (this[account] >= amount) {
-    this[account] -= amount;
-    return this[account];
-  } else {
-    if (this.savings + this.checking >= amount) {
-      amount -= this[account];
-      this[account] = 0;
-      otherAccount = (account === "savings") ?
-        "checking" : "savings";
-      this[otherAccount] -= amount;  
+  this.withdraw = function(account, amount) {
+    if (this[account] >= amount) {
+      this[account] -= amount;
+    } else {
+      if (this.savings + this.checking >= amount) {
+        amount -= this[account];
+        this[account] = 0;
+        otherAccount = (account === "savings") ?
+          "checking" : "savings";
+        this[otherAccount] -= amount;  
+      }
     }
     return this[account];
   }
-}
 
-function deposit(account, amount){
-  this[account] += amount;
-  return this[account];
+  this.deposit = function(account, amount) {
+    this[account] += amount;
+    return this[account];
+  }
 }
