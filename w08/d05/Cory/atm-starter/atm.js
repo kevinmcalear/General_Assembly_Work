525117b85824d1a123f2dab62b4d541b00f2837d
My Name <myemail@example.com>
2014-03-09 11:53:12 -0400

    all functionality working

--- w08/d05/Cory/atm-starter/atm.js
+++ w08/d05/Cory/atm-starter/atm.js
@@ -3,20 +3,27 @@ var depositButton = document.getElementById("checking_deposit");
 var withdrawButton = document.getElementById("checking_withdraw");
 var checkingAmount = document.getElementById("checking_amount");
 var checkingBalance = document.getElementById("checking_balance");
-var checkingAccountDiv = document.getElementById("account1");
 
 // Find elements for savings
 var savingsDepositButton = document.getElementById("savings_deposit");
 var savingsWithdrawButton = document.getElementById("savings_withdraw");
 var savingsAmount = document.getElementById("savings_amount");
 var savingsBalance = document.getElementById("savings_balance");
-var savingsAccountDiv = document.getElementById("account2");
+
+
+// Starts balance with class zero
+var checkingBalanceClass = checkingBalance.classList.add("zero");
+var savingsBalanceClass = savingsBalance.classList.add("zero");
+
+
 
 
 // Build functions for checking
 var addToChecking = function() {
   checking_balance.innerText = "$" + (parseInt(checking_balance.innerText.replace("$", "")) + 
-    parseInt(checking_amount.value.replace("$", "")));  
+  parseInt(checking_amount.value.replace("$", ""))); 
+
+  checkingBalanceClass = checkingBalance.classList.remove("zero");
 };
 
 var subtractFromChecking = function() {
@@ -39,15 +46,30 @@ var subtractFromChecking = function() {
     checking_balance.innerText = "$" + "0";
 
   } else {
+  
     alert("You can't withdraw more than you have!");
   }
-    checkingAccountDiv.classList.add("zero");
+    // checkingBalanceClass = checkingBalance.classList.add("zero");
+};
+
+var addZeroChecking = function() {
+  if(parseInt(checking_balance.innerText.replace("$", "")) === 0) {
+    checkingBalanceClass = checkingBalance.classList.add("zero");
+  }
+};
+
+var addZeroSavingsFromChecking = function() {
+  if(parseInt(savings_balance.innerText.replace("$", "")) === 0) {
+  savingsBalanceClass = savingsBalance.classList.add("zero");
+  }
 };
 
 // Build functions for savings
 var addToSavings = function() {
   savings_balance.innerText = "$" + (parseInt(savings_balance.innerText.replace("$", "")) +
   parseInt(savings_amount.value.replace("$", "")));
+
+  savingsBalanceClass = savingsBalance.classList.remove("zero");
 };
 
 var subtractFromSavings = function() {
@@ -60,17 +82,26 @@ var subtractFromSavings = function() {
   } else {
     alert("You can't withdraw more than you have!");
   }
-  savingsAccountDiv.classList.add("zero");
+  // savingsBalanceClass = savingsBalance.classList.add("zero");
+};
+
+var addZeroSavings = function() {
+  if(parseInt(savings_balance.innerText.replace("$", "")) === 0) {
+  savingsBalanceClass = savingsBalance.classList.add("zero");
+  }
 };
 
 
 // Add event listeners for checking
 depositButton.addEventListener("click", addToChecking);
 withdrawButton.addEventListener("click", subtractFromChecking);
+withdrawButton.addEventListener("click", addZeroChecking);
+withdrawButton.addEventListener("click", addZeroSavingsFromChecking);
 
 // Add event listeners for savings
 savingsDepositButton.addEventListener("click", addToSavings);
 savingsWithdrawButton.addEventListener("click", subtractFromSavings);
+savingsWithdrawButton.addEventListener("click", addZeroSavings);
 
 
 
