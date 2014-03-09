1276f8d0bfd96c66782149764928de309df0a6ac
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-09 17:53:13 -0400

    Full Unfactored Code

--- w08/d05/Michael/atm-starter/atm.js
+++ w08/d05/Michael/atm-starter/atm.js
@@ -3,32 +3,55 @@
 //Functions
 var depositChecking = function(){
   var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
+  var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))  
+  checking_balance.innerText = "$"+ ( xchecking_balance + xchecking_amount);
+
   var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
-    checking_balance.innerText = "$"+ ( xchecking_balance + xchecking_amount);
+    if (xchecking_balance > 0){
+      checking_balance.classList.remove("zero")
+    };
 };
 
  var depositSavings = function(){
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
   var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
   savings_balance.innerText = "$"+ ( xsavings_balance + xsavings_amount)
+
+  var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
+    if (xsavings_balance > 0){
+      savings_balance.classList.remove("zero")
+    };
+
 };
 
 var withdrawChecking = function(){
   var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
   var xchecking_amount = parseInt(checking_amount.value.replace("$",""))  
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
-  if(xchecking_amount >= xchecking_balance){
+  if(xchecking_amount < xchecking_balance){
     checking_balance.innerText = "$"+ (xchecking_balance - xchecking_amount ) 
-  }else if(xchecking_amount <= ( xsavings_balance + xchecking_balance ) ){
+  }else if(xchecking_amount === xchecking_balance){
+    checking_balance.innerText = "$"+ (xchecking_balance - xchecking_amount )
+    checking_balance.classList.add("zero")
+  }else if(xchecking_amount < ( xsavings_balance + xchecking_balance ) ){
     checking_balance.innerText = "$0",
-    savings_balance.innerText = "$"+ ( xsavings_balance + xchecking_balance )
+    savings_balance.innerText = "$"+ ( xsavings_balance - (xchecking_amount- xchecking_balance) )
+    checking_balance.classList.add("zero")
+  }else if(xchecking_amount === ( xsavings_balance + xchecking_balance ) ){
+    checking_balance.innerText = "$0",
+    savings_balance.innerText = "$"+ ( xsavings_balance - xchecking_amount )
+    checking_balance.classList.add("zero")
+    savings_balance.classList.add("zero")
   };
 };
 var withdrawSavings = function(){
   var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
-  if (xsavings_amount <= xsavings_balance){
+  if (xsavings_amount < xsavings_balance){
     savings_balance.innerText = "$"+ (xsavings_balance - xsavings_amount )
+  }else if(xsavings_amount === xsavings_balance){
+    savings_balance.innerText = "$"+ (xsavings_balance - xsavings_amount )
+    savings_balance.classList.add("zero")
   };
 };
 
