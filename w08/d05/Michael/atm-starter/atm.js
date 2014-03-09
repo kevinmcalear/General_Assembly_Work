b35830553fdcc5673d1d8c5133c0cb180f98d0b5
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-09 13:04:02 -0400

    Refactored Deposits using eventObject

--- w08/d05/Michael/atm-starter/atm.js
+++ w08/d05/Michael/atm-starter/atm.js
@@ -8,16 +8,6 @@ var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
 var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
 
 //Functions
-var depositChecking = function(){
-  var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
-  var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
-  checking_balance.innerText = "$"+ ( xchecking_balance + xchecking_amount)
-};
-var depositSavings = function(){
-  var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
-  var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
-  savings_balance.innerText = "$"+ ( xsavings_balance + xsavings_amount)
-};
 var withdrawChecking = function(){
   var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
   var xchecking_amount = parseInt(checking_amount.value.replace("$",""))  
@@ -36,6 +26,12 @@ var withdrawSavings = function(){
   }
 };
 
+var deposits = function(eventObject){
+  var amount = parseInt( eventObject.value.replace("$","") ) 
+  var balance = parseInt( eventObject.innerText.replace("$","") )
+  eventObject.innerText = "$" + ( balance + amount)
+}
+
 var balanceZero = function(eventObject){
   if(eventObject.innerText=== "$0"){
     eventObject.classList.add("zero")
@@ -47,7 +43,8 @@ var balanceZero = function(eventObject){
 //Event Listeners
 window.onload = balanceZero
 content.addEventListener("change",balanceZero)
-checking_deposit.addEventListener("click",depositChecking)
-savings_deposit.addEventListener("click",depositSavings)
+
+content.addEventListener("click",deposits)
+
 checking_withdraw.addEventListener("click",withdrawChecking)
 savings_withdraw.addEventListener("click",withdrawSavings)
