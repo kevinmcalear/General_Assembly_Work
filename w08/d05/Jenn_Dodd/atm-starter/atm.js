2892ca30cd0309ce5869556d7db7bce91bee42e0
My Name <dodd.jl@gmail.com>
2014-03-09 18:24:25 -0400

    user can deposit into checking accout

---
+++ w08/d05/Jenn_Dodd/atm-starter/atm.js
@@ -0,0 +1,13 @@
+var input = document.querySelector("#checking_amount");
+var depositButton = document.querySelector("#checking_deposit");
+var withdrawButton = document.querySelector("#checking_withdraw");
+var balance = document.querySelector("#checking_balance");
+console.log(balance);
+
+var enterDeposit = function() {
+
+  balance.innerHTML = "$" + input.value;
+};
+
+depositButton.addEventListener("click", enterDeposit);
+console.log(balance);
