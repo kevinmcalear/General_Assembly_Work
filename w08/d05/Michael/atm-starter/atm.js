0e1b643cd616de8c059773c1782db593ed894295
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-09 11:52:19 -0400

    DRY Up event listeners for Zero Class

--- w08/d05/Michael/atm-starter/atm.js
+++ w08/d05/Michael/atm-starter/atm.js
@@ -1,6 +1,8 @@
 //Open Question 1:  It seems that I dont' need to do querySelector for IDs?  Is that correct? 
 //Open Question 2:  How to make more dry? 
 
+//Task 2:  Do event listeners on window load. 
+
 //Variables
 var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
 var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
@@ -31,7 +33,6 @@ var withdrawChecking = function(){
     savings_balance.innerText = "$"+ ( ( xsavings_balance + xchecking_balance )  - xchecking_amount )
   };
 
-
 };
 var withdrawSavings = function(){
   var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
@@ -41,60 +42,29 @@ var withdrawSavings = function(){
   };
 };
 
+var checkingZero = function(){
+  if(checking_balance.innerText === "$0"){
+    checking_balance.className = "balance zero"
+  }else if(checking_balance.innerText != "$0"){
+    checking_balance.className = "balance"
+  }
+};
+
+var savingsZero = function(){
+  if(savings_balance.innerText === "$0"){
+    savings_balance.className = "balance zero"
+  }else if(savings_balance.innerText != "$0"){
+    savings_balance.className = "balance"
+  }
+};
+
 //Event Listeners
 checking_deposit.addEventListener("click",depositChecking)
 savings_deposit.addEventListener("click",depositSavings)
 checking_withdraw.addEventListener("click",withdrawChecking)
 savings_withdraw.addEventListener("click",withdrawSavings)
 
-
-
-//User Story:  Overdraft Protection
-//Step 1:  if withdraw is greater than whats in checking, look at Savings
-//Step 2:  If savings is greater than withdraw,
-//Step 2a:  add checkings to savings
-//Step 2b:  subtract amount from savings. 
-
-
-//User Story:  Change the color of the panel
-//REQ 1:  if the balance is zero, change the class
-//Step 1:  Create an event listener for when either div changes
-//Step 2: Create a function that will change the class to include "zero
-
-
-
-
-
-checking_withdraw.addEventListener("click",function(eventObject){
-  if(checking_balance.innerText === "$0"){
-    checking_balance.className = "balance zero"
-  }else if(checking_balance.innerText != "$0"){
-    checking_balance.className = "balance"
-  }
-});
-
-savings_withdraw.addEventListener("click",function(eventObject){
-  if(savings_balance.innerText === "$0"){
-    savings_balance.className = "balance zero"
-  }else if(savings_balance.innerText != "$0"){
-    savings_balance.className = "balance"
-  }
-});
-
-checking_deposit.addEventListener("click",function(eventObject){
-  if(checking_balance.innerText === "$0"){
-    checking_balance.className = "balance zero"
-  }else if(checking_balance.innerText != "$0"){
-    checking_balance.className = "balance"
-  }
-});
-
-savings_deposit.addEventListener("click",function(eventObject){
-  if(checking_balance.innerText === "$0"){
-    checking_balance.className = "balance zero"
-  }else if(checking_balance.innerText != "$0"){
-    checking_balance.className = "balance"
-  }
-});
-
-
+checking_withdraw.addEventListener("click",checkingZero);
+checking_deposit.addEventListener("click",checkingZero);
+savings_withdraw.addEventListener("click",savingsZero)
+savings_deposit.addEventListener("click",savingsZero)
