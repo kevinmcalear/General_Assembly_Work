62e74df06a69f116d89ab56fe7c16cfcb3067afc
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-09 12:36:24 -0400

    DRY up functions for Zero Class

--- w08/d05/Michael/atm-starter/atm.js
+++ w08/d05/Michael/atm-starter/atm.js
@@ -1,5 +1,4 @@
 //Open Question 1:  It seems that I dont' need to do querySelector for IDs?  Is that correct? 
-//Open Question 2:  How to make more dry? 
 
 //Task 2:  Do event listeners on window load. 
 
@@ -9,52 +8,41 @@ var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
 var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
 var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
 
-var accounts = document.querySelectorAll(".account")
-
 //Functions
 var depositChecking = function(){
   var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
   var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
-  checking_balance.innerText = "$"+ ( xchecking_balance + xchecking_amount)
+  checking_balance.innerText = "$"+ ( xchecking_balance + xchecking_amount),
+  checking_balance.classList.remove("zero")
 };
 var depositSavings = function(){
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
   var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
-  savings_balance.innerText = "$"+ ( xsavings_balance + xsavings_amount)
+  savings_balance.innerText = "$"+ ( xsavings_balance + xsavings_amount),
+  savings_balance.classList.remove("zero")
 };
 var withdrawChecking = function(){
   var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
   var xchecking_amount = parseInt(checking_amount.value.replace("$",""))  
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
-  if (xchecking_amount <= xchecking_balance){
+  if (xchecking_amount < xchecking_balance){
     checking_balance.innerText = "$"+ (xchecking_balance - xchecking_amount ) 
-  }else if(xchecking_amount <= ( xsavings_balance + xchecking_balance ) ){
+  }else if(xchecking_amount===xchecking_balance ){
+    checking_balance.innerText = "$"+ (xchecking_balance - xchecking_amount )
+    checking_balance.classList.add("zero")
+  }else if(xchecking_amount < ( xsavings_balance + xchecking_balance ) ){
     checking_balance.innerText = "$0",
-    savings_balance.innerText = "$"+ ( ( xsavings_balance + xchecking_balance )  - xchecking_amount )
-  };
-
+    savings_balance.innerText = "$"+ ( ( xsavings_balance + xchecking_balance )  - xchecking_amount ),
+    checking_balance.classList.add("zero")
+  }else if(xchecking_amount === ( xsavings_balance + xchecking_balance )){
+    savings_balance.classList.add("zero")
+  }
 };
 var withdrawSavings = function(){
   var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
   var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
   if (xsavings_amount <= xsavings_balance){
     savings_balance.innerText = "$"+ (xsavings_balance - xsavings_amount )
-  };
-};
-
-var checkingZero = function(){
-  if(checking_balance.innerText === "$0"){
-    checking_balance.className = "balance zero"
-  }else if(checking_balance.innerText != "$0"){
-    checking_balance.className = "balance"
-  }
-};
-
-var savingsZero = function(){
-  if(savings_balance.innerText === "$0"){
-    savings_balance.className = "balance zero"
-  }else if(savings_balance.innerText != "$0"){
-    savings_balance.className = "balance"
   }
 };
 
@@ -64,7 +52,3 @@ savings_deposit.addEventListener("click",depositSavings)
 checking_withdraw.addEventListener("click",withdrawChecking)
 savings_withdraw.addEventListener("click",withdrawSavings)
 
-checking_withdraw.addEventListener("click",checkingZero);
-checking_deposit.addEventListener("click",checkingZero);
-savings_withdraw.addEventListener("click",savingsZero)
-savings_deposit.addEventListener("click",savingsZero)
