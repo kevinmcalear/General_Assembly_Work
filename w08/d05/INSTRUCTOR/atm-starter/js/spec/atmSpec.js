19ac3e784db75895b2ce7e559944f44cb58249e1
h4w5 <philip@h4w5.com>
2014-03-08 23:06:03 -0500

    w08d05 create withdraw, deposit

--- w08/d05/INSTRUCTOR/atm-starter/js/spec/atmSpec.js
+++ w08/d05/INSTRUCTOR/atm-starter/js/spec/atmSpec.js
@@ -2,9 +2,28 @@ describe("ATM", function(){
 
   describe("withdraw", function(){
 
-    it("withdraws money from an account", function(){
+    it("removes money from an account", function(){
       var account = 200;
-      withdraw(account,100);
+      account = withdraw(account,100);
+      expect(account).toBe(100);
+    });
+    xit("throws an error on an overdraft", function(){
+      var account = 100;
+      expect(function(){account = withdraw(account, 200);}).toThrow("Overdraft");
+    });
+    it("can't remove more money than is in the account", function(){
+      var account = 100;
+      account = withdraw(account, 200);
+      expect(account).toBe(100);
+    });
+
+  });
+
+  describe("deposit", function(){
+
+    it("adds money to an account", function(){
+      var account = 0;
+      account = deposit(account,100);
       expect(account).toBe(100);
     });
 
--- w08/d05/INSTRUCTOR/atm-starter/js/src/atm.js
+++ w08/d05/INSTRUCTOR/atm-starter/js/src/atm.js
@@ -1,1 +1,12 @@
-atm.js
+function withdraw(account, amount){
+  if (account >= amount) {
+    return account - amount;
+  } else {
+    //throw "Overdraft";
+    return account;
+  }
+}
+
+function deposit(account, amount){
+  return account + amount;
+}
