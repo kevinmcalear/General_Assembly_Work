c4e4c1568dc2f637196315895be4b2a7c8bdb0dc
My Name <myemail@example.com>
2014-03-09 14:23:17 -0400

    hw about halfway

--- w08/d05/Steven/atm-starter/atm.js
+++ w08/d05/Steven/atm-starter/atm.js
@@ -1,13 +1,52 @@
-var depositAmount = document.getElementById("checking_amount");
+var checkingAmount = document.getElementById("checking_amount");
 var checkingBalance = document.getElementById("checking_balance");
-var depositButton = document.getElementById("checking_deposit");
+var checkingDepositButton = document.getElementById("checking_deposit");
+var checkingWithdrawButton = document.getElementById('checking_withdraw');
+
+var savingsAmount = document.getElementById("savings_amount");
+var savingsBalance = document.getElementById("savings_balance");
+var savingsDepositButton = document.getElementById("savings_deposit");
+var savingsWithdrawlButton = document.getElementById("savings_withdraw");
 
 var checkingDeposit = function(){
-  // e.preventDefault();
-   var currentBalance = parseInt(checkingBalance.innerText.replace("$",""));
-   var newAmount = currentBalance + parseInt(depositAmount.value.replace("$",""));
-   checkingBalance.innerText = "$" + newAmount;
-   depositAmount.value = "";
+   var currentCheckingBalance = parseInt(checkingBalance.innerText.replace("$",""));
+   var newCheckingAmount = currentCheckingBalance + parseInt(checkingAmount.value.replace("$",""));
+   checkingBalance.innerText = "$" + newCheckingAmount;
+   checkingAmount.value = "";
 };
 
-depositButton.addEventListener("click", checkingDeposit);
+var checkingWithdrawl = function(){
+  var currentCheckingBalance = parseInt(checkingBalance.innerText.replace("$",""));
+  var newCheckingAmount = currentCheckingBalance - parseInt(checkingAmount.value.replace("$",""));
+  if(newCheckingAmount < 0){
+    checkingBalance.innerText = "$" + 0;
+    var checkingRemainder = Math.abs(newCheckingAmount);
+    
+    // console.log(checkingRemainder);
+  } else { checkingBalance.innerText = "$" + newCheckingAmount;}
+  checkingAmount.value = "";
+};
+
+var savingsDeposit = function(){
+  var currentSavingsBalance = parseInt(savingsBalance.innerText.replace("$",""));
+  var newSavingsAmount = currentSavingsBalance + parseInt(savingsAmount.value.replace("$",""));
+  savingsBalance.innerText = "$" + newSavingsAmount;
+  savingsAmount.value = "";
+
+};
+
+var savingsWithdrawl = function(){
+  var currentSavingsBalance = parseInt(savingsBalance.innerText.replace("$",""));
+  var newSavingsAmount = currentSavingsBalance - parseInt(savingsAmount.value.replace("$",""));
+  if(newSavingsAmount < 0){
+    savingsBalance.innerText = "$" + 0;
+    var savingsRemainder = Math.abs(newSavingsAmount);
+    // console.log(savingsRemainder);
+  } else { savingsBalance.innerText = "$" + newSavingsAmount;}
+  savingsAmount.value = "";
+};
+
+checkingDepositButton.addEventListener("click", checkingDeposit);
+checkingWithdrawButton.addEventListener("click", checkingWithdrawl);
+savingsDepositButton.addEventListener("click", savingsDeposit);
+savingsWithdrawlButton.addEventListener("click", savingsWithdrawl);
