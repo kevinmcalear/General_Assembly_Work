f1545aceb8fa06bb84d220617d3199c98d6bc8f5
My Name <dodd.jl@gmail.com>
2014-03-09 18:31:39 -0400

    a user can deposit into savings

--- w08/d05/Jenn_Dodd/atm-starter/atm.js
+++ w08/d05/Jenn_Dodd/atm-starter/atm.js
@@ -1,13 +1,25 @@
-var input = document.querySelector("#checking_amount");
-var depositButton = document.querySelector("#checking_deposit");
-var withdrawButton = document.querySelector("#checking_withdraw");
-var balance = document.querySelector("#checking_balance");
-console.log(balance);
+// CHECKING
+var inputCheck = document.querySelector("#checking_amount");
+var depositButtonCheck = document.querySelector("#checking_deposit");
+var withdrawButtonCheck = document.querySelector("#checking_withdraw");
+var balanceCheck = document.querySelector("#checking_balance");
 
-var enterDeposit = function() {
+// SAVINGS
 
-  balance.innerHTML = "$" + input.value;
+var inputSavings = document.querySelector("#savings_amount");
+var depositButtonSavings = document.querySelector("#savings_deposit");
+var withdrawButtonSavings = document.querySelector("#savings_withdraw");
+var balanceSavings = document.querySelector("#savings_balance");
+
+var enterCheckingDeposit = function() {
+
+  balanceCheck.innerHTML = "$" + inputCheck.value;
 };
 
-depositButton.addEventListener("click", enterDeposit);
-console.log(balance);
+var enterSavingsDeposit = function() {
+
+  balanceSavings.innerHTML = "$" + inputSavings.value;
+};
+
+depositButtonCheck.addEventListener("click", enterCheckingDeposit);
+depositButtonSavings.addEventListener("click", enterSavingsDeposit);
