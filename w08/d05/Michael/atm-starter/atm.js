b0aa75484a5517eeaebd34a0179271a4152a1191
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-09 13:13:55 -0400

    Removing unnesseary lines

--- w08/d05/Michael/atm-starter/atm.js
+++ w08/d05/Michael/atm-starter/atm.js
@@ -1,11 +1,4 @@
 //Open Question 1:  It seems that I dont' need to do querySelector for IDs?  Is that correct? 
-//Task 2:  Do event listeners on window load. 
-
-//Variables
-var xsavings_balance = parseInt(savings_balance.innerText.replace("$",""))
-var xchecking_balance = parseInt(checking_balance.innerText.replace("$",""))
-var xsavings_amount = parseInt(savings_amount.value.replace("$",""))
-var xchecking_amount = parseInt(checking_amount.value.replace("$",""))
 
 //Functions
 var withdrawChecking = function(){
@@ -43,8 +36,6 @@ var balanceZero = function(eventObject){
 //Event Listeners
 window.onload = balanceZero
 content.addEventListener("change",balanceZero)
-
 content.addEventListener("click",deposits)
-
 checking_withdraw.addEventListener("click",withdrawChecking)
 savings_withdraw.addEventListener("click",withdrawSavings)
