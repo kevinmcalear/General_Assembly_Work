67c7cbf40070c5f7cbc93cca2ebe2f6feec1ccaf
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-09 21:27:34 -0400

    got savings and checking both working along with overdraft protection

--- w08/d05/Marco/atm-starter/atm.js
+++ w08/d05/Marco/atm-starter/atm.js
@@ -1,14 +1,42 @@
-var input = document.querySelector("#checking_amount");
-var depositButton = document.querySelector("#checking_deposit");
-var withdrawButton = document.querySelector("#checking_withdraw");
-var balance = document.querySelector("#checking_balance");
-console.log(balance);
+var checkingInput = document.querySelector("#checking_amount");
+var checkingDepositButton = document.querySelector("#checking_deposit");
+var checkingWithdrawButton = document.querySelector("#checking_withdraw");
+var checkingBalance = document.querySelector("#checking_balance");
 
-var enterDeposit = function() {
+var savingsInput = document.querySelector("#savings_amount");
+var savingsDepositButton = document.querySelector("#savings_deposit");
+var savingsWithdrawButton = document.querySelector("#savings_withdraw");
+var savingsBalance = document.querySelector("#savings_balance");
 
-  balance.innerHTML = input.value;
+var makeCheckingDeposit = function() {
+  if (parseInt(checkingInput.value) > 0) {
+    checkingBalance.innerHTML = "$" + (parseInt(checkingBalance.innerHTML.substring(1)) + parseInt(checkingInput.value));
+  };
 };
 
-depositButton.addEventListener("click", enterDeposit);
-console.log(balance);
+var makeCheckingWithdrawl = function() {
+  if (parseInt(checkingBalance.innerHTML.substring(1)) >= parseInt(checkingInput.value)) {
+    checkingBalance.innerHTML = "$" + (parseInt(checkingBalance.innerHTML.substring(1)) - parseInt(checkingInput.value));
+  } else if ((parseInt(checkingBalance.innerHTML.substring(1)) + parseInt(savingsBalance.innerHTML.substring(1))) >= parseInt(checkingInput.value)) {
+    var overdraftAmount = (parseInt(checkingInput.value) - parseInt(checkingBalance.innerHTML.substring(1)));
+    savingsBalance.innerHTML = "$" + (parseInt(savingsBalance.innerHTML.substring(1)) - overdraftAmount);
+    checkingBalance.innerHTML = "$0";
+  };
+};
 
+var makeSavingsDeposit = function() {
+  if (parseInt(savingsInput.value) > 0) {
+    savingsBalance.innerHTML = "$" + (parseInt(savingsBalance.innerHTML.substring(1)) + parseInt(savingsInput.value));
+  };
+};
+
+var makeSavingsWithdrawl = function() {
+  if (parseInt(savingsBalance.innerHTML.substring(1)) >= parseInt(savingsInput.value)) {
+    savingsBalance.innerHTML = "$" + (parseInt(savingsBalance.innerHTML.substring(1)) - parseInt(savingsInput.value));
+  };
+};
+
+checkingDepositButton.addEventListener("click", makeCheckingDeposit);
+checkingWithdrawButton.addEventListener("click", makeCheckingWithdrawl);
+savingsDepositButton.addEventListener("click", makeSavingsDeposit);
+savingsWithdrawButton.addEventListener("click", makeSavingsWithdrawl);
