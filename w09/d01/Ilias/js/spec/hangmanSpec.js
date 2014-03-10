fd86dc4b02e8e94a6d9baa65276a43ce1ce4b6a4
iliastsangaris <iliastsangaris@gmail.com>
2014-03-10 14:42:56 -0400

    Hangman game, user can start a game and guess letters

--- w09/d01/Ilias/js/spec/hangmanSpec.js
+++ w09/d01/Ilias/js/spec/hangmanSpec.js
@@ -3,29 +3,54 @@
 // a counter keeps track of 
 
 describe ("Hangman", function(){
+  var game = new Hangman("yellow");
 
-  describe("game", function(){
-    var game = new Hangman("yellowpencil");
-
-    it("can return the word", function(){
-      expect(game.word).toBe("yellowpencil");
-    });
-
-    it ("can count the letters in the word", function(){
-      expect(game.wordCount).toBe(12);
-    });
-
-    it ("returns the position of the letter if in the word", function(){
-      expect(game.guess("y")).toBe(0);
-    });
-
-    it ("returns false if the letter is not in the word", function(){
-      expect(game.guess("z")).toBe(false);
-    });
-
-    it ("stores the letters you have guessed", function(){
-      
-    });
-
+  it("can return the word", function(){
+    expect(game.word).toBe("yellow");
   });
+
+  it ("can count the letters in the word", function(){
+    expect(game.wordCount).toBe(6);
+  });
+
+  it ("returns the position of the letter if in the word", function(){
+    expect(game.guess("y")).toBe(0);
+  });
+
+  it ("returns false if the letter is not in the word", function(){
+    expect(game.guess("z")).toBe(false);
+  });
+
+  it ("stores the letters you have guessed", function(){
+    expect(game.incorrectLetters).toContain("y" && "z");
+    expect(game.incorrectLetters).not.toContain("a");
+  });
+
+  it ("ends the game after they have guessed seven times", function(){
+    game.guess("a");
+    game.guess("b");
+    game.guess("c");
+    game.guess("d");
+    expect(game.guess("f")).toBe("You lose!");
+  });
+
+  var game2 = new Hangman("red");
+
+  it("congratulates the user on winning the game when guessing the correct letters", function(){
+    game2.guess("r");
+    game2.guess("e");
+    expect (game2.guess("d")).toBe("You win!");
+  });
+
+  var game3 = new Hangman("green");
+
+  it("finds all letters in the word", function(){
+    game3.guess("g");
+    game3.guess("r");
+    game3.guess("e");
+    expect(game3.guess("n")).toBe("You win!");
+  });
+
+
+
 });
--- w09/d01/Ilias/js/src/hangman.js
+++ w09/d01/Ilias/js/src/hangman.js
@@ -1,11 +1,28 @@
 function Hangman(word){
   this.word = word;
   this.wordCount = word.length;
+  this.incorrectLetters = [];
+  this.correctLetters = new Array(this.word.length);
   this.guess = function(letter){
-    if (word.search(letter) === -1){
-      return false;
+    if (this.word.search(letter) === -1){
+      this.incorrectLetters.push(letter);
+      if (this.incorrectLetters.length === 7){
+        return "You lose!";
+      } else {
+        return false;
+      }
     } else {
-      return word.search(letter);
+      var wordArray = word.split("");
+      for (var i; i < this.word.length; i++ ){
+        if (wordArray[i] === letter) {
+          this.correctLetters[i] = letter;
+        }
+        if (this.correctLetters.join("") === this.word){
+          return "You win!";
+        } else {
+        return letterIndex;
+        }
+      }
     }
   };
 }
