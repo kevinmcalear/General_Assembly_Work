0e21ffd7719f675ed6a9a72a1ec0cb4140a06696
Julia Lovett <julia.m.lovett@gmail.com>
2014-03-09 22:25:25 -0400

    savings account deposits and withdraw

--- w08/d05/Julia/atm-starter/atm.js
+++ w08/d05/Julia/atm-starter/atm.js
@@ -1,9 +1,17 @@
+//Checking Account Queries
 var checkingBalance = document.querySelector("div.balance");
 var depositOrWithdrawInput = document.querySelector("input#checking_amount");
 var depositButton = document.querySelector("input#checking_deposit");
 var checkingWithdrawButton = document.querySelector("input#checking_withdraw");
 
+//Savings Account Queries
+var savingsDepositOrWithdraw = document.querySelector("input#savings_amount");
+var savingsDepositButton = document.querySelector("input#savings_deposit");
+var savingsWithdrawButton = document.querySelector("input#savings_withdraw");
+var savingsBalance = document.querySelector("div#savings_balance");
 
+
+//Checking Account Deposits
 depositButton.addEventListener("click", function(){
 	var checkingBalanceValue = parseInt(checkingBalance.innerText.replace("$", ""));
 	var depositAmount = parseInt(depositOrWithdrawInput.value);
@@ -12,6 +20,7 @@ depositButton.addEventListener("click", function(){
 	checkingBalanceValue = (checkingBalanceValue + depositAmount);
 });
 
+//Checking Account Withdraws
 checkingWithdrawButton.addEventListener("click", function(){
 	var checkingBalanceValue = parseInt(checkingBalance.innerText.replace("$", ""));
 	var withdrawAmount = parseInt(depositOrWithdrawInput.value);
@@ -20,11 +29,22 @@ checkingWithdrawButton.addEventListener("click", function(){
 	checkingBalanceValue = (checkingBalanceValue - withdrawAmount);
 });
 
+//Savings Account Deposits
+savingsDepositButton.addEventListener("click", function(){
+	var savingsBalanceValue = parseInt(savingsBalance.innerText.replace("$",""));
+	var savingsDepositAmount = parseInt(savingsDepositOrWithdraw.value);
+	savingsBalance.innerText = "$" + (savingsBalanceValue + savingsDepositAmount);
+	savingsBalance.value = (savingsBalanceValue + savingsDepositAmount);
+	savingsBalanceValue = (savingsBalanceValue + savingsDepositAmount);
+});
 
+//Savings Account Withdraws
+savingsWithdrawButton.addEventListener("click", function(){
+	var savingsBalanceValue = parseInt(savingsBalance.innerText.replace("$",""));
+	var savingsWithdrawAmount = parseInt(savingsDepositOrWithdraw.value);
+	savingsBalance.innerText = "$" + (savingsBalanceValue - savingsWithdrawAmount);
+	savingsBalance.value = (savingsBalanceValue - savingsWithdrawAmount);
+	savingsBalanceValue = (savingsBalanceValue - savingsWithdrawAmount);
+});
 
-// checkingWithdrawButton.addEventListener("click", function(){
-// 	depositOrWithdrawInput = parseInt(depositOrWithdrawInput.value);
-// 	checkingBalance.innerText = checkingBalance - depositOrWithdrawInput;
-// 	checkingBalance = checkingBalance - depositOrWithdrawInput;
-// })
 
