03e96e4dee06f83506f3f20144d7497d43503bfa
kevinmcalear <kevin.mcalear@gmail.com>
2014-03-09 21:01:50 -0400

    finished first user story

--- w08/d05/Kevin/atm-starter/atm.js
+++ w08/d05/Kevin/atm-starter/atm.js
@@ -1,10 +1,42 @@
-var displayDiv = document.querySelector("#checking_balance");
+// I need to save two variables; one for the checking account amount and one for the savings account and find where they are displayed for later
+var savings = 0;
+var checking = 0;
 
-var inputBox = document.querySelector("#checking_amount");
+var checkingBalance = document.querySelector("#checking_balance");
+var savingsBalance = document.querySelector("#savings_balance");
 
+// I then find the savings and checking input boxes in the DOM
+var checkingInput = document.querySelector("#checking_amount");
+var savingsInput = document.querySelector("#savings_amount");
 
-inputBox.addEventListener('keyup', function() {
+// And their corrisponding "Deposit" and "Withdra" boxes.
+var checkingDeposit = document.querySelector("#checking_deposit");
+var checkingWithdraw = document.querySelector("#checking_withdraw");
 
-	displayDiv.innerText = inputBox.value;
+var savingsDeposit = document.querySelector("#savings_deposit");
+var savingsWithdraw = document.querySelector("#checking_withdraw");
 
+// I have to set up event listeners on the buttons to actually add and take away money
+checkingDeposit.addEventListener('click', function() {	
+	checking += parseInt(checkingInput.value);
+	checkingBalance.innerText = "$" + checking;
+	checkingInput.value = '';
+});
+
+checkingWithdraw.addEventListener('click', function() {
+	checking -= parseInt(checkingInput.value);
+	checkingBalance.innerText = "$" + checking;
+	checkingInput.value = '';
+});
+
+savingsDeposit.addEventListener('click', function() {	
+	savings += parseInt(savingsInput.value);
+	savingsBalance.innerText = "$" + savings;
+	savingsInput.value = '';
+});
+
+savingsWithdraw.addEventListener('click', function() {
+	savings -= parseInt(savingsInput.value);
+	savingsBalance.innerText = "$" + savings;
+	savingsInput.value = '';
 });
