0b65ea2996d6e3615ba0ed878468f34bad295000
Zach Brady <zacbra@gmail.com>
2014-03-10 07:40:02 -0400

    various class ex I forgot to push

--- w08/d05/INSTRUCTOR/fave_films/app.js
+++ w08/d05/INSTRUCTOR/fave_films/app.js
@@ -1,5 +1,9 @@
+// at the beginning variables are declared that correspond to the existing html structures that will (1) receive the product of the function handling user input and (2) be the source of that user input
+
 var ul = document.querySelector("ul");
 var form = document.querySelector("form.new-movie");
+
+// Next a fuction is defined that defines (1) how (2) movie is added to an existing part of the dom tree
 
 var addMovie = function(eventObject){
   eventObject.preventDefault();
@@ -8,12 +12,15 @@ var addMovie = function(eventObject){
   // this refers to the FormElement object
   li.innerText = this.elements["title"].value;
 
+  // This code is responsible for creating and appending a checkbox to the movie that gets added
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   li.appendChild(checkbox);
 
+  // This is where the html string that we've built actually gets added to the DOM tree
   ul.appendChild(li);
 
+  // I don't know what this does, but I suspect it allows us to define li anew? except that I thought declaring var li is enough
   this.reset();
 };
 
