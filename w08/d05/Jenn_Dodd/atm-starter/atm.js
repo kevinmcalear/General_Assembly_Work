59f0c9bbaf25d18c6e32a3074d13d205b77d6313
My Name <dodd.jl@gmail.com>
2014-03-09 22:17:53 -0400

    A user can withdraw and overdraft protection is in place

--- w08/d05/Jenn_Dodd/atm-starter/atm.js
+++ w08/d05/Jenn_Dodd/atm-starter/atm.js
@@ -13,13 +13,42 @@ var balanceSavings = document.querySelector("#savings_balance");
 
 var enterCheckingDeposit = function() {
 
-  balanceCheck.innerHTML = "$" + inputCheck.value;
+  balanceCheck.innerHTML = "$" + (parseInt(balanceCheck.innerHTML.substring(1)) + parseInt(inputCheck.value));
+  inputCheck.value = "";
+};
+
+var enterCheckingWD = function() {
+  var remainder = parseInt(balanceCheck.innerHTML.substring(1)) - parseInt(inputCheck.value)
+  if (parseInt(remainder) < 0 && parseInt(balanceSavings.innerHTML.substring(1)) + parseInt(remainder) < 0) {
+    alert("Can't do that StarFox!");
+  } else if (parseInt(remainder) < 0 && parseInt(balanceSavings.innerHTML.substring(1)) + parseInt(remainder) >= 0){
+    balanceCheck.innerHTML = "$0";
+    balanceSavings.innerHTML = "$" + (parseInt(balanceSavings.innerHTML.substring(1)) + parseInt(remainder));
+  } else {
+    balanceCheck.innerHTML = "$" + (parseInt(balanceCheck.innerHTML.substring(1)) - parseInt(inputCheck.value) );
+  };
+  inputCheck.value = "";
 };
 
 var enterSavingsDeposit = function() {
 
-  balanceSavings.innerHTML = "$" + inputSavings.value;
+  balanceSavings.innerHTML = "$" + (parseInt(inputSavings.value) + parseInt(balanceSavings.innerHTML.substring(1)));
+  inputSavings.value = "";
 };
 
+var enterSavingsWD = function() {
+  var remainder = parseInt(balanceSavings.innerHTML.substring(1)) - parseInt(inputSavings.value)
+  if (parseInt(remainder) < 0 && parseInt(balanceSavings.innerHTML.substring(1)) + parseInt(remainder) < 0) {
+    alert("Can't do that StarFox!");
+  } else if (parseInt(remainder) < 0 && parseInt(balanceCheck.innerHTML.substring(1)) + parseInt(remainder) >= 0){
+    balanceSavings.innerHTML = "$0";
+    balanceCheck.innerHTML = "$" + (parseInt(balanceCheck.innerHTML.substring(1)) + parseInt(remainder));
+  } else {
+    balanceSavings.innerHTML = "$" + (parseInt(balanceSavings.innerHTML.substring(1)) - parseInt(inputSavings.value) );
+  };
+  inputSavings.value = "";
+};
 depositButtonCheck.addEventListener("click", enterCheckingDeposit);
+withdrawButtonCheck.addEventListener("click", enterCheckingWD);
 depositButtonSavings.addEventListener("click", enterSavingsDeposit);
+withdrawButtonSavings.addEventListener("click", enterSavingsWD);
