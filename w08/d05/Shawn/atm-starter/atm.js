d9d12e96572dac18054fe4c87c0366ca1027b8f7
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-10 09:02:19 -0400

    Complete HW 08-05

--- w08/d05/Shawn/atm-starter/atm.js
+++ w08/d05/Shawn/atm-starter/atm.js
@@ -2,30 +2,25 @@
 //Shawn Broukhim
 var accounts = document.querySelectorAll("div.account");
 
-//checking info:
-var checkingBalance = document.querySelector("div.balance#checking_balance");
-var checkingDeposit = document.querySelector("input#checking_deposit");
-var checkingWithdraw = document.querySelector("input#checking_withdraw");
-var checkingInput = document.querySelector("input#checking_amount");
+//account(s) info:
 
-//savings info:
-var savingsBalance = document.querySelector("div.balance#savings_balance");
-var savingsDeposit = document.querySelector("input#savings_deposit");
-var savingsWithdraw = document.querySelector("input#savings_withdraw");
-var savingsInput = document.querySelector("input#savings_amount");
+var accountBalance = [document.querySelector("div.balance#checking_balance"), document.querySelector("div.balance#savings_balance")];
+var accountDeposit = [document.querySelector("input#checking_deposit"), document.querySelector("input#savings_deposit")];
+var accountWithdraw = [document.querySelector("input#checking_withdraw"), document.querySelector("input#savings_withdraw")];
+var accountInput = [document.querySelector("input#checking_amount"), document.querySelector("input#savings_amount")];
 
 //add event listener to checking account
-accounts[0].addEventListener("click", function(e){
-  console.log(e.target);
-  if (e.target.id === "checking_withdraw" ) { withdraw(checkingBalance, checkingInput); }
-  if (e.target.id === "checking_deposit" ) { deposit(checkingBalance, checkingInput); } 
-});
+  accounts[0].addEventListener("click", function(e){
+    console.log(e.target);
+    if (e.target.id === "checking_withdraw" ) { withdraw(accountBalance[0], accountInput[0]); };
+    if (e.target.id === "checking_deposit" ) { deposit(accountBalance[0], accountInput[0]); }; 
+  });
 
 //add event listener to savings account
 accounts[1].addEventListener("click", function(e){
   console.log(e.target);
-  if (e.target.id === "savings_withdraw" ) { withdraw(savingsBalance, savingsInput); }
-  if (e.target.id === "savings_deposit" ) { deposit(savingsBalance, savingsInput); } 
+  if (e.target.id === "savings_withdraw" ) { withdraw(accountBalance[1], accountInput[1]); };
+  if (e.target.id === "savings_deposit" ) { deposit(accountBalance[1], accountInput[1]); };
 });
 
 //account withdraw function  
@@ -36,16 +31,17 @@ var withdraw = function(accountBalance, accountInput) {
   var siblingAmount = parseInt(siblingAccount.innerText.split("$")[1]);
 
   //check if withdrawal amount exceeds balance
-  //if so withdraw from other account  
   if( newTotal <= 0 ) {
     var remainder = Math.abs(newTotal);
     
+    //if so & other account has enough funds
+    //withdraw from other account  
     if( siblingAmount >= remainder ) {
       accountBalance.innerText = "$0";
       var newAmount = siblingAmount - remainder;
       siblingAccount.innerText = "$" + newAmount;
       accountBalance.style.background = "#F52F4F";
-
+      //set sibling background to red if new balance is 0
       if( siblingAccount.innerText === "$0" ) {
         siblingAccount.style.background = "#F52F4F";
       };
