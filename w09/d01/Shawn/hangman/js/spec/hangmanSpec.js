680f5309df990f48639041042c18415f1f53bf2b
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-10 14:12:48 -0400

    some sweet tests

--- w09/d01/Shawn/hangman/js/spec/hangmanSpec.js
+++ w09/d01/Shawn/hangman/js/spec/hangmanSpec.js
@@ -1,25 +1,59 @@
 // hangmanSpec.js
   
 describe("Hangman", function() {
-  var hangman = new Hangman();
-  // hangman.words = ["one", "two", "three", "four", "five"]
+
+  var hangman = new Hangman("basketball");
+
   it("is a game", function() {
     expect(hangman.game()).toBe(true);
+  });
+
+  it("starts with 7 guessesLeft", function() {
+    expect(hangman.guessesLeft).toBe(7);
   })
-  
-  describe("words", function(){
-    it("is a list of words", function() {
-      expect(hangman.words()).toBe(Array)
-    })
-  })  
 
-
-})
-
+  it("can return the word", function() {
+    expect(hangman.word).toBe("basketball");
+  })
 
   describe("countLetters", function() {
     it("takes a work and returns the number of letters", function() {
       var word = "basketball";    
-      expect(countLetters(word)).toBe(10);
-    })
+      expect(hangman.countLetters(word)).toBe(10);
+    });
   });
+
+  describe("checkLetter", function() {
+    xit("takes a letter as an argument and returns the number of times that letter appears in the word", function(){
+      expect(hangman.checkLetter('b', hangman.word)).toBe(2);
+    })
+    it("reveals the correctly guessed letters positions in an array", function() {
+      expect(hangman.checkLetter('b', hangman.word)).toEqual(['b', '_', '_', '_', '_', '_', 'b', '_', '_', '_']);
+    })
+    xit("subtracts one from guesses if word does not include guessed letter & includes the letter in misses array", function() {
+      hangman.checkLetter('c', hangman.word);
+      expect(hangman.guessesLeft).toBe(6);
+      expect('c').toContain(hangman.misses);
+      console.log(hangman.misses);
+    })
+    it("won't let you guess a letter which has already been guessed", function() {
+      hangman.checkLetter('c', hangman.word);
+      expect(hangman.guessesLeft).toBe(6);
+      hangman.checkLetter('c', hangman.word);
+      expect(hangman.guessesLeft).toBe(6);
+    })
+
+    it("ends the game if guessesLeft reaches 0", function() {
+      hangman.checkLetter('c', hangman.word);
+      hangman.checkLetter('d', hangman.word);
+      hangman.checkLetter('x', hangman.word);
+      hangman.checkLetter('y', hangman.word);
+      hangman.checkLetter('z', hangman.word);
+      hangman.checkLetter('u', hangman.word);
+      hangman.checkLetter('f', hangman.word);
+      expect(hangman.game).toBe(false);
+    })
+  
+  }) 
+});
+
--- w09/d01/Shawn/hangman/js/src/hangman.js
+++ w09/d01/Shawn/hangman/js/src/hangman.js
@@ -1,17 +1,46 @@
 // hangman.js
-var Hangman = function() {
+var Hangman = function(word) {
+  this.word = word;
+  this.guessesLeft = 7;
+  this.misses = [];
   
   this.game = function() {
     return true;
   }
-  
-  this.words = function() {
-    return Array;
+
+  this.countLetters = function(word) {
+    var letterCount = word.length;
+    return letterCount;
   }
+
+  this.checkLetter = function(letter, word) {
+    var counter = 0;
+    var misses = [];
+    var wordLetters = word.split("");
+    var blankArray = new Array(wordLetters.length);
+
+    for(var i = 0; i < wordLetters.length; i++) {
+      if(letter === wordLetters[i]){
+        counter++;
+        blankArray[i] = letter;
+      }
+    }
+    if(counter === 0) {
+      this.guessesLeft -= 1;
+      this.misses.push(letter);
+      if(this.guessesLeft === 0) {
+        this.game = false;
+        return this.game;
+      }
+    }
+    for(var i = 0; i < blankArray.length; i++) {
+      if(blankArray[i] === undefined) {
+        blankArray[i] = '_';
+      }
+    }
+    return blankArray;
+  }
+
 }
 
 
-var countLetters = function(word) {
-  var letterCount = word.length;
-  return letterCount;
-}
