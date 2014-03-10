224cc6f80db0cd203451f813f5a4ce5d063e24fd
Zach Brady <zacbra@gmail.com>
2014-03-10 18:08:13 -0400

    Basic Hangman game

---
+++ w09/d01/Zach_Brady/hangman/js/src/hangman.js
@@ -0,0 +1,34 @@
+function Hangman(category){
+  this.categories = {
+    Pokemon: ["PIKACHU", "BULBASAUR", "CHARMELEON", "MACHOP"],
+    Pirates: [],
+    Ninjas: [],
+    FecalMatterSynonyms: []
+  };
+  this.wrongGuesses = 7;
+
+  this.wordToGuess = this.categories[category][Math.floor(Math.random()*this.categories[category].length)];
+
+  this.hiddenWord = "_ "
+  for(i = 0; i < (this.wordToGuess.length-1); i++){
+    this.hiddenWord += "_ ";
+  }
+
+  this.guess = function(letter){
+    this.letter = letter.toUpperCase();
+    if(this.wordToGuess.indexOf(this.letter) == -1) {
+      this.wrongGuesses -= 1;
+      return false;
+    } else {
+      this.hiddenWordArray = this.hiddenWord.split(" ");
+      for (i=0; i < this.wordToGuess.length; i++){
+        if( this.letter === this.wordToGuess[i]) {
+          this.hiddenWordArray[i] = this.letter;
+        }
+        this.hiddenWord = this.hiddenWordArray.join(" ");
+      }
+      return true;
+    };
+  };
+
+};
