a85d44739f2e6df951bbf2e7d86e449922598fe8
kdoyne <kdoyne930@gmail.com>
2014-03-10 18:01:13 -0400

    functioning 2 player mode

--- w09/d01/Kirsten/hangman_starter/js/controller.js
+++ w09/d01/Kirsten/hangman_starter/js/controller.js
@@ -4,13 +4,41 @@ var lettersGuessed = document.querySelector("span.guessed-letters");
 var guessesLeft = document.querySelector("span.guesses-left");
 var input = document.querySelector("#letter");
 var gameWord = document.querySelector("div.game-word");
-
+var gamesWonP1 = document.querySelector("span.games-won-1");
+var gamesLostP1 = document.querySelector("span.games-lost-1");
+var gamesWonP2 = document.querySelector("span.games-won-2");
+var gamesLostP2 = document.querySelector("span.games-lost-2");
+var player1 = document.querySelector("span.player-1");
+var player2 = document.querySelector("span.player-2");
 
 game = new Game();
+gameCount = 0;
+
+var winsP1 = 0;
+var losesP1 = 0;
+gamesWonP1.innerText = winsP1;
+gamesLostP1.innerText = losesP1;
+
+var winsP2 = 0;
+var losesP2 = 0;
+gamesWonP2.innerText = winsP2;
+gamesLostP2.innerText = losesP2;
 
 startGame.addEventListener("click", function(){
   game = new Game();
+  ++gameCount;
+  if(gameCount%2===0){
+    player2.classList.add("current-player");
+    player1.classList.remove("current-player")
+  } else {
+    player1.classList.add("current-player");
+    player2.classList.remove("current-player")
+  };
   console.log(game.word);
+  game.correctGuesses =[];
+  guessesLeft.innerText = 0;
+  game.incorrectGuesses = [];
+  lettersGuessed.innerText = game.incorrectGuesses;
   var dashes = [];
   for(var i = 0; i < game.word.length; i++){
     dashes.push("_") };
@@ -20,12 +48,26 @@ startGame.addEventListener("click", function(){
 giveUp.addEventListener("click", function(){
   gameWord.innerText = game.word;
   guessesLeft.innerText = "Computer wins!";
+  if(gameCount%2===0){
+    ++losesP2;
+    gamesLostP2.innerText = losesP2;
+  } else {
+    ++losesP1;
+    gamesLostP1.innerText = losesP1;
+  };
 })
 
 var updateCount = function(){
   guessCount = 7 - game.incorrectGuesses.length;
   if(guessCount <= 0) {
-    guessCount = "Computer wins!"
+    guessCount = "Computer wins!";
+      if(gameCount%2===0){
+        ++losesP2;
+        gamesLostP2.innerText = losesP2;
+      } else {
+        ++losesP1;
+        gamesLostP1.innerText = losesP1;
+      };
     gameWord.innerText = game.word;};
   guessesLeft.innerText = guessCount;
 };
@@ -43,7 +85,15 @@ input.addEventListener("keyup", function(){
 
 var checkWinner = function(){
   if(game.correctGuesses.length === game.word.length){
-    guessesLeft.innerText = "User Wins!"}
+    guessesLeft.innerText = "User Wins!";
+      if(gameCount%2===0){
+        ++winsP2;
+        gamesWonP2.innerText = winsP2;
+      } else {
+        ++winsP1;
+        gamesWonP1.innerText = winsP1;
+      };
+  }
 }
 
 lettersGuessed.innerText + game.incorrectGuesses;
@@ -58,4 +108,13 @@ var updateWord = function(letter){
   });
   gameWord.innerText = array.join("");
 };
+
+
+
+// add a games won/lost counter to hang man.
+// these reset back to 0/0 when you reset the page.
+// add a second player.
+// both play against a computer, alternate between players, basically who is winning more games
+
+
 
