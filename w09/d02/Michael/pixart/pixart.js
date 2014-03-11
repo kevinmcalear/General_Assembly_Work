94bac6c06bb23288827702e4f7f02c0cf60b3564
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-11 09:31:02 -0400

    Commit 2

--- w09/d02/Michael/pixart/pixart.js
+++ w09/d02/Michael/pixart/pixart.js
@@ -1,13 +1,17 @@
 //Set Color
 //Step 1:  Identify the div
+var input = document.querySelector("input")
 
 //Step 2:  Write the function
 var changeColor = function(eventObject){
+  eventObject.preventDefault();
   var brush = document.querySelector("div.brush")
   var color = eventObject.target.parentElement.querySelector("#color-field").value
+  input.value = ""
   brush.style.background = color
+
 }
 
 //Step 3:  Write the EventListener
 var panel = document.querySelector(".controls")
-panel.addEventListener("click",changeColor)
+panel.addEventListener("submit",changeColor)
