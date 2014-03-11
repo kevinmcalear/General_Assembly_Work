244c2b3f22d849f7f252d881e6273cd66dd80fec
John Hinrichs <jmhinric@gmail.com>
2014-03-11 09:20:03 -0400

    enter key changes the brush box color

--- w09/d02/John/pixart/pixart.js
+++ w09/d02/John/pixart/pixart.js
@@ -1,8 +1,12 @@
 colorInput = document.getElementById("color-field");
 button = document.querySelector("button");
+form = document.querySelector("form");
 brush = document.querySelector(".brush");
 
-button.addEventListener("click", function() {
+// button.addEventListener("submit", function(e) {
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
   var color = colorInput.value;
   brush.style.background = color;
+  this.reset();
 });
