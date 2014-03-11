57a1b3e6fcd2acf81098bcf26226322a2d1ff569
Zach Brady <zacbra@gmail.com>
2014-03-11 09:45:52 -0400

    commit #2

--- w09/d02/Zach_Brady/pixart/pixart.js
+++ w09/d02/Zach_Brady/pixart/pixart.js
@@ -1,7 +1,14 @@
 var newColorInput = document.querySelector("#color-field");
 var setColorButton = document.querySelector("#set-color");
 var brush = document.querySelector(".brush");
+var enterColor = document.querySelector("enter-color");
 
 setColorButton.addEventListener("click", function(){
     brush.style.backgroundColor = newColorInput.value;
+});
+
+newColorInput.addEventListener("keypress", function(e){
+  if(e.keyCode == 13 ){
+    brush.style.backgroundColor = newColorInput.value;
+  };
 });
