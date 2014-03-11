24736d455670ecd08a2373afce0dd40178303aa1
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-11 09:39:22 -0400

    20 Boxes Added

--- w09/d02/Nicholas/pixart/pixart.js
+++ w09/d02/Nicholas/pixart/pixart.js
@@ -2,12 +2,11 @@ var colorChoice = document.getElementById('color-field');
 var setColorButton = document.getElementById('set-color');
 var brushBox = document.querySelector('.brush');
 
+
 var changeColor = function(){
   brushBox.style.background = colorChoice.value;
   colorChoice.value = "";
 };
-
-
 
 setColorButton.addEventListener('click', changeColor);
 colorChoice.addEventListener('keypress', function(e){
@@ -16,3 +15,9 @@ colorChoice.addEventListener('keypress', function(e){
     changeColor();
   }
 });
+
+for(var i = 0; i < 20; i ++){
+  var box = document.createElement('div');
+  box.className = 'brush';
+  document.body.appendChild(box);
+}
