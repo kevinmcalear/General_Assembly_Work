67d7248616d16eb3723a9e198019113059cfadff
Tahira <tahira.adaya@gmail.com>
2014-03-11 09:47:10 -0400

    commit 5:

--- w09/d02/Tahira/pixart/pixart.js
+++ w09/d02/Tahira/pixart/pixart.js
@@ -2,21 +2,25 @@ var form = document.querySelector("#color-setter");
 var brush = document.querySelector(".brush");
 var body = document.querySelector("body");
 
+color = "#1B4370";
+
 var changeColor = function(eventObject) {
   eventObject.preventDefault();
-  var color = form.elements["color"].value;
+  color = form.elements["color"].value;
   console.log(color);
   brush.style.background = color;
+};
+
+var changeColorToBrush = function(eventObject) {
+  this.style.background = color;
 };
 
 for(var i=0; i<20; i++){
   square = document.createElement("div");
   square.classList.add("square");
   body.insertAdjacentElement("beforeend", square);
-  square.addEventListener("click", function(){
-    this.style.background = "green";
-  });
-}
+  square.addEventListener("click", changeColorToBrush);
+};
 
 form.addEventListener("submit", changeColor);
 
