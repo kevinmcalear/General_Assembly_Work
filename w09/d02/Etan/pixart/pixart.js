979177a0a5c9a38985c9c6cf3756ec8d869b55f6
renagadex2 <etan.berkowitz@gmail.com>
2014-03-11 09:33:22 -0400

    can submit color with return

--- w09/d02/Etan/pixart/pixart.js
+++ w09/d02/Etan/pixart/pixart.js
@@ -5,6 +5,16 @@ var colorField = document.querySelector("#color-field");
 
 var coloredSquare = document.querySelector(".brush");
 
+
 setColorButon.addEventListener("click", function() {
 	coloredSquare.style.backgroundColor=colorField.value;
 })
+
+colorField.addEventListener("keypress", function(e) {
+	if(e.keyCode===13) {
+	coloredSquare.style.backgroundColor=colorField.value;}
+})
+
+
+
+
