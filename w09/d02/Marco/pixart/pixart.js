3d58d109b133b6d9d52c4b6322a80c4ac89f436c
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-11 10:06:29 -0400

    added 20 div squares to body

--- w09/d02/Marco/pixart/pixart.js
+++ w09/d02/Marco/pixart/pixart.js
@@ -1,6 +1,16 @@
 var form = document.querySelector("form");
 var colorBox = document.querySelector(".brush");
 var newColor = document.querySelector("#color-field");
+var body = document.querySelector("body");
+
+var addDivs = function() {
+  for (var i = 0; i < 20; i++) {
+    var div = document.createElement("div");
+    div.classList.add("square");
+    body.appendChild(div);
+  }
+}
+addDivs();
 
 var setColor = function(e) {
   e.preventDefault();
