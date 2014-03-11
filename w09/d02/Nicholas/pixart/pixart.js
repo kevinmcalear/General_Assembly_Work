1572dae589fdfe75495eb87632ba050ecd42ec76
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-11 09:21:57 -0400

    Changed Color by Enter Key

--- w09/d02/Nicholas/pixart/pixart.js
+++ w09/d02/Nicholas/pixart/pixart.js
@@ -2,9 +2,17 @@ var colorChoice = document.getElementById('color-field');
 var setColorButton = document.getElementById('set-color');
 var brushBox = document.querySelector('.brush');
 
-
-
-
-setColorButton.addEventListener('click', function(){
+var changeColor = function(){
   brushBox.style.background = colorChoice.value;
+  colorChoice.value = "";
+};
+
+
+
+setColorButton.addEventListener('click', changeColor);
+colorChoice.addEventListener('keypress', function(e){
+  var key = e.keyCode;
+  if(key == 13) {
+    changeColor();
+  }
 });
