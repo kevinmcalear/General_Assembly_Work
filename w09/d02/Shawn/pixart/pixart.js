8e382ea167b16ce281b81a29df749385be2fb9fd
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-11 09:57:14 -0400

    final 3 steps

--- w09/d02/Shawn/pixart/pixart.js
+++ w09/d02/Shawn/pixart/pixart.js
@@ -15,7 +15,7 @@ colorInput.addEventListener("keypress", function(e) {
   }
 });
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 8000; i++) {
   var div = document.createElement("div");
   div.className = "square";
   body.appendChild(div);
@@ -25,6 +25,13 @@ var squares = document.querySelectorAll("div.square");
 
 for(var i = 0; i < squares.length; i ++) {
   
-  squares[i].addEventListener("click", function(e) {
+  squares[i].addEventListener("mouseover", function(e) {
     e.target.style.background = colorInput.value;})
+  squares[i].style.height = 10;
+  squares[i].style.margin = 0;
 }
+
+// * Modify the CSS so that the "square" class has a height and width of 10px and a margin of 0.
+// * Modify your code so that you are creating 8000 divs instead of 20.
+// * Change the event that changes your box colors from 'click' to 'mouseover'
+// * Paint a picture!
