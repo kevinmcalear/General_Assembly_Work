f4a464be970915fbd045cd8081cef9388ff73f1e
Tahira <tahira.adaya@gmail.com>
2014-03-11 09:36:41 -0400

    pix art commit 3

--- w09/d02/Tahira/pixart/pixart.js
+++ w09/d02/Tahira/pixart/pixart.js
@@ -1,5 +1,6 @@
 var form = document.querySelector("#color-setter");
 var brush = document.querySelector(".brush");
+var body = document.querySelector("body");
 
 var changeColor = function(eventObject) {
   eventObject.preventDefault();
@@ -8,6 +9,11 @@ var changeColor = function(eventObject) {
   brush.style.background = color;
 };
 
+for(var i=0; i<20; i++){
+  square = document.createElement("div");
+  square.class = "square";
+  body.insertAdjacentElement("beforeend", square);
+}
 
 form.addEventListener("submit", changeColor);
 
