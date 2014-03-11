e4e2a385f25c12f10971852ee0c48514aa9b30ff
My Name <myemail@example.com>
2014-03-11 09:43:19 -0400

    commit 3

--- w09/d02/Cory/pixart/pixart.js
+++ w09/d02/Cory/pixart/pixart.js
@@ -3,7 +3,8 @@ var button = document.getElementById("set-color");
 var field = document.getElementById("color-field");
 var brush = document.querySelector(".brush");
 var form = document.getElementById("form");
-
+var body = document.querySelector("body");  
+// var div = document.querySelector(".square");
 
 // Build functions
 var changeColor = function(e) {
@@ -11,9 +12,22 @@ var changeColor = function(e) {
   brush.style.backgroundColor = field.value;
 };
 
+var createDivs = function() {
+  for(var i = 0; i <= 19; i++) {
+    var div = document.createElement("div");
+    div.className = "square";
+    body.appendChild(div);
+  }
+};
+createDivs();
 
 
 
 // Add listeners
 button.addEventListener("click", changeColor);
 form.addEventListener("submit", changeColor);
+
+
+
+
+
