655eee8c90240f85db6e4825c33ef77e9ea36981
My Name <dodd.jl@gmail.com>
2014-03-11 09:40:13 -0400

    create 20 divs and add to body

--- w09/d02/Jenn_Dodd/pixart/pixart.js
+++ w09/d02/Jenn_Dodd/pixart/pixart.js
@@ -1,14 +1,25 @@
 var button = document.querySelector("#set-color");
 var input = document.querySelector("#color-field");
 var form = document.querySelector("#color-picker");
-var colorPixel = document.querySelector(".brush");
+var brushBox = document.querySelector(".brush");
+var body = document.querySelector("body");
+
+
 
 
 var changeColor = function(eventObject){
   eventObject.preventDefault();
-  colorPixel.style.backgroundColor = input.value;
+  brushBox.style.backgroundColor = input.value;
 };
 
+var makeDivs = function(){
+  for(i = 0; i <= 20; i++){
+    var colorPixel = document.createElement("div");
+    colorPixel.classList.add("square");
+    body.appendChild(colorPixel);
+  }
+};
 
+makeDivs();
 
 form.addEventListener("submit", changeColor);
