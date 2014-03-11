026a6e3ad32446eddbf71ce6561028e076b36640
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-11 09:23:56 -0400

    hitting enter changes value

--- w09/d02/Shawn/pixart/pixart.js
+++ w09/d02/Shawn/pixart/pixart.js
@@ -6,3 +6,12 @@ var brush = document.querySelector("div.brush");
 setColor.addEventListener("click", function() {
   brush.style.background = colorInput.value;
 })
+
+colorInput.addEventListener("keypress", function(e) {
+  if(e.keyCode === 13) {
+    brush.style.background = colorInput.value;
+    colorInput.value = "";
+  }
+});
+
+
