b52d1f0a38fccd7fc64cb8b9f480eb136730d3fa
Zack Stayman <z.stayman@gmail.com>
2014-03-11 09:33:06 -0400

    20 divs

--- w09/d02/Zack_Stayman/pixart/pixart.js
+++ w09/d02/Zack_Stayman/pixart/pixart.js
@@ -3,12 +3,18 @@ var box = document.querySelector("div.brush");
 var input = document.getElementById("color-field");
 var colorForm = document.getElementById("color-form");
 
+
 setColor.addEventListener("click", function(e){
   e.preventDefault();
   box.style.background = input.value;
   input.value = "";
 });
 
-document.body.addEventListener("load", function(){
-  for(var i = )
-});
+(function(){
+  for(var i = 0; i < 19; i++){
+    var div = document.createElement("div");
+    div.className = "square";
+    document.body.appendChild(div);
+  };
+})();
+
