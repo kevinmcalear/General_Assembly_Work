e2de9779d6c6ef6714f81edcd81659053bca9c8c
My Name <dodd.jl@gmail.com>
2014-03-11 11:16:06 -0400

    swatches

--- w09/d02/Jenn_Dodd/pixart/pixart.js
+++ w09/d02/Jenn_Dodd/pixart/pixart.js
@@ -1,35 +1,55 @@
 var button = document.querySelector("#set-color");
 var input = document.querySelector("#color-field");
 var form = document.querySelector("#color-picker");
-var brushBox = document.querySelector(".brush");
+var currentBrush = document.querySelector("#current");
+var brushBox1 = document.querySelector("#one");
+var brushBox2 = document.querySelector("#two");
+var brushBox3 = document.querySelector("#three");
 var body = document.querySelector("body");
+var ul = document.querySelector("ul");
 
 
-
+var lastColor = 0;
 
 var changeColor = function(eventObject){
   eventObject.preventDefault();
-  brushBox.style.backgroundColor = input.value;
-};
-
-var makeDivs = function(){
-  for(i = 0; i <= 8000; i++){
-    var colorPixel = document.createElement("div");
-    colorPixel.classList.add("square");
-    colorPixel.style.width = "10px";
-    colorPixel.style.height = "10px";
-    colorPixel.style.margin = "0";
-    body.appendChild(colorPixel);
+  currentBrush.style.backgroundColor = input.value;
+  if (lastColor === 3){
+    lastColor = 0;
   }
+  lastColor += 1;
+  if (lastColor === 1) {
+    brushBox1.style.backgroundColor = input.value;
+  } else if (lastColor === 2) {
+    brushBox2.style.backgroundColor = input.value;
+  } else {
+    brushBox3.style.backgroundColor = input.value;
+  };
+  
+
 };
 
 
+for(var i = 0; i <= 8000; i++){
+  var colorPixel = document.createElement("div");
+  colorPixel.classList.add("square");
+  colorPixel.style.width = "10px";
+  colorPixel.style.height = "10px";
+  colorPixel.style.margin = "0";
+  body.appendChild(colorPixel);
+}
 
-makeDivs();
 
 body.addEventListener("mouseover", function(eventObject) {
   if (eventObject.target.className === "square") {
     eventObject.target.style.backgroundColor = input.value;
+  }
+});
+
+ul.addEventListener("click", function(eventObject) {
+  if (eventObject.target.className === "swatch") {
+    input.value = eventObject.target.style.backgroundColor;
+    currentBrush.style.backgroundColor = input.value;
   }
 });
 
