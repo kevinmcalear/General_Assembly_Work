194d4bec9a34b80bddd78143c740fc02a595a4d9
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-11 10:26:09 -0400

    event listener to change boxes to green

--- w09/d02/Marco/pixart/pixart.js
+++ w09/d02/Marco/pixart/pixart.js
@@ -3,14 +3,22 @@ var colorBox = document.querySelector(".brush");
 var newColor = document.querySelector("#color-field");
 var body = document.querySelector("body");
 
-var addDivs = function() {
-  for (var i = 0; i < 20; i++) {
-    var div = document.createElement("div");
-    div.classList.add("square");
-    body.appendChild(div);
-  }
+
+for (var i = 0; i < 20; i++) {
+  var div = document.createElement("div");
+  var a = document.createElement("a");
+  div.appendChild(a);
+  div.classList.add("square");
+  body.appendChild(div);
 }
-addDivs();
+
+colorDivs = document.querySelectorAll(".square");
+
+for (var i = 0; i < colorDivs.length; i++) {
+  colorDivs[i].addEventListener("click", function() {
+    this.style.background = "green";
+  });
+}
 
 var setColor = function(e) {
   e.preventDefault();
