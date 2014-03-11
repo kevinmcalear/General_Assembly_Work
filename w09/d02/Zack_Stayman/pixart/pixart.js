552b805e7c8705872dfe3978e05097c1736cc9e9
Zack Stayman <z.stayman@gmail.com>
2014-03-11 09:18:45 -0400

    form tag added

--- w09/d02/Zack_Stayman/pixart/pixart.js
+++ w09/d02/Zack_Stayman/pixart/pixart.js
@@ -1,7 +1,14 @@
 var setColor = document.getElementById("set-color");
 var box = document.querySelector("div.brush");
-var input = document.getElementById("color-field")
-setColor.addEventListener("click", function(){
+var input = document.getElementById("color-field");
+var colorForm = document.getElementById("color-form");
+
+setColor.addEventListener("click", function(e){
+  e.preventDefault();
   box.style.background = input.value;
   input.value = "";
+});
+
+document.body.addEventListener("load", function(){
+  for(var i = )
 });
