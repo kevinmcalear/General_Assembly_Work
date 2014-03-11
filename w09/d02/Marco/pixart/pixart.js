877f5dbcd2b3a233a7096c3d6daa79004a75721a
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-11 10:36:07 -0400

    added mouseover function to event listener and 8000 smaller pixels

--- w09/d02/Marco/pixart/pixart.js
+++ w09/d02/Marco/pixart/pixart.js
@@ -1,10 +1,10 @@
 var form = document.querySelector("form");
-var colorBox = document.querySelector(".brush");
+var colorField = document.querySelector(".brush");
 var newColor = document.querySelector("#color-field");
 var body = document.querySelector("body");
 
 
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 8000; i++) {
   var div = document.createElement("div");
   var a = document.createElement("a");
   div.appendChild(a);
@@ -15,14 +15,14 @@ for (var i = 0; i < 20; i++) {
 colorDivs = document.querySelectorAll(".square");
 
 for (var i = 0; i < colorDivs.length; i++) {
-  colorDivs[i].addEventListener("click", function() {
+  colorDivs[i].addEventListener("mouseover", function() {
     this.style.background = newColor.value || "green";
   });
 }
 
 var setColor = function(e) {
   e.preventDefault();
-  colorBox.style.backgroundColor = newColor.value;
+  colorField.style.backgroundColor = newColor.value;
 }
 
 form.addEventListener("submit", setColor);
