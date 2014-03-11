9413657619855ece37a808cc08d2d0cf9fc28c5d
Julia Lovett <julia.m.lovett@gmail.com>
2014-03-11 09:29:45 -0400

    can change color by pressing 'enter'

--- w09/d02/Julia/pixart/pixart.js
+++ w09/d02/Julia/pixart/pixart.js
@@ -5,3 +5,9 @@ var brush = document.querySelector("div.brush");
 setColorButton.addEventListener("click", function(){
 	brush.style.background = inputField.value;
 });
+
+inputField.addEventListener("keypress", function(event){
+	if(event.keyCode === 13){
+		brush.style.background = inputField.value;
+	}
+});
