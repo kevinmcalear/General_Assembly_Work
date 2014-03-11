ea56af902baa78db8ebcd989cd451036aaac04fb
tunneylax20 <bryantunney@gmail.com>
2014-03-11 09:43:10 -0400

    commit 2

--- w09/d02/Drew/pixart/pixart.js
+++ w09/d02/Drew/pixart/pixart.js
@@ -2,7 +2,8 @@ var setColor = document.getElementById('set-color');
 
 
 
-function changeColor(){
+function changeColor(e) {
+  e.preventDefault();
   var brush = document.getElementsByClassName('brush');
   var colorField = document.getElementById('color-field');
   brush[0].style.background = colorField.value;
