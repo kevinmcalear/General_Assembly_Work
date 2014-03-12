6fad425e8ebf9ce5a773f701e960981bfdb0eaaf
My Name <myemail@example.com>
2014-03-11 20:53:03 -0400

    added ids to all cells

---
+++ w09/d02/Cory/tictactoe/js/src/controller.js
@@ -0,0 +1,19 @@
+var game = new Game();
+
+function render(){
+  var board = document.querySelector("ul.board");
+
+  board.innerHTML = "";
+
+  for (var i = 0; i < 9; i++) {
+    var li = document.createElement("li");
+    li.className = game.board[parseInt(i/3)][i%3];
+    li.id = [i];
+    board.appendChild(li);
+    li.addEventListener("click", function() {
+      li.id = game.turn;
+    });
+  }
+}
+
+render();
