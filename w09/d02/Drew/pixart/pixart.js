baf7d61dcd6d41524014b75a7606d8420cb824e7
tunneylax20 <bryantunney@gmail.com>
2014-03-12 07:37:36 -0400

    hw 9 2

--- w09/d02/Drew/pixart/pixart.js
+++ w09/d02/Drew/pixart/pixart.js
@@ -1,13 +1,23 @@
 var setColor = document.getElementById('set-color');
-
-
+var body = document.body
+var colorField = document.getElementById('color-field');
+// var brush = document.getElementsByClassName('brush')
 
 function changeColor(e) {
   e.preventDefault();
   var brush = document.getElementsByClassName('brush');
-  var colorField = document.getElementById('color-field');
+  
   brush[0].style.background = colorField.value;
-
 };
 
-setColor.addEventListener('click', changeColor)
+for(var i = 0; i < 20; i++){
+  var div = document.createElement('div');
+  div.className = "square"
+  body.appendChild(div);
+  div.addEventListener('click', function(){
+    this.style.background = brush[0].style.background;
+  })
+};
+
+
+
