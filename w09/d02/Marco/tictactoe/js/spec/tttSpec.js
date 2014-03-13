c99d92d376b5d3a6f43716c8531b98a8a2ef9488
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-03-13 11:48:41 -0400

    parallax exercises completed

--- w09/d02/Marco/tictactoe/js/spec/tttSpec.js
+++ w09/d02/Marco/tictactoe/js/spec/tttSpec.js
@@ -9,34 +9,34 @@ describe("Game", function() {
 
   describe("mark", function() {
     beforeEach(function() {
+      game.mark("x", {row: 0, column: 0});
+    });
+
+    it("places an x or o on the board", function() {
+      expect(game.playAt( {row: 0, column: 0} )).toBe("x");
+    });
+    
+    it("cannot overwrite a play", function() {
+      game.mark("o", {row: 0, column: 0});
+      expect(game.playAt( {row: 0, column: 0} )).toBe("x"); 
+      expect(game.turn).toBe("o");
+    });
+
+    it("can't play the same mark twice in a row", function(){
       game.mark("x", {row: 1, column: 1});
-    });
-
-    it("places a 'x' or an 'o' in the game", function() {
-      expect(game.playAt({row: 1, column: 1})).toEqual("x");
-    });
-
-    it("can't play an 'o' over a spot played by 'x'", function() {
+      expect(game.playAt( {row: 1, column: 1} )).toBe(undefined);
+      
       game.mark("o", {row: 1, column: 1});
-      expect(game.playAt({row: 1, column: 1})).toBe('x');
-      expect(game.turn).toBe('o')
+      expect(game.playAt( {row: 1, column: 1} )).toBe("o");
     });
-
-    it("doesn't allow two plays in a row", function() {
-      game.mark("x", {row: 2, column: 0});
-      expect(game.playAt({row: 2, column: 0})).toBe(undefined);
-
-      game.mark("o", {row: 2, column: 0});
-      expect(game.playAt({row: 2, column: 0})).toBe('o');
-    }); 
 
     it("only takes valid marks", function() {
-      game.mark("xo", {row: 2, column: 0});
-      game.mark("X", {row: 2, column: 0});
-      game.mark("O", {row: 2, column: 0});
-      game.mark("ke$sha", {row: 2, column: 0});
-
-      expect(game.playAt({row: 2, column: 0})).toBe(undefined);
+      game.mark("X", {row: 1, column: 1});     
+      game.mark("xo", {row: 1, column: 1});
+      game.mark("O", {row: 1, column: 1});
+      game.mark("sadf", {row: 1, column: 1});
+ 
+      expect(game.playAt( {row: 1, column: 1} )).toBe(undefined);
     });
 
     it("doesn't play if the game is over", function() {
@@ -46,6 +46,8 @@ describe("Game", function() {
 
       expect( function() { game.mark("o", {row: 1, column: 1}); }).toThrowError("game over!");
     });
+
+    xit("increments the turn number", function(){});
   });
 
   describe("gameOver", function() {
@@ -70,19 +72,36 @@ describe("Game", function() {
 
   describe("checkWinner", function() {
     it("is true if there are three in a row", function() {
+      var game = new Game();
       game.board[0][0] = "x";
       game.board[0][1] = "x";
       game.board[0][2] = "x";
 
-      expect(game.checkWinner()).toBe('x');
+      expect(game.checkWinner()).toBe("x");
     });
 
     it("is true if there are three in a column", function() {
+      var game = new Game();
+      game.board[0][0] = "x";
+      game.board[1][0] = "x";
+      game.board[2][0] = "x";
 
+      expect(game.checkWinner()).toBe("x");
     });
 
     it("is true if there are three on a diagonal", function() {
+      var game = new Game();
+      game.board[0][0] = "x";
+      game.board[1][1] = "x";
+      game.board[2][2] = "x";
 
+      expect(game.checkWinner()).toBe("x");
+
+      game.board[0][2] = "o";
+      game.board[1][1] = "o";
+      game.board[2][0] = "o";
+
+      expect(game.checkWinner()).toBe("o");
     });
   });
 });
