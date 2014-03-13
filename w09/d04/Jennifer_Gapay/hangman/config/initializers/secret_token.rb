a928a3d5efde48dcd0c294fc5d72a96ac186c051
Jennifer <jennifergapay@gmail.com>
2014-03-13 11:23:42 -0400

    started ajax version of hangman

---
+++ w09/d04/Jennifer_Gapay/hangman/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangman::Application.config.secret_key_base = '80bb6dc5b930091815eb176786a45bc0f50845a819e53ad02c3a42af2337402f4e701f5dc7f273de05b97e1d4620de2e827df80be572dc3f09f5af8f635e88af'
