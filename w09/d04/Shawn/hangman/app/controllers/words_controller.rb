3c6b6c5dad116cd8a5a935c729076c68c09801ab
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-03-13 13:39:36 -0400

    json api

---
+++ w09/d04/Shawn/hangman/app/controllers/words_controller.rb
@@ -0,0 +1,6 @@
+# words_controller.rb
+class WordsController < ApplicationController
+  def random
+    render json: Word.order("RANDOM()").first
+  end
+end
--- w09/d04/Shawn/hangman/config/routes.rb
+++ w09/d04/Shawn/hangman/config/routes.rb
@@ -1,3 +1,9 @@
 Hangman::Application.routes.draw do
   root 'hangman#index'
+
+  resources :words, only: [] do 
+    collection do 
+      get "random"
+    end
+  end
 end
