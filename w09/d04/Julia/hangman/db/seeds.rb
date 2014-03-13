e9abb3e90610e2900f62880fee3e45f13e387559
Julia Lovett <julia.m.lovett@gmail.com>
2014-03-13 12:05:48 -0400

    added random words

--- w09/d04/Julia/hangman/db/seeds.rb
+++ w09/d04/Julia/hangman/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do
+  Word.create(word: Faker::Company.bs.split(' ').first)
+end
