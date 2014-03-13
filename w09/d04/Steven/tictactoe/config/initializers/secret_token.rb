24db9f7ce6a369cd34137ad76711ff0ff42c7c9c
My Name <myemail@example.com>
2014-03-13 16:58:10 -0400

    tic tac toe working

---
+++ w09/d04/Steven/tictactoe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictactoe::Application.config.secret_key_base = 'b7abd9050a69288803063f957ce2f6f1de96752f79208b3290fef96e12b893617e0f953ef4ea8b4756a9367b92e4b7d09fb64ddc45c2fd548b80a68618aca7c8'
