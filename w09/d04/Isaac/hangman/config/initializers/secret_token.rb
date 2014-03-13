ea6b9b64ef25175723f698858dc4fb12de429dbf
My Name <myemail@example.com>
2014-03-13 11:23:04 -0400

    hangman rails set up with jasmine

---
+++ w09/d04/Isaac/hangman/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangman::Application.config.secret_key_base = '9a152d5cf39ad993037de7ddffdae095ed75a3bf120e44afdcf0d017df2e341066f901588521cedb44b5954752e594bfa368a7fe90ef0fa0712f58c3ea5e368c'
