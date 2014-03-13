53b32f576fd19db640ffbf3b5842651787d84a2d
Sahil Trikha <trikha93@gmail.com>
2014-03-13 11:23:20 -0400

    new rails app with old hangman project

---
+++ w09/d04/Sahil/hangman/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangman::Application.config.secret_key_base = '271baae1e3786bd45fd7778ae937e061c33b179650a0326f2cda8894a6cb134dd8f393443d70d03fe15ac10bf53ad4ec4619ff453f54988103bf82a11db81a84'
