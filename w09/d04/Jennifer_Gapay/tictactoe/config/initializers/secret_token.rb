da5288ed7e13f52b65265b002cce8dc9d164dd2c
Jennifer <jennifergapay@gmail.com>
2014-03-13 16:18:43 -0400

    working tic tac toe on rails

---
+++ w09/d04/Jennifer_Gapay/tictactoe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictactoe::Application.config.secret_key_base = 'f0db50de124fc14302e42d8234c120af2cf50e3cb81ec28eb0dc893984fe9b487569bb2095eae1147fd9b2b936fb659667d887123b6303d666a9af2e0f819ee9'
