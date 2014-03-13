4c3d296ff4c9421566e379e18a7579970818888f
Zack Stayman <z.stayman@gmail.com>
2014-03-13 12:05:54 -0400

    working rails

---
+++ w09/d04/Zack_Stayman/hangman/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangman::Application.config.secret_key_base = '89ac930bfd0d7414a8defd4046f2cbb479d4c98589666bf078cf10b95d6b4679a9b079aa074ae0471b07c8a9305c3d8c32ae77751351aea497f81f8d769b7738'
