18712b76ea70e16c9876e7effe505974dbe003a4
Quincy Iheme <qiheme@gmail.com>
2014-03-13 16:58:34 -0400

    got tictactoe on rails

---
+++ w09/d04/Quincy/Tictactoe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tictactoe::Application.config.secret_key_base = 'd379de624cd53fb7fbf6c44f0d2c7d09e26150bb7a6c427e7f339a5ec19653bb493df124077bb1bef8d308d38e0d8be2074eb79171ca62be2a9fc68ca4fef9dd'
