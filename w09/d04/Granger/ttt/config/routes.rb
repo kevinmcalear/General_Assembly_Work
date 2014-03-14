ceb10a61184ac623fc7d4021ec3ca843c06c64e9
Granger <GrangerAbuhoff@gmail.com>
2014-03-14 09:02:48 -0400

    hw attempt

--- w09/d04/Granger/ttt/config/routes.rb
+++ w09/d04/Granger/ttt/config/routes.rb
@@ -2,6 +2,8 @@ Ttt::Application.routes.draw do
 
   root to: 'games#new'
 
+  resources :games, only: [:create, :index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
