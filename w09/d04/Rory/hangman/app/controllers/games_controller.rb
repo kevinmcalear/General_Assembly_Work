d76688dae9180d32c8d6a94bd0b622ea9a7612cd
Rory Budnick <rorybudnick@gmail.com>
2014-03-14 09:01:57 -0400

    hw 9 4

--- w09/d04/Rory/hangman/app/controllers/games_controller.rb
+++ w09/d04/Rory/hangman/app/controllers/games_controller.rb
@@ -9,6 +9,9 @@ class GamesController < ApplicationController
     render json: @game
     #it's customary to respond with the newly created object
     #a json representation of the game is rendering
+    #otherwise it will try to render an html template
+    #servers get requests and they respond.  in this case we've received
+    #a request and we need to respond with something.
   end
 
 end
