87db3282b628033da03565754efb980f5f57e593
Peter Lai <peter.yd.lai@gmail.com>
2014-03-31 10:30:27 -0400

    recursion

--- w12/d01/INSTRUCTOR/jsrecursion/recursion.js
+++ w12/d01/INSTRUCTOR/jsrecursion/recursion.js
@@ -17,3 +17,30 @@ function recFactorial(n){
 
 console.log(loopFactorial(5));
 console.log(recFactorial(5));
+
+function loopFib(n){
+  var fib1 = 0;
+  var fib2 = 1;
+  var sum = fib1 + fib2;
+  for( var i = 1; i < n; i++){
+    sum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = sum;
+  }
+  return sum;
+}
+
+function recFib(n) {
+  if (n === 0){
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return recFib(n-1) + recFib(n-2);
+  }
+}
+
+
+
+
+
