a85ef745d4cf8cc817e647857cd567de69442ba8
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2014-04-01 10:19:53 -0400

    pre optimized

--- w12/d02/INSTRUCTOR/rails-opt/app/controllers/app_controller.rb
+++ w12/d02/INSTRUCTOR/rails-opt/app/controllers/app_controller.rb
@@ -1,8 +1,6 @@
 class AppController < ApplicationController
   def index
-    @posts = Post.all.includes(:author)
-    @movies =  Rails.cache.fetch(params[:s]) do
-      HTTParty.get("http://www.omdbapi.com/?s=#{params[:s]}").to_json
-    end
+    @posts = Post.all
+    #@movies = HTTParty.get("http://www.omdbapi.com/?s=#{params[:s]}").to_json
   end
 end
