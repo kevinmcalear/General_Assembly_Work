9e55a83db64715e426a1624ef3a2655a945a26f6
Peter Lai <peter.yd.lai@gmail.com>
2014-04-04 09:50:13 -0400

    refactor

--- w12/d05/INSTRUCTOR/poker/lib/poker.rb
+++ w12/d05/INSTRUCTOR/poker/lib/poker.rb
@@ -4,126 +4,41 @@ class Hand
   end
 
   def values
-    myfavoritestnumbersintheworld = []
-    @cards.each do |mycard|
-      value = mycard[0]
-
-                if value == "A"
-
-  myfavoritestnumbersintheworld << 1
-  elsif value == "T"
-  myfavoritestnumbersintheworld << 10
-
-
-  elsif value == "J"
-  myfavoritestnumbersintheworld.push(11)
-  elsif value =="Q"
-  myfavoritestnumbersintheworld << 12
-  elsif value == "K"
-  myfavoritestnumbersintheworld.push(13)
-  else
-  myfavoritestnumbersintheworld.push(value.to_i)
-  end
-    end
-    myfavoritestnumbersintheworld
+    @cards.map {|card| map_to_number(card[0])}.sort
   end
 
   def suits
-    mycardarray = Array.new
-    for card in @cards do
-    mycardarray << card.split("")[1]
-    end
-    return mycardarray
+    @cards.map {|card| card[1]}
+  end
+
+  def map_to_number(value)
+    letter_key = {"A" => 1, "T" => 10, "J" => 11, "Q" => 12, "K" => 13}
+    letter_key[value] || value.to_i
   end
 
   def straight?
-    ordered_values = values.sort
-    is_straight = true
-    ordered_values.each_with_index do |value, index| 
-    if index > 0
-      if value != (ordered_values[index - 1] + 1)
-    is_straight = false
-    end
-      end
-    end
-    is_straight
+    (values[0]..values[-1]).to_a == values
   end
 
   def flush?
-    is_flush = true
-    suits.each_with_index do |suit, index| 
-    if index > 0
-    if suit != (suits[index - 1])
-    is_flush = false
-    end
-    end
-    end
-    is_flush
+    suits.uniq.length == 1
   end
 
   def full_house?
-    ordered_values = values.sort
-    if values.count(ordered_values[0]) == 2
-      if values.count(ordered_values[-1]) == 3
-        true
-      end
-    elsif values.count(ordered_values[-1]) == 2
-      if values.count(ordered_values[0]) == 3
-        true
-      end
-    else
-      false
-    end
-      
-
+    values.uniq.length == 2 && values[1] != values[3]
   end
 
   def four_of_a_kind?
-    ordered_values = values.sort
-    if ordered_values[0] == ordered_values[1] && ordered_values[1] == ordered_values[2] && ordered_values[2] == ordered_values[3]
-      true
-    elsif ordered_values[1] == ordered_values[2] && ordered_values[2] == ordered_values[3] && ordered_values[3] == ordered_values[4]
-      true
-    else
-      false
-    end
+    values.uniq.length == 2 && values[1] == values[3]
   end
 
   def best_hand
-        ordered_values = values.sort
-    is_straight = true
-    ordered_values.each_with_index do |value, index| 
-    if index > 0
-    if value != (ordered_values[index - 1] + 1)
-    is_straight = false
-    end
-    end
-    end
-    is_flush = true
-    suits.each_with_index do |suit, index| 
-    if index > 0
-    if suit != (suits[index - 1])
-    is_flush = false
-    end
-    end
-    end
-
-    if is_straight && is_flush
-      "Straight Flush"
-    elsif four_of_a_kind?
-      "Four of a Kind"
-    elsif full_house?
-      "Full House"
-    elsif flush?
-      "Flush"
-    elsif straight?
-      "Straight"
-    else
-      "Not much"
-    end
+    straight? && flush? ? "Straight Flush"
+    : four_of_a_kind? ? "Four of a Kind"
+    : full_house? ? "Full House"
+    : flush? ? "Flush"
+    : straight? ? "Straight"
+    : "Not much"
   end
 
 end
-
-
-
